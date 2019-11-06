7c845f953ce6e1c242a7dcf02da0aab3310b5d3d
James Reames <james.reames@realpage.com>
2019-11-06 08:18:11 -0600

    testing check in

--- Enterprise/Foundation/Activity/Database/Logging/Stored Procedures/ListLoggingMetadata.sql
+++ Enterprise/Foundation/Activity/Database/Logging/Stored Procedures/ListLoggingMetadata.sql
@@ -5,7 +5,6 @@ BEGIN
  
 	SET NOCOUNT ON;
 	-- Get Product, log type & log category list to display in drop-down box of search UI
-	-- testing
 	select ProductID, BooksProductCode from Logging.Product
 	select LogCategoryTypeid, Name  from   Logging.LogCategoryType
 	select LogTypeId,Name,LogcategoryTypeId from Logging.LogType
