6e417478288c7cee7966e8d4c4376f691177a6c9
Nabil Nabhan <nabil.nabhan@realpage.com>
2019-11-21 11:04:28 -0600

    Removing OR Alter

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Person/Stored Procedures/ListPersons_Ver04.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Person/Stored Procedures/ListPersons_Ver04.sql
@@ -1,4 +1,4 @@
-﻿CREATE OR ALTER PROCEDURE [Person].[ListPersons_Ver04] (
+﻿CREATE PROCEDURE [Person].[ListPersons_Ver04] (
 	@RealPageId uniqueidentifier = NULL,
 	@ParentPartyRoleTypeId int = NULL,
 	@UserListFilterType tinyint = 0,
