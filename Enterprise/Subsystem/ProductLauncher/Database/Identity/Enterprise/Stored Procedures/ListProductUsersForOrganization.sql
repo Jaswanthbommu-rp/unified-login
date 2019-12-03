0f7437825428aecb5c84d1d5af48a0d3e5af8cd0
Vijayasekher Manellore <vijayasekher.manellore@realpage.com>
2019-12-03 12:58:36 -0600

    GB-6889- AO Multi Company Proc Update

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Enterprise/Stored Procedures/ListProductUsersForOrganization.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Enterprise/Stored Procedures/ListProductUsersForOrganization.sql
@@ -1,20 +1,19 @@
-﻿/*
-EXECUTE Person.ListUsersWithCompanyId @CompanyId = 379, @ProductId = 'CD,LS,OPS'
-EXECUTE Person.ListUsersWithCompanyId @CompanyId = 4638, @ProductId = '1,2,3'
-EXECUTE Person.ListUsersWithCompanyId @CompanyId = '379,4638',  @ProductId = 'CD,LS,OPS'
-
+﻿
+/*
+EXECUTE Enterprise.ListProductUsersForOrganization @OrgPartyId = 6768, @ProductId = 'CD,LS,OPS'
+EXECUTE Enterprise.ListProductUsersForOrganization @OrgPartyId = 6768, @ProductId = '4'
 */
 
 Create PROCEDURE Enterprise.ListProductUsersForOrganization
-(@OrgPartyId   NVARCHAR(100)  = NULL, 
- @ProductId NVARCHAR(200) = NULL
+(@OrgPartyId INT, 
+ @ProductId  NVARCHAR(100)
 )
 AS
     BEGIN
-        DECLARE @Now DATETIME= GETUTCDATE();
+        DECLARE @BlueBookId INT, @Now DATETIME= GETUTCDATE();
         WITH Products
              AS (SELECT DISTINCT 
-						p.PersonaID, 
+                        p.PersonaID, 
                         pp.ProductId
                  FROM Person.Persona p
                       INNER JOIN Ident.UserLoginPersona ULP ON ULP.UserLoginPersonaId = p.UserLoginPersonaId
@@ -24,7 +23,7 @@ AS
                       INNER JOIN Enterprise.ProductSetting ps ON prc.ProductSettingId = ps.ProductSettingId
                                                                  AND ps.Value = '8'
                       INNER JOIN Enterprise.ProductSettingType pst ON ps.ProductSettingTypeId = pst.ProductSettingTypeId
-                                                                      AND pst.Name = 'ProductStatus'					  
+                                                                      AND pst.Name = 'ProductStatus'
                  WHERE((@NOW BETWEEN pec.FromDate AND pec.ThruDate)
                        OR (@NOW >= pec.FromDate
                            AND pec.ThruDate IS NULL))
@@ -36,63 +35,41 @@ AS
                                AND ps.ThruDate IS NULL))
                       AND pec.ProductId NOT IN(14, 19, 24, 25, 34, 39) --Client Portal, Product Learning Portal, Black Book, Self-provisioning portal, Benchmarking, Integration Marketplace
                       AND (
-							 (
-								 SELECT COUNT(*)
-								 FROM STRING_SPLIT(@ProductId, ',')
-							 ) = 0)
-								  OR pp.ProductId IN
-							 (
-								 SELECT *
-								 FROM STRING_SPLIT(@ProductId, ',')
-							 )),
-             ConcatProducts
-             AS (SELECT DISTINCT 
-                        P2.PersonaId, 
-                        SUBSTRING(
                  (
-                     SELECT ',' + CONVERT(NVARCHAR(100), PR1.ProductId) AS [text()]
-                     FROM Products PR1
-                     WHERE PR1.PersonaId = P2.PersonaId
-                     ORDER BY PR1.PersonaId FOR XML PATH('')
-                 ), 2, 1000) ProductId
-                 FROM Products P2),
+                     SELECT COUNT(*)
+                     FROM STRING_SPLIT(@ProductId, ',')
+                 ) = 0)
+                      OR pp.ProductId IN
+                 (
+                     SELECT *
+                     FROM STRING_SPLIT(@ProductId, ',')
+                 )),
              Users
              AS (SELECT ul.UserId, 
                         ul.LoginName, 
                         p.FirstName, 
-                        p.LastName,                        
-                        pa.realpageid 'RealPageId', 
-                        dim.SourceId, 
-                        cp.ProductId,
-						sua.Value 'ProductUserName'
-                FROM ident.UserLogin ul
+                        p.LastName, 
+                        cp.ProductId, 
+                        sua.Value 'ProductUserName'
+                 FROM ident.UserLogin ul
                       INNER JOIN ident.UserLoginPersona ulp ON ul.UserId = ulp.UserLoginId
                       INNER JOIN person.Persona p2 ON ulp.UserLoginPersonaId = p2.UserLoginPersonaId
                       INNER JOIN Person.Person p ON ul.PersonPartyId = p.partyid
-                      INNER JOIN Enterprise.Party pa ON pa.partyid = p.PartyId
-                      INNER JOIN ConcatProducts cp ON cp.PersonaId = p2.PersonaId
-					  INNER JOIN Ident.SamlUserAttribute sua ON sua.PersonaId = p2.PersonaId 
-					  AND sua.ProductId =  cp.ProductId	  AND sua.SamlAttributeId = 1
+                      INNER JOIN Products cp ON cp.PersonaId = p2.PersonaId
+                      INNER JOIN Ident.SamlUserAttribute sua ON sua.PersonaId = cp.PersonaId
+                                                                AND sua.SamlAttributeId = 1
+                                                                AND sua.ProductId IN(SELECT *
+                                                                                     FROM STRING_SPLIT(@ProductId, ','))
                       INNER JOIN Enterprise.DataImportMapping dim ON ULP.OrganizationPartyId = dim.PartyId
-                      INNER JOIN Enterprise.DataImportApplication dia ON dim.DataImportApplicationId = dia.DataImportApplicationId					 
-                      
-                 WHERE
-				 (
-							 (
-								 SELECT COUNT(*)
-								 FROM STRING_SPLIT(@OrgPartyId, ',')
-							 ) = 0)
-								  OR dim.PartyId  IN
-							 (
-								 SELECT *
-								 FROM STRING_SPLIT(@OrgPartyId, ',')
-							 ))
+                      INNER JOIN Enterprise.DataImportApplication dia ON dim.DataImportApplicationId = dia.DataImportApplicationId
+                 WHERE dia.Name = 'Bluebook'
+                       AND dim.PartyId = @OrgPartyId)
              SELECT UserId, 
                     LoginName, 
                     FirstName, 
-                    LastName,                  
-                    ProductId,
-					ProductUserName
+                    LastName, 
+                    ProductId, 
+                    ProductUserName
              FROM Users u
              WHERE ProductId IS NOT NULL;
     END;
