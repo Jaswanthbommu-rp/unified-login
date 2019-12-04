e6de5ba37149b77e83d7685c55684c5fdfd81204
DJ Singh <dj.singh@realpage.com>
2019-12-04 09:37:33 -0600

    Adding back CIMPL rights back for production release.

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/December2019Release.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/December2019Release.sql
@@ -529,3 +529,465 @@ BEGIN
 
 END;
 GO
+
+DECLARE @OrgRowNum INT;
+DECLARE @ActionID INT;
+DECLARE @RightID INT;
+DECLARE @RoleID INT;
+DECLARE @Status INT;
+DECLARE @ActionValueID INT;
+DECLARE @OrgID INT;
+DECLARE @ProductID INT;
+DECLARE @ParentActionId INT;
+DECLARE @UserActionId INT;
+DECLARE @RightCategoryId INT;
+DECLARE @PartyId INT;
+DECLARE @RightName VARCHAR(100);
+DECLARE @RVT INT;
+DECLARE @DefaultRoute NVARCHAR(200);
+DECLARE @RightValueTypeId INT;
+DECLARE @StatusId INT;
+DECLARE @PersonaId INT;
+DECLARE @FromDate DATETIME;
+DECLARE @TRoleId INT;
+DECLARE @TRoleName NVARCHAR(500);
+DECLARE @TRoleDesc NVARCHAR(500);
+DECLARE @TRightId INT;
+DECLARE @TRightName NVARCHAR(500);
+DECLARE @TRightDesc NVARCHAR(500);
+DECLARE @RightCategory INT;
+DECLARE @RoleCategory INT;
+DECLARE @RoleName NVARCHAR(500);
+DECLARE @RoleTypeID INT;
+DECLARE @PerosonaP INT;
+DECLARE @PartyRowNum INT;
+DECLARE @TRightShortName NVARCHAR(100)
+DECLARE @TargetProductId INT;
+DECLARE @VisibilityStatusId INT
+IF OBJECT_ID('tempdb..#RightsUnifiedSettings') IS NOT NULL
+BEGIN
+	DROP TABLE #RightsUnifiedSettings;
+END;
+
+IF OBJECT_ID('tempdb..#HoldPartyForUnifiedSettings') IS NOT NULL
+BEGIN
+	DROP TABLE #HoldPartyForUnifiedSettings;
+END;
+
+CREATE TABLE #RightsUnifiedSettings
+( 
+			 RightId int, Name nvarchar(500), description nvarchar(500), shortname varchar(100)
+);
+
+INSERT INTO #RightsUnifiedSettings( rightid, name, description, shortname )
+VALUES( 1, 'Manage Sensitive Financial Data in CIMPL', 'Manage Sensitive Financial Data in CIMPL', 'CIMPLManageSensitiveFinancialData' );
+
+SELECT @ProductId = ProductId
+FROM Enterprise.Product
+WHERE name = 'Unified Platform';
+
+--select * from enterprise.product where name like '%lead%'
+
+SELECT @TargetProductId = ProductId
+FROM Enterprise.Product
+WHERE Name = 'CIMPL';
+
+SET @ActionValueID = 1
+
+SELECT @RoleCategory = ST.StatusTypeId
+FROM Enterprise.StatusTypeCategoryType AS STCT
+	 JOIN
+	 Enterprise.StatusTypeCategory AS STC
+	 ON STCT.StatusTypeCategoryTypeId = STC.StatusTypeCategoryTypeId
+	 JOIN
+	 Enterprise.StatusTypeCategoryClassification AS STCC
+	 ON STCC.StatusTypeCategoryId = STC.StatusTypeCategoryId
+	 JOIN
+	 Enterprise.StatusType AS ST
+	 ON ST.StatusTypeId = STCC.StatusTypeId
+WHERE STC.Name = 'Role Type' AND 
+	  ST.Name = 'System';
+
+SELECT @RightCategory = ST.StatusTypeId
+FROM Enterprise.StatusTypeCategoryType AS STCT
+	 JOIN
+	 Enterprise.StatusTypeCategory AS STC
+	 ON STCT.StatusTypeCategoryTypeId = STC.StatusTypeCategoryTypeId
+	 JOIN
+	 Enterprise.StatusTypeCategoryClassification AS STCC
+	 ON STCC.StatusTypeCategoryId = STC.StatusTypeCategoryId
+	 JOIN
+	 Enterprise.StatusType AS ST
+	 ON ST.StatusTypeId = STCC.StatusTypeId
+WHERE STC.Name = 'Right Type' AND 
+	  ST.Name = 'System';
+
+
+SELECT @Status = StatusType.StatusTypeID
+FROM Enterprise.StatusTypeCategoryType
+	 JOIN
+	 Enterprise.StatusTypeCategory
+	 ON StatusTypeCategory.StatusTypeCategoryTypeId = StatusTypeCategoryType.StatusTypeCategoryTypeId
+	 JOIN
+	 Enterprise.StatusTypeCategoryClassification
+	 ON StatusTypeCategoryClassification.StatusTypeCategoryId = StatusTypeCategory.StatusTypeCategoryId
+	 JOIN
+	 Enterprise.StatusType
+	 ON StatusType.StatusTypeId = StatusTypeCategoryClassification.StatusTypeId
+WHERE StatusType.name = 'ALL' AND 
+	  StatusTypeCategoryType.Name = 'Security';
+
+SET @VisibilityStatusId = @Status;
+
+IF NOT EXISTS
+(
+	SELECT 1
+	FROM Enterprise.ACTION
+	WHERE ObjectValue = 'CIMPL Manage Sensitive Financial Data' AND 
+		  ParentActionId IS NULL
+)
+BEGIN
+	EXEC Enterprise.CreateAction @ProductID = @ProductId, @Action = N'CIMPL Manage Sensitive Financial Data', @ActionTarget = N'Right', @ActionbValueTypeId = 1, @Description = '', @ActionID = @ActionID OUTPUT;
+	SELECT @ActionID AS N'@ActionID';
+END;
+
+SELECT @ParentActionId = ActionId
+FROM Enterprise.ACTION
+WHERE ObjectValue = 'Dashboard' AND 
+	  ObjectType = 'Route' AND 
+	  Description = 'SuperUser';
+
+IF NOT EXISTS
+(
+	SELECT 1
+	FROM Enterprise.ACTION
+	WHERE ObjectValue = 'CIMPL Manage Sensitive Financial Data' AND 
+		  ParentActionID = @ParentActionId
+)
+BEGIN
+	EXEC [Enterprise].[CreateAction] @ProductID = @ProductId, @Action = N'CIMPL Manage Sensitive Financial Data', @ActionTarget = N'Right', @ActionbValueTypeId = 1, @Description = '', @ParentActionID = @ParentActionId, @ActionID = @ActionID OUTPUT;
+	SELECT @ActionID AS N'@ActionID';
+END;
+
+SELECT DISTINCT 
+	   IDENTITY(int, 1, 1) AS RowNumber, o.PartyId OrganizationPartyID, 0 AS PStatus
+INTO #HoldPartyForUnifiedSettings
+FROM Enterprise.Organization o
+	 INNER JOIN Enterprise.Party p
+	 ON P.partyid  = O.PartyId
+--WHERE O.Name = 'RealPage Employee'
+
+WHILE EXISTS
+(
+	SELECT 1
+	FROM #HoldPartyForUnifiedSettings
+	WHERE PStatus = 0
+)
+BEGIN
+	SELECT TOP 1 @PartyRowNum = Rownumber, @PartyId = OrganizationPartyID
+	FROM #HoldPartyForUnifiedSettings
+	WHERE PStatus = 0;
+	SELECT @RoleId = RoleId
+	FROM Enterprise.Role AS R
+		 INNER JOIN
+		 Enterprise.RoleValueType AS RR
+		 ON RR.RoleValueTypeId = R.RoleValueTypeId
+	WHERE RR.Value = 'User Administrator' AND 
+		  R.PartyId = @PartyId;
+	DECLARE Rights CURSOR
+	FOR SELECT RightId, Name, Description, ShortName
+		FROM #RightsUnifiedSettings;
+	OPEN Rights;
+	FETCH Rights INTO @TRightId, @TRightName, @TRightDesc, @TRightShortName;
+	WHILE @@FETCH_STATUS = 0
+	BEGIN
+		EXECUTE Enterprise.CreateRight @RoleId = -1, @RightName = 'Default_CIMPLManageSensitiveFinancialData', @ShortName = 'CIMPLManageSensitiveFinancialData', @RightCategoryId = @RightCategory, @PartyId = @PartyId, @ProductId = @ProductId, @Description = '',  @TargetProductId = @TargetProductId, @VisibilityStatusId = @VisibilityStatusId, @RightId = @RightId OUTPUT;
+		EXECUTE Enterprise.CreateRight @RoleId = @RoleId, @RightName = @TRightName, @RightCategoryId = @RightCategory, @PartyId = @PartyId, @ProductId = @ProductId, @Shortname = @TRightShortName, @Description = @TRightDesc, @TargetProductId = @TargetProductId, @VisibilityStatusId = @VisibilityStatusId, @RightId = @RightId OUTPUT;
+
+
+		SELECT @ActionID = ActionID
+		FROM Enterprise.ACTION
+		WHERE ObjectValue = 'CIMPL Manage Sensitive Financial Data'  AND 
+			  ObjectType = 'Right' AND 
+			  ParentActionId IS NULL;
+		EXEC [Enterprise].[LinkActionToRights] @ActionID = @ActionID, @RightId = @RightId, @StatusId = @Status, @UserActionId = @UserActionId OUTPUT;
+
+		SELECT @RightId = RightId
+		FROM Enterprise.[Right] AS R
+			 INNER JOIN
+			 Enterprise.RightValueType AS RR
+			 ON RR.RightValueTypeId = R.RightValueTypeId
+		WHERE RR.Value = 'Default_CIMPLManageSensitiveFinancialData';
+		SELECT @ActionID = ActionID
+		FROM Enterprise.ACTION
+		WHERE ObjectValue = 'CIMPL Manage Sensitive Financial Data'   AND 
+			  ObjectType = 'Right' AND 
+			  ParentActionId IS NULL;
+		EXEC [Enterprise].[LinkActionToRights] @ActionID = @ActionID, @RightId = @RightId, @StatusId = @Status, @UserActionId = @UserActionId OUTPUT;
+
+
+
+		FETCH Rights INTO @TRightId, @TRightName, @TRightDesc, @TRightShortName;
+	END;
+	CLOSE Rights;
+	DEALLOCATE Rights;
+	UPDATE #HoldPartyForUnifiedSettings
+	  SET PStatus = 1
+	WHERE RowNumber = @PartyRowNum;
+END;
+
+DECLARE @Dashboard int;
+
+SELECT @DashBoard = RightValueTypeId
+FROM Enterprise.RightValueType
+WHERE value = 'Default_CIMPLManageSensitiveFinancialData';
+
+SELECT @RightValueTypeId = RightValueTypeId
+FROM Enterprise.RightValueType
+WHERE value IN( 'Manage Sensitive Financial Data in CIMPL' );
+
+IF NOT EXISTS
+(
+	SELECT 1
+	FROM Enterprise.RightDependency
+	WHERE RightValueTypeId = @RightValueTypeId AND 
+		  DependentRightValueTypeId = @DashBoard
+)
+BEGIN
+	INSERT INTO Enterprise.RightDependency( RightValueTypeId, DependentRightValueTypeId )
+	VALUES( @RightValueTypeId, @DashBoard );
+
+END;
+GO
+
+DECLARE @OrgRowNum INT;
+DECLARE @ActionID INT;
+DECLARE @RightID INT;
+DECLARE @RoleID INT;
+DECLARE @Status INT;
+DECLARE @ActionValueID INT;
+DECLARE @OrgID INT;
+DECLARE @ProductID INT;
+DECLARE @ParentActionId INT;
+DECLARE @UserActionId INT;
+DECLARE @RightCategoryId INT;
+DECLARE @PartyId INT;
+DECLARE @RightName VARCHAR(100);
+DECLARE @RVT INT;
+DECLARE @DefaultRoute NVARCHAR(200);
+DECLARE @RightValueTypeId INT;
+DECLARE @StatusId INT;
+DECLARE @PersonaId INT;
+DECLARE @FromDate DATETIME;
+DECLARE @TRoleId INT;
+DECLARE @TRoleName NVARCHAR(500);
+DECLARE @TRoleDesc NVARCHAR(500);
+DECLARE @TRightId INT;
+DECLARE @TRightName NVARCHAR(500);
+DECLARE @TRightDesc NVARCHAR(500);
+DECLARE @RightCategory INT;
+DECLARE @RoleCategory INT;
+DECLARE @RoleName NVARCHAR(500);
+DECLARE @RoleTypeID INT;
+DECLARE @PerosonaP INT;
+DECLARE @PartyRowNum INT;
+DECLARE @TRightShortName NVARCHAR(100)
+DECLARE @TargetProductId INT;
+DECLARE @VisibilityStatusId INT
+IF OBJECT_ID('tempdb..#RightsUnifiedSettings') IS NOT NULL
+BEGIN
+	DROP TABLE #RightsUnifiedSettings;
+END;
+
+IF OBJECT_ID('tempdb..#HoldPartyForUnifiedSettings') IS NOT NULL
+BEGIN
+	DROP TABLE #HoldPartyForUnifiedSettings;
+END;
+
+CREATE TABLE #RightsUnifiedSettings
+( 
+			 RightId int, Name nvarchar(500), description nvarchar(500), shortname varchar(100)
+);
+
+INSERT INTO #RightsUnifiedSettings( rightid, name, description, shortname )
+VALUES( 1, 'Manage Personally Identifiable Information (PII) in CIMPL', 'Manage Personally Identifiable Information (PII) in CIMPL', 'CIMPLManagePII' );
+
+SELECT @ProductId = ProductId
+FROM Enterprise.Product
+WHERE name = 'Unified Platform';
+
+--select * from enterprise.product where name like '%lead%'
+
+SELECT @TargetProductId = ProductId
+FROM Enterprise.Product
+WHERE Name = 'CIMPL';
+
+SET @ActionValueID = 1
+
+SELECT @RoleCategory = ST.StatusTypeId
+FROM Enterprise.StatusTypeCategoryType AS STCT
+	 JOIN
+	 Enterprise.StatusTypeCategory AS STC
+	 ON STCT.StatusTypeCategoryTypeId = STC.StatusTypeCategoryTypeId
+	 JOIN
+	 Enterprise.StatusTypeCategoryClassification AS STCC
+	 ON STCC.StatusTypeCategoryId = STC.StatusTypeCategoryId
+	 JOIN
+	 Enterprise.StatusType AS ST
+	 ON ST.StatusTypeId = STCC.StatusTypeId
+WHERE STC.Name = 'Role Type' AND 
+	  ST.Name = 'System';
+
+SELECT @RightCategory = ST.StatusTypeId
+FROM Enterprise.StatusTypeCategoryType AS STCT
+	 JOIN
+	 Enterprise.StatusTypeCategory AS STC
+	 ON STCT.StatusTypeCategoryTypeId = STC.StatusTypeCategoryTypeId
+	 JOIN
+	 Enterprise.StatusTypeCategoryClassification AS STCC
+	 ON STCC.StatusTypeCategoryId = STC.StatusTypeCategoryId
+	 JOIN
+	 Enterprise.StatusType AS ST
+	 ON ST.StatusTypeId = STCC.StatusTypeId
+WHERE STC.Name = 'Right Type' AND 
+	  ST.Name = 'System';
+
+
+SELECT @Status = StatusType.StatusTypeID
+FROM Enterprise.StatusTypeCategoryType
+	 JOIN
+	 Enterprise.StatusTypeCategory
+	 ON StatusTypeCategory.StatusTypeCategoryTypeId = StatusTypeCategoryType.StatusTypeCategoryTypeId
+	 JOIN
+	 Enterprise.StatusTypeCategoryClassification
+	 ON StatusTypeCategoryClassification.StatusTypeCategoryId = StatusTypeCategory.StatusTypeCategoryId
+	 JOIN
+	 Enterprise.StatusType
+	 ON StatusType.StatusTypeId = StatusTypeCategoryClassification.StatusTypeId
+WHERE StatusType.name = 'ALL' AND 
+	  StatusTypeCategoryType.Name = 'Security';
+
+SET @VisibilityStatusId = @Status;
+
+IF NOT EXISTS
+(
+	SELECT 1
+	FROM Enterprise.ACTION
+	WHERE ObjectValue = 'CIMPL Manage PII' AND 
+		  ParentActionId IS NULL
+)
+BEGIN
+	EXEC Enterprise.CreateAction @ProductID = @ProductId, @Action = N'CIMPL Manage PII', @ActionTarget = N'Right', @ActionbValueTypeId = 1, @Description = '', @ActionID = @ActionID OUTPUT;
+	SELECT @ActionID AS N'@ActionID';
+END;
+
+SELECT @ParentActionId = ActionId
+FROM Enterprise.ACTION
+WHERE ObjectValue = 'Dashboard' AND 
+	  ObjectType = 'Route' AND 
+	  Description = 'SuperUser';
+
+IF NOT EXISTS
+(
+	SELECT 1
+	FROM Enterprise.ACTION
+	WHERE ObjectValue = 'CIMPL Manage PII' AND 
+		  ParentActionID = @ParentActionId
+)
+BEGIN
+	EXEC [Enterprise].[CreateAction] @ProductID = @ProductId, @Action = N'CIMPL Manage PII', @ActionTarget = N'Right', @ActionbValueTypeId = 1, @Description = '', @ParentActionID = @ParentActionId, @ActionID = @ActionID OUTPUT;
+	SELECT @ActionID AS N'@ActionID';
+END;
+
+SELECT DISTINCT 
+	   IDENTITY(int, 1, 1) AS RowNumber, o.PartyId OrganizationPartyID, 0 AS PStatus
+INTO #HoldPartyForUnifiedSettings
+FROM Enterprise.Organization o
+	 INNER JOIN Enterprise.Party p
+	 ON P.partyid  = O.PartyId
+	 --WHERE O.Name = 'RealPage Employee'
+
+WHILE EXISTS
+(
+	SELECT 1
+	FROM #HoldPartyForUnifiedSettings
+	WHERE PStatus = 0
+)
+BEGIN
+	SELECT TOP 1 @PartyRowNum = Rownumber, @PartyId = OrganizationPartyID
+	FROM #HoldPartyForUnifiedSettings
+	WHERE PStatus = 0;
+	SELECT @RoleId = RoleId
+	FROM Enterprise.Role AS R
+		 INNER JOIN
+		 Enterprise.RoleValueType AS RR
+		 ON RR.RoleValueTypeId = R.RoleValueTypeId
+	WHERE RR.Value = 'User Administrator' AND 
+		  R.PartyId = @PartyId;
+	DECLARE Rights CURSOR
+	FOR SELECT RightId, Name, Description, ShortName
+		FROM #RightsUnifiedSettings;
+	OPEN Rights;
+	FETCH Rights INTO @TRightId, @TRightName, @TRightDesc, @TRightShortName;
+	WHILE @@FETCH_STATUS = 0
+	BEGIN
+		EXECUTE Enterprise.CreateRight @RoleId = -1, @RightName = 'Default_CIMPLManagePII', @ShortName = 'CIMPLManagePII', @RightCategoryId = @RightCategory, @PartyId = @PartyId, @ProductId = @ProductId, @Description = '',  @TargetProductId = @TargetProductId, @VisibilityStatusId = @VisibilityStatusId, @RightId = @RightId OUTPUT;
+		EXECUTE Enterprise.CreateRight @RoleId = @RoleId, @RightName = @TRightName, @RightCategoryId = @RightCategory, @PartyId = @PartyId, @ProductId = @ProductId, @Shortname = @TRightShortName, @Description = @TRightDesc, @TargetProductId = @TargetProductId, @VisibilityStatusId = @VisibilityStatusId, @RightId = @RightId OUTPUT;
+
+
+		SELECT @ActionID = ActionID
+		FROM Enterprise.ACTION
+		WHERE ObjectValue = 'CIMPL Manage PII'  AND 
+			  ObjectType = 'Right' AND 
+			  ParentActionId IS NULL;
+		EXEC [Enterprise].[LinkActionToRights] @ActionID = @ActionID, @RightId = @RightId, @StatusId = @Status, @UserActionId = @UserActionId OUTPUT;
+
+		SELECT @RightId = RightId
+		FROM Enterprise.[Right] AS R
+			 INNER JOIN
+			 Enterprise.RightValueType AS RR
+			 ON RR.RightValueTypeId = R.RightValueTypeId
+		WHERE RR.Value = 'Default_CIMPLManagePII';
+		SELECT @ActionID = ActionID
+		FROM Enterprise.ACTION
+		WHERE ObjectValue = 'CIMPL Manage PII'   AND 
+			  ObjectType = 'Right' AND 
+			  ParentActionId IS NULL;
+		EXEC [Enterprise].[LinkActionToRights] @ActionID = @ActionID, @RightId = @RightId, @StatusId = @Status, @UserActionId = @UserActionId OUTPUT;
+
+
+
+		FETCH Rights INTO @TRightId, @TRightName, @TRightDesc, @TRightShortName;
+	END;
+	CLOSE Rights;
+	DEALLOCATE Rights;
+	UPDATE #HoldPartyForUnifiedSettings
+	  SET PStatus = 1
+	WHERE RowNumber = @PartyRowNum;
+END;
+
+DECLARE @Dashboard int;
+
+SELECT @DashBoard = RightValueTypeId
+FROM Enterprise.RightValueType
+WHERE value = 'Default_CIMPLManagePII';
+
+SELECT @RightValueTypeId = RightValueTypeId
+FROM Enterprise.RightValueType
+WHERE value IN( 'Manage Personally Identifiable Information (PII) in CIMPL' );
+
+IF NOT EXISTS
+(
+	SELECT 1
+	FROM Enterprise.RightDependency
+	WHERE RightValueTypeId = @RightValueTypeId AND 
+		  DependentRightValueTypeId = @DashBoard
+)
+BEGIN
+	INSERT INTO Enterprise.RightDependency( RightValueTypeId, DependentRightValueTypeId )
+	VALUES( @RightValueTypeId, @DashBoard );
+
+END;
+
+GO
+
