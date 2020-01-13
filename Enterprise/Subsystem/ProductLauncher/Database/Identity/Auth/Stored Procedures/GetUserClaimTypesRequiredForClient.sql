e07264bd8b07a72f46d07c560c08ea518dfd5774
Monte Jennings <monte.jennings@realpage.com>
2020-01-13 14:51:15 -0600

    Added "s" to pluralize Clients to make proc work

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Auth/Stored Procedures/GetUserClaimTypesRequiredForClient.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Auth/Stored Procedures/GetUserClaimTypesRequiredForClient.sql
@@ -9,7 +9,7 @@ BEGIN
 	SELECT CL.ClaimName
 		  ,CL.SAMLAttributeName
 		  ,CL.ProductId 
-	FROM Auth.Client C
+	FROM Auth.Clients C
 	INNER JOIN Auth.ClientUserClaim AS cc ON C.ClientId = CC.ClientId
 	INNER JOIN Auth.Claim CL ON CL.ClaimId = CC.ClaimId
 	WHERE C.ClientCode= @ClientName
