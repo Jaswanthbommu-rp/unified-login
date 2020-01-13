5a94c884d4d247df25e521f230c2b73978524496
Monte Jennings <monte.jennings@realpage.com>
2020-01-13 14:38:23 -0600

    Added Claim and ClientUserClaim and the proc GetUserClaimTypesRequiredForClient to retrieve the data

---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Auth/Stored Procedures/GetUserClaimTypesRequiredForClient.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [Auth].[GetUserClaimTypesRequiredForClient] (
+	 @ClientName VARCHAR(50) = 'unifiedAmenities'  -- input param
+)
+AS
+BEGIN
+	SET NOCOUNT ON; 
+
+	
+	SELECT CL.ClaimName
+		  ,CL.SAMLAttributeName
+		  ,CL.ProductId 
+	FROM Auth.Client C
+	INNER JOIN Auth.ClientUserClaim AS cc ON C.ClientId = CC.ClientId
+	INNER JOIN Auth.Claim CL ON CL.ClaimId = CC.ClaimId
+	WHERE C.ClientCode= @ClientName
+END
