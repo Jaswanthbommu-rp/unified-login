456c1d503c0c58c88a6a3c7ac49d5bfcaa0db212
James Reames <james.reames@realpage.com>
2020-03-19 10:56:57 -0500

    Update IdentityProviderSetting.sql
    altering identity provider setting value to nvarchar(max)

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Ident/Tables/IdentityProviderSetting.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Ident/Tables/IdentityProviderSetting.sql
@@ -2,7 +2,7 @@
 (
 	[IdentityProviderSettingId] INT NOT NULL IDENTITY,
 	[IdentityProviderSettingTypeId] INT NOT NULL,
-	[Value] NVARCHAR(255) NOT NULL,
+	[Value] NVARCHAR(MAX) NOT NULL,
     CONSTRAINT [PK_IdentityProviderSetting] PRIMARY KEY ([IdentityProviderSettingId]), 
     CONSTRAINT [FK_IdentityProviderSetting_IdentityProviderSettingType] FOREIGN KEY ([IdentityProviderSettingTypeId]) REFERENCES Ident.[IdentityProviderSettingType]([IdentityProviderSettingTypeId]) ON DELETE CASCADE ON UPDATE CASCADE
 )
