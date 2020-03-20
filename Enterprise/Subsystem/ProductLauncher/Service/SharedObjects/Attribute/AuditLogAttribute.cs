54d75f8a71aa458b54d96b9ae79c771eb0b5e8fc
Raul Bobadilla <raul.bobadilla@realpage.com>
2020-03-20 10:45:26 -0500

    110451 - Changes on logging attributes

--- Enterprise/Subsystem/ProductLauncher/Service/SharedObjects/Attribute/AuditLogAttribute.cs
+++ Enterprise/Subsystem/ProductLauncher/Service/SharedObjects/Attribute/AuditLogAttribute.cs
@@ -1,4 +1,6 @@
-﻿namespace RP.Enterprise.Subsystem.ProductLauncher.Component.SharedObjects.Attribute
+﻿using RP.Enterprise.Subsystem.ProductLauncher.Component.SharedObjects.Constants;
+
+namespace RP.Enterprise.Subsystem.ProductLauncher.Component.SharedObjects.Attribute
 {
     /// <summary>
     /// Attribute to audit properties in models
@@ -17,6 +19,16 @@
         /// </summary>
         public string NullBlankValue { get; private set; }
 
+        /// <summary>
+        /// Activity log value
+        /// </summary>
+        public string LogActivityTypeConstant { get; private set; }
+
+        /// <summary>
+        /// Property format
+        /// </summary>
+        public string Format { get; private set; }
+
         #endregion
 
         #region "Constructors"
@@ -25,11 +37,15 @@
         /// Class constructor
         /// </summary>
         /// <param name="columName">Column name</param>
+        /// <param name="format">Property format</param>
+        /// <param name="logActivityTypeConstant">Activity log value</param>
         /// <param name="nullBlankValue">Value for nulls or empty</param>
-        public AuditLogAttribute(string columName, string nullBlankValue = "a blank value")
+        public AuditLogAttribute(string columName, string logActivityTypeConstant, string format ="", string nullBlankValue = "a blank value")
         {
             ColumnName = columName;
             NullBlankValue = nullBlankValue;
+            LogActivityTypeConstant = logActivityTypeConstant;
+            Format = format;
         }
 
         #endregion 
