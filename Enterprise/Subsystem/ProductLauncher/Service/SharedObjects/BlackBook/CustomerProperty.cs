0625920488e9ba8e3c53ed4eee5824b078914127
Nabil Nabhan <nabil.nabhan@realpage.com>
2020-04-16 14:49:25 -0500

    Map the result to ProductProperty object.

--- Enterprise/Subsystem/ProductLauncher/Service/SharedObjects/BlackBook/CustomerProperty.cs
+++ Enterprise/Subsystem/ProductLauncher/Service/SharedObjects/BlackBook/CustomerProperty.cs
@@ -22,23 +22,8 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Component.SharedObjects.BlackB
 
 	public class CustomerProperty
 	{
-		//[JsonProperty(PropertyName = "Id", NullValueHandling = NullValueHandling.Ignore)]
 		[JsonIgnore]
 		public string id { get; set; }
-		[JsonProperty(PropertyName = "companyId", NullValueHandling = NullValueHandling.Ignore)]
-		public string companyId { get; set; }
-		[JsonProperty(PropertyName = "propertyId", NullValueHandling = NullValueHandling.Ignore)]
-		public string propertyId { get; set; }
-		[JsonProperty(PropertyName = "name", NullValueHandling = NullValueHandling.Ignore)]
-		public string name { get; set; }
-		[JsonProperty(PropertyName = "street", NullValueHandling = NullValueHandling.Ignore)]
-		public string street { get; set; }
-		[JsonProperty(PropertyName = "city", NullValueHandling = NullValueHandling.Ignore)]
-		public string city { get; set; }
-		[JsonProperty(PropertyName = "state", NullValueHandling = NullValueHandling.Ignore)]
-		public string state { get; set; }
-		[JsonProperty(PropertyName = "postalCode", NullValueHandling = NullValueHandling.Ignore)]
-		public string postalCode { get; set; }
 		[JsonProperty(PropertyName = "attributes", NullValueHandling = NullValueHandling.Ignore)]
 		public PropertyAttributes attributes { get; set; }
 	}
