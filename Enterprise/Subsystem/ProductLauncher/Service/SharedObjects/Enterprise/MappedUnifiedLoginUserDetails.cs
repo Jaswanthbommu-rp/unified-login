99c0fed09075da45f6af6132ffb9e492e164e6c4
Shishir Dubey <shishir.dubey@realpage.com>
2020-04-28 16:25:12 -0500

    Add code changes to support a new method for getting Unified Login Ids that map with Product User IDs.

---
+++ Enterprise/Subsystem/ProductLauncher/Service/SharedObjects/Enterprise/MappedUnifiedLoginUserDetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RP.Enterprise.Subsystem.ProductLauncher.Component.SharedObjects.Enterprise
+{
+    public class MappedUnifiedLoginUserDetails
+    {
+        public int CompanyId { get; set; }
+        public int ProductId { get; set; }
+        public List<ULMappedUserIds> ULMappedUserId { get; set; }
+    }
+
+    public class ULMappedUserIds
+    {
+        public int ProductUserId { get; set; }
+        public int UnifiedLoginUserId { get; set; }
+    }
+}
