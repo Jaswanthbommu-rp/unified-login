ca8bbf9aceefa3720fbc49a032d918768e47db74
Vijayasekher Manellore <Vijayasekher.Manellore@RealPage.com>
2020-04-27 21:59:10 -0500

    Merged PR 9681: Code Update
    Code Update
    
    Related work items: #212427

--- Enterprise/Subsystem/ProductLauncher/Web/landing-ui-src/v1/user/assign-product-access/unified-amenities/js/config/unified-amenities.js
+++ Enterprise/Subsystem/ProductLauncher/Web/landing-ui-src/v1/user/assign-product-access/unified-amenities/js/config/unified-amenities.js
@@ -3,14 +3,18 @@
 (function (angular) {
     "use strict";
 
-    function config(model, productAccess) {
-        productAccess.register({
-            model: model,
-            key: "soln107"
-        });
+    function config(model, productAccess, templateModel) {
+
+        if (!templateModel.isProductExists(26)) {
+            productAccess.register({
+                model: model,
+                key: "soln107",
+                product: "26"
+            });
+        }
     }
 
     angular
         .module("settings")
-        .run(["unifiedAmenitiesProductAccessModel", "assignProductAccessModel", config]);
+        .run(["unifiedAmenitiesProductAccessModel", "assignProductAccessModel", "productTemplateModel", config]);
 })(angular);
