9e70867b81a4749b6f5bd3ac576de04a476c20d8
James Reames <james.reames@realpage.com>
2020-05-01 14:33:39 -0500

    adding domain to procs

---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Enterprise/Stored Procedures/ListOrganizationDomain.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [Enterprise].[ListOrganizationDomain]
+AS
+BEGIN
+	SELECT 
+		OrganizationDomainId,
+		Name
+	FROM
+		Enterprise.OrganizationDomain
+	WHERE
+		ThruDate IS NULL 
+		OR 
+		Thrudate > GETUTCDATE()
+END
+
