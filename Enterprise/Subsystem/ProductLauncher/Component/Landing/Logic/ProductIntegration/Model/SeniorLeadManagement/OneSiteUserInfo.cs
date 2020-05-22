9615f5176a7af4e3cdbdd306cea972d233f667a8
Cristian Gutierrez <cristian.gutierrez@realpage.com>
2020-05-21 20:46:34 -0500

    New OneSiteUserInfo class added

---
+++ Enterprise/Subsystem/ProductLauncher/Component/Landing/Logic/ProductIntegration/Model/SeniorLeadManagement/OneSiteUserInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace RP.Enterprise.Subsystem.ProductLauncher.Component.Landing.Logic.ProductIntegration.Model.SeniorLeadManagement
+{
+    /// <summary>
+    /// Model for One site user info
+    /// </summary>
+    public sealed class OneSiteUserInfo
+    {
+        #region "Properties"
+
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public int LastName { get; set; }
+        public List<string> Properties { get; set; }
+
+        #endregion
+    }
+}
