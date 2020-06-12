e8798ef5c10eb36471d4cfae3b10d04163174604
Vijayasekher Manellore <vijayasekher.manellore@realpage.com>
2020-06-12 10:22:46 -0500

    Update Setup super user for upfm

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Enterprise/Stored Procedures/SetupSuperUser.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Enterprise/Stored Procedures/SetupSuperUser.sql
@@ -1473,6 +1473,7 @@ AS
                              @StatusId = @Status_Right, 
                              @UserActionId = @UserActionID OUTPUT;
                 END;
+
                 SET @RoleId = NULL;
                 SET @PersonaId = NULL;
                 SELECT @RoleId = R.RoleID
@@ -1480,13 +1481,16 @@ AS
                      INNER JOIN Enterprise.RoleValueType RVT ON RVT.RoleValueTypeId = R.RoleValueTypeId
                 WHERE RVT.Value = 'User Administrator'
                       AND R.PartyID = @OrganizationId;
+
                 SELECT @PersonaId = P.PersonaId
                 FROM Ident.UserLogin UL
                      INNER JOIN Ident.UserLoginPersona ULP ON ULP.UserLoginId = UL.UserId
                      INNER JOIN Person.Persona P ON P.UserLoginPersonaId = ULP.UserLoginPersonaId--P.UserId = UL.UserId
                 WHERE UL.LoginName = @Email;
+
                 SELECT @PersonaId, 
                        @RoleId;
+
                 IF NOT EXISTS
                 (
                     SELECT 1
@@ -1500,6 +1504,31 @@ AS
                              @RoleID = @RoleId, 
                              @PersonaPrivilgeID = @PerPriv OUTPUT;
                 END;
+				--INSERT all properties indicator for UPFM
+				IF NOT EXISTS
+				(
+					SELECT 1
+					FROM Enterprise.PropertyMapping
+					WHERE PersonaId = @PersonaId
+					AND ProductId = 3
+				)
+				BEGIN
+					INSERT INTO Enterprise.PropertyMapping (
+						PersonaId,
+						PropertyId,
+						ProductId,
+						FromDate,
+						ThruDate
+					)
+					VALUES (
+						@PersonaId,
+						-1,
+						3,
+						@NOW,
+						NULL
+					)
+				END
+
                 UPDATE #HoldOrgs
                   SET 
                       PStatus = 1
