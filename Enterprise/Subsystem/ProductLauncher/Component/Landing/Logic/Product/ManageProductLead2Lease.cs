65961f43e163bb441d53e8d0b09fb191fa990c36
Cristian Gutierrez <cristian.gutierrez@realpage.com>
2020-07-06 17:50:29 -0500

    try/catch statement added in GetMigrationUsers of ManageProductLead2Lease class

--- Enterprise/Subsystem/ProductLauncher/Component/Landing/Logic/Product/ManageProductLead2Lease.cs
+++ Enterprise/Subsystem/ProductLauncher/Component/Landing/Logic/Product/ManageProductLead2Lease.cs
@@ -828,50 +828,64 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Component.Landing.Logic.Produc
                 IsError = true,
                 ErrorReason = "No Users."
             };
-            var claimResposnse = base.GetCompanyEditorAndUserDetails(editorPersonaId, 0);
-            if (claimResposnse.IsError) { response.ErrorReason = claimResposnse.ErrorReason; return response; }
 
-            int companyInstanceSourceId = Convert.ToInt32(GetProductCompanyInstanceId(BlueBookProductConstants.Lead2Lease).CompanyInstanceSourceId);
-            if (companyInstanceSourceId == 0)
+            try
             {
-                WriteToErrorLog(
-                    $"ManageProductLead2Lease.GetMigrationUsers.GetProductCompanyInstanceId - Error looking for company id in bluebook for user with editorPersona id - {editorPersonaId}.");
-                response.ErrorReason = "Company Setup Error: Please Contact Support.";
-                return response;
-            }
-            var filter = "NonMigrated";
-            var startRow = 0;
-            var resultPerRow = 1000;
-            if (datafilter != null)
-            {
-                if (datafilter.FilterBy.ContainsKey("filter"))
+                var claimResposnse = base.GetCompanyEditorAndUserDetails(editorPersonaId, 0);
+                if (claimResposnse.IsError) { response.ErrorReason = claimResposnse.ErrorReason; return response; }
+
+                int companyInstanceSourceId = Convert.ToInt32(GetProductCompanyInstanceId(BlueBookProductConstants.Lead2Lease).CompanyInstanceSourceId);
+                if (companyInstanceSourceId == 0)
                 {
-                    filter = datafilter.FilterBy["filter"];
+                    WriteToErrorLog(
+                        $"ManageProductLead2Lease.GetMigrationUsers.GetProductCompanyInstanceId - Error looking for company id in bluebook for user with editorPersona id - {editorPersonaId}.");
+                    response.ErrorReason = "Company Setup Error: Please Contact Support.";
+                    return response;
                 }
-                if (datafilter.Pages != null)
+                var filter = "NonMigrated";
+                var startRow = 0;
+                var resultPerRow = 1000;
+                if (datafilter != null)
                 {
-                    startRow = datafilter.Pages.StartRow;
-                    resultPerRow = datafilter.Pages.ResultsPerPage;
+                    if (datafilter.FilterBy.ContainsKey("filter"))
+                    {
+                        filter = datafilter.FilterBy["filter"];
+                    }
+                    if (datafilter.Pages != null)
+                    {
+                        startRow = datafilter.Pages.StartRow;
+                        resultPerRow = datafilter.Pages.ResultsPerPage;
+                    }
                 }
+
+                var url = $"{_mtApiEndPoint}/{companyInstanceSourceId}/users?filter={filter}&startRow={startRow}&resultsperpage={resultPerRow}";
+                WriteToDiagnosticLog("ManageProductLead2Lease.GetMigrationUsers", new Dictionary<string, object> { { "Url", url } });
+
+                var allUsers = GetResultFromApi<IList<MigrationUser>>(url);
+
+                if (allUsers == null)
+                {
+                    WriteToErrorLog($"ManageProductLead2Lease.GetMigrationUsers-no users received from product for user with editorPersona id - {editorPersonaId}.");
+                    return response;
+                }
+                WriteToDiagnosticLog($"ManageProductLead2Lease.GetUsers - Received users from product for user with editorPersona id - {editorPersonaId}.");
+                response.RowsPerPage = resultPerRow;
+                response.ErrorReason = string.Empty;
+                response.IsError = false;
+                response.TotalPages = 1;
+                response.Records = allUsers.Cast<object>().ToList();
+                response.TotalRows = allUsers.Count();
             }
-
-            var url = $"{_mtApiEndPoint}/{companyInstanceSourceId}/users?filter={filter}&startRow={startRow}&resultsperpage={resultPerRow}";
-            WriteToDiagnosticLog("ManageProductLead2Lease.GetMigrationUsers", new Dictionary<string, object> { { "Url", url } });
-
-            var allUsers = GetResultFromApi<IList<MigrationUser>>(url);
-
-            if (allUsers == null)
+            catch (Exception ex)
             {
-                WriteToErrorLog($"ManageProductLead2Lease.GetMigrationUsers-no users received from product for user with editorPersona id - {editorPersonaId}.");
-                return response;
+                response = new ListResponse
+                { 
+                    IsError = true,
+                    ErrorReason = ex.Message
+                };
+
+                WriteToErrorLog($"ManageProductLead2Lease.GetMigrationUsers Error for user with editorPersona id - {editorPersonaId} ", exception: ex);
             }
-            WriteToDiagnosticLog($"ManageProductLead2Lease.GetUsers - Received users from product for user with editorPersona id - {editorPersonaId}.");
-            response.RowsPerPage = resultPerRow;
-            response.ErrorReason = string.Empty;
-            response.IsError = false;
-            response.TotalPages = 1;
-            response.Records = allUsers.Cast<object>().ToList();
-            response.TotalRows = allUsers.Count();
             return response;
         }
 
