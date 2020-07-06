311b98b85a528de2d056a9f239e81e195b7e92e7
Cristian Gutierrez <cristian.gutierrez@realpage.com>
2020-07-06 14:38:58 -0500

    Generic PropertyGroupErrorMessage implemented in GetPropertyGroups  of ManageProductVendorServices class

--- Enterprise/Subsystem/ProductLauncher/Component/Landing/Logic/Product/ManageProductVendorServices.cs
+++ Enterprise/Subsystem/ProductLauncher/Component/Landing/Logic/Product/ManageProductVendorServices.cs
@@ -177,8 +177,20 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Component.Landing.Logic.Produc
             }
             catch (Exception ex)
             {
-                response.IsError = true;
-                response.ErrorReason = "There was a problem getting the property groups.";
+                response = new ListResponse
+                {
+                    IsError = true
+                };
+
+                if (ex is BlueBookException)
+                {
+                    response.ErrorReason = ex.Message;
+                }
+                else
+                {
+                    response.ErrorReason = CommonMessageConstants.PropertyGroupErrorMessage;
+                }
+                
                 WriteToErrorLog($"ManageProductVendorServices.GetPropertyGroups - error for user with editorPersona id - {editorPersonaId} - {response.ErrorReason}", exception: ex);
             }
 
