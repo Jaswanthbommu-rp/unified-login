6476b3c6b072893ce1de1a3732da69bacaf85aa0
Cristian Gutierrez <cristian.gutierrez@realpage.com>
2020-07-09 15:21:32 -0500

    new region error messages added

--- Enterprise/Subsystem/ProductLauncher/Service/SharedObjects/Constants/CommonMessageConstants.cs
+++ Enterprise/Subsystem/ProductLauncher/Service/SharedObjects/Constants/CommonMessageConstants.cs
@@ -46,7 +46,16 @@
         /// CompanyErrorMessage
         /// </summary>
         public const string CompanyErrorMessage = "This product is not yet implemented.  The product access page will be available when your implementation is complete. Thank you for choosing RealPage.";
-        
+
+        #endregion
+
+        #region Regions
+
+        /// <summary>
+        /// RegionErrorMessage
+        /// </summary>
+        public const string RegionErrorMessage = "There was a problem getting the list of regions.  If the problem persists, please contact support.";
+
         #endregion
 
         #endregion
