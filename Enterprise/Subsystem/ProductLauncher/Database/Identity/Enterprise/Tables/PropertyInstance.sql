06a7f66d9db776e48f626bc8a2986e32ccec8dc7
James Reames <james.reames@realpage.com>
2020-07-15 09:13:09 -0500

    new tables for property instances

---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Enterprise/Tables/PropertyInstance.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [Enterprise].[PropertyInstance]
+(
+	[PropertyInstanceId] INT NOT NULL PRIMARY KEY, 
+	[Name] NVARCHAR(250) NOT NULL,
+    [Address] NVARCHAR(250) NOT NULL,
+	[City] NVARCHAR(250) NOT NULL,
+	[State] NVARCHAR(150) NULL,
+	[PostalCode] NVARCHAR(50) NOT NULL,
+	[Country] NVARCHAR(250) NOT NULL,
+	[County] NVARCHAR(250) NULL,
+	[Latitude] NVARCHAR(100) NULL,
+	[Longitude] NVARCHAR(100) NULL, 
+    [InstanceId] UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID(),
+
+)
---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Enterprise/Tables/PropertyInstanceMapping.sql
@@ -0,0 +1,23 @@
+﻿
+CREATE TABLE [Enterprise].[PropertyInstanceMapping](
+	[PropertyInstanceMappingID] [bigint] IDENTITY(1,1) NOT NULL,
+	[PersonaId] [bigint] NOT NULL,
+	[PropertyInstanceId] [int] NOT NULL,
+	[ProductId] [int] NOT NULL,
+	[FromDate] [datetime] NOT NULL DEFAULT GETUTCDATE(),
+	[ThruDate] [datetime] NULL,
+	[Active] BIT NOT NULL DEFAULT 1
+
+ CONSTRAINT [PK_PropertyInstanceMapping] PRIMARY KEY CLUSTERED
+ (
+	[PropertyInstanceMappingID] ASC
+),
+ CONSTRAINT [FK_PropertyInstanceMapping_PropertyInstance] FOREIGN KEY([PropertyInstanceId]) REFERENCES [Enterprise].[PropertyInstance] ([PropertyInstanceId]) ,
+    CONSTRAINT [FK_PropertyInstanceMapping_Persona] FOREIGN KEY([PersonaId]) REFERENCES [Person].[Persona] ([PersonaId]) ,
+ CONSTRAINT [FK_PropertyInstanceMapping_Product] FOREIGN KEY([ProductId]) REFERENCES [Enterprise].[Product] ([ProductId]),
+) ON [PRIMARY]
+GO
+
+
+
+
