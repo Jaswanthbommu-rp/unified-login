8765da0f842e50064bbb2ec3bba216d1fe434b17
James Reames <james.reames@realpage.com>
2020-07-15 14:58:56 -0500

    changed schema

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Enterprise/Tables/PropertyInstance.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Enterprise/Tables/PropertyInstance.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [Enterprise].[PropertyInstance]
 (
-	[PropertyInstanceId] INT NOT NULL PRIMARY KEY, 
+	[PropertyInstanceId] [BIGINT] NOT NULL IDENTITY, 
 	[Name] NVARCHAR(250) NOT NULL,
     [Address] NVARCHAR(250) NOT NULL,
 	[City] NVARCHAR(250) NOT NULL,
@@ -10,6 +10,6 @@
 	[County] NVARCHAR(250) NULL,
 	[Latitude] NVARCHAR(100) NULL,
 	[Longitude] NVARCHAR(100) NULL, 
-    [InstanceId] UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID(),
-
+    [InstanceId] UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID(), 
+    CONSTRAINT [PK_PropertyInstance] PRIMARY KEY ([PropertyInstanceId]),
 )
