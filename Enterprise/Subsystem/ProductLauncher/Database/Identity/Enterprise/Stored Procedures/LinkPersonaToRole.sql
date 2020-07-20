fe26d15ba3ac2f90957efc90c151f6767b3399ba
Vijayasekher Manellore <Vijayasekher.Manellore@RealPage.com>
2020-07-19 23:04:56 -0500

    Merged PR 23297: 375667 Unable to find Home button in left side panel

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Enterprise/Stored Procedures/LinkPersonaToRole.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Enterprise/Stored Procedures/LinkPersonaToRole.sql
@@ -7,7 +7,29 @@
 )
 AS
      BEGIN
-         DECLARE @ErrorLogID INT;
+        DECLARE @ErrorLogID INT;
+		 DECLARE @SchemaName varchar(25);
+		 Declare @OrgPartyId INT;
+		 Declare @SecurityRoleId INT
+		 DECLARE @Rolename varchar(256)
+		
+		Select @Rolename = RV.Value, @OrgPartyId = R.PartyID 
+		From Enterprise.Role R
+		Join Enterprise.RoleValueType RV ON
+			RV.RoleValueTypeId = R.RoleValueTypeId
+		Where RoleId = @RoleId
+
+		SELECT	@SchemaName = ps.Value				
+		FROM	Enterprise.GlobalProductConfiguration gpc
+				JOIN Enterprise.ProductConfiguration pc ON pc.ConfigurationId = gpc.ConfigurationId
+				JOIN Enterprise.ProductSetting ps ON ps.ProductSettingId = pc.ProductSettingId
+				JOIN Enterprise.ProductSettingType pst ON pst.ProductSettingTypeId = ps.ProductSettingTypeId
+		WHERE  gpc.ProductId = 3
+		AND (gpc.ThruDate IS NULL)
+		AND ( pc.ThruDate IS NULL)
+		AND ( ps.ThruDate IS NULL)
+		And PST.Name = 'RolesRightsSchemaName'
+
          IF @IsDeleted = 0
              BEGIN
                  IF NOT EXISTS
@@ -56,7 +78,7 @@ AS
                      WHERE PersonaId = @PersonaID
                            AND RoleID = @RoleID
                  )
-                     BEGIN
+                 BEGIN
                          DECLARE @DeletedPersonaPrivilegeId TABLE(PersonaPrivilegeId INT);
                  END;
                  BEGIN TRY
@@ -68,7 +90,7 @@ AS
                      SELECT PersonaPrivilegeId AS 'Id',
                             '' AS ErrorMessage
                      FROM @DeletedPersonaPrivilegeId;
-         END TRY
+				END TRY
                  BEGIN CATCH
                      EXEC dbo.LogError
                           @ErrorLogID = @ErrorLogID OUTPUT;
@@ -76,7 +98,17 @@ AS
                             ErrorMessage
                      FROM dbo.ErrorLog
                      WHERE ErrorLogID = @ErrorLogID;
-         END CATCH;
+             END CATCH;
          END;
+		 --link persona to role in security schema 
+		IF (@SchemaName = 'Enterprise')
+		BEGIN
+			Declare @PersonaRoleID INT 
+			Select @SecurityRoleId = R.RoleID from Security.Role R 							
+			Where R.RoleName = @Rolename
+			AND R.OrgPartyID = @OrgPartyId
+
+			EXEC [Security].[LinkPersonaToRole] @PersonaId,@SecurityRoleId,@IsDeleted , @CreatedBy,@PersonaRoleID OUTPUT
+		END
      END;
 
