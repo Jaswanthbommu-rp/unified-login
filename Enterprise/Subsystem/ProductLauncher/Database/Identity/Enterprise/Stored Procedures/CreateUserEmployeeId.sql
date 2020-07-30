2fb1582185bc693c381737a5f500b803c0073095
Vivek Vishal <vivek.vishal@realpage.com>
2020-07-30 21:27:15 +0530

    398826-Add new employee id schema to store the employee per company

---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Enterprise/Stored Procedures/CreateUserEmployeeId.sql
@@ -0,0 +1,31 @@
+﻿CREATE PROCEDURE [Enterprise].[CreateUserEmployeeId]
+	 @UserLoginPersonaId BIGINT = NULL,
+	 @EmployeeId NVARCHAR(40) = NULL
+AS
+    BEGIN
+        BEGIN TRY
+            BEGIN TRAN;
+			INSERT INTO Enterprise.UserEmployeeId (
+                UserLoginPersonaId ,
+                Employee
+				)
+			OUTPUT Inserted.UserEmployeeId AS Id, '' AS ErrorMessage
+			VALUES (
+                     @UserLoginPersonaId ,
+					 @EmployeeId
+                 );
+            COMMIT;
+        END TRY
+        BEGIN CATCH
+            DECLARE @ErrorLogID INT;
+        EXEC dbo.LogError
+			@ErrorLogID = @ErrorLogID OUTPUT;
+
+            SELECT 0 AS Id ,
+                   ErrorMessage
+            FROM   dbo.ErrorLog
+            WHERE  ErrorLogID = @ErrorLogID;
+
+            ROLLBACK;
+        END CATCH;
+    END;
---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Enterprise/Stored Procedures/UpdateUserEmployeeId.sql
@@ -0,0 +1,31 @@
+﻿CREATE PROCEDURE [Enterprise].[UpdateUserEmployeeId](
+	 @UserEmployeeId INT,
+	 @UserLoginPersonaId BIGINT = NULL,
+	 @EmployeeId NVARCHAR(40) = NULL
+	) 
+AS
+BEGIN
+	SET NOCOUNT ON;
+	 BEGIN TRY
+            BEGIN TRAN;
+				UPDATE [Enterprise].[UserEmployeeId]
+				   SET 
+					   Employee = @EmployeeId
+					WHERE
+						UserEmployeeId = @UserEmployeeId
+
+				SELECT @UserEmployeeId as Id, '' AS ErrorMessage
+			COMMIT;
+        END TRY
+        BEGIN CATCH
+            DECLARE @ErrorLogID INT;
+            EXEC dbo.LogError @ErrorLogID = @ErrorLogID OUTPUT;
+
+            SELECT 0 AS Id ,
+                   ErrorMessage
+            FROM   dbo.ErrorLog
+            WHERE  ErrorLogID = @ErrorLogID;
+
+            ROLLBACK;
+        END CATCH;
+END;
