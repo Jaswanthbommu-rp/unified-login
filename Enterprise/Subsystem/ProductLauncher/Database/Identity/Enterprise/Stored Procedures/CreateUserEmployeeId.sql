b001b6bdf5ab3b9a64e721486be0be93e3a16355
Vivek Vishal <vivek.vishal@RealPage.com>
2020-07-30 12:40:57 -0500

    Merged PR 25329: 398826-Add new employee id schema to store the employee per company
    398826-Add new employee id schema to store the employee per company

---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Enterprise/Stored Procedures/CreateUserEmployeeId.sql
@@ -0,0 +1,27 @@
+﻿CREATE PROCEDURE [Enterprise].[CreateUserEmployeeId]
+	 @UserLoginPersonaId BIGINT = NULL,
+	 @EmployeeId NVARCHAR(40) = NULL
+AS
+    BEGIN
+        BEGIN TRY
+			INSERT INTO Enterprise.UserEmployeeId (
+                UserLoginPersonaId ,
+                Employee
+				)
+			OUTPUT Inserted.UserEmployeeId AS Id, '' AS ErrorMessage
+			VALUES (
+                     @UserLoginPersonaId ,
+					 @EmployeeId
+                 );
+        END TRY
+        BEGIN CATCH
+            DECLARE @ErrorLogID INT;
+        EXEC dbo.LogError
+			@ErrorLogID = @ErrorLogID OUTPUT;
+
+            SELECT 0 AS Id ,
+                   ErrorMessage
+            FROM   dbo.ErrorLog
+            WHERE  ErrorLogID = @ErrorLogID;
+        END CATCH;
+    END;
