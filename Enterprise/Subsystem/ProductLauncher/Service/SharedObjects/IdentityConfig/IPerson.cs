4d9dd28f9daf50444b1e165034a5eb06c4a2ae9f
Vivek Vishal <vivek.vishal@realpage.com>
2020-08-05 12:21:16 +0530

    reversed employeeid form persona.cs

--- Enterprise/Subsystem/ProductLauncher/Service/SharedObjects/IdentityConfig/IPerson.cs
+++ Enterprise/Subsystem/ProductLauncher/Service/SharedObjects/IdentityConfig/IPerson.cs
@@ -18,11 +18,6 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Component.SharedObjects.Identi
 		string LastName { get; set; }
 
 		/// <summary>
-		/// Lastname
-		/// </summary>
-		string EmployeeId { get; set; }
-
-		/// <summary>
 		/// Middle initial
 		/// </summary>
 		string MiddleName { get; set; }
