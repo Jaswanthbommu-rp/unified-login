d66a71764867a8bd9b8217327d83421d9e1cd9c4
Vivek Vishal <vivek.vishal@realpage.com>
2020-08-06 14:00:13 +0530

    EmployeeId related api changes

--- Enterprise/Subsystem/ProductLauncher/Component/Landing/Logic/Product/ManageProductOps.cs
+++ Enterprise/Subsystem/ProductLauncher/Component/Landing/Logic/Product/ManageProductOps.cs
@@ -613,6 +613,11 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Component.Landing.Logic.Produc
 
             var userLogin = _manageUserLogin.GetUserLoginOnly(realPageId);
 
+            IList<UserLoginPersona> userLoginPersonaList = _userLoginPersonaRepository.ListUserLoginPersona(userLoginPersonaId: null, userLoginId: userPersona.UserId, organizationPartyId: userPersona.Organization.PartyId);
+            var employeeId = _userRepository.GetUserEmployeeId(userLoginPersonaList[0].UserLoginPersonaId, userPersona.OrganizationPartyId);
+            person.EmployeeId = (employeeId != null && !string.IsNullOrEmpty(employeeId.EmployeeId)) ? employeeId.EmployeeId : null;
+
+
             IManageContactMechanism contactMechanismLogic = new ManageContactMechanism();
             IList<CommonAddress> contactMechansimList = contactMechanismLogic.ListContactMechanismForPerson(realPageId, null);
 
@@ -773,6 +778,7 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Component.Landing.Logic.Produc
                 FirstName = person.FirstName,
                 MiddleName = person.MiddleName,
                 LastName = person.LastName,
+                EmployeeId = person.EmployeeId,
                 Loginname = _productUsername,
                 Password = Membership.GeneratePassword(15, 5),
                 RoleName = roleName,
@@ -911,6 +917,11 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Component.Landing.Logic.Produc
                 var realPageId = persona.RealPageId;
                 var person = _managePerson.GetPerson(realPageId);
                 var userLogin = _manageUserLogin.GetUserLoginOnly(realPageId);
+
+                IList<UserLoginPersona> userLoginPersonaList = _userLoginPersonaRepository.ListUserLoginPersona(userLoginPersonaId: null, userLoginId: persona.UserId, organizationPartyId: persona.Organization.PartyId);
+                var employeeId = _userRepository.GetUserEmployeeId(userLoginPersonaList[0].UserLoginPersonaId, persona.OrganizationPartyId);
+                person.EmployeeId = (employeeId != null && !string.IsNullOrEmpty(employeeId.EmployeeId)) ? employeeId.EmployeeId : null;
+
                 IManageContactMechanism contactMechanismLogic = new ManageContactMechanism();
                 IList<CommonAddress> contactMechansimList = contactMechanismLogic.ListContactMechanismForPerson(realPageId, null);
                 // get the email address
@@ -932,6 +943,7 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Component.Landing.Logic.Produc
                     FirstName = person.FirstName,
                     MiddleName = person.MiddleName,
                     LastName = person.LastName,
+                    EmployeeId = person.EmployeeId,
                     Loginname = _productUsername,
                     Email = userEmailAddress,
                     Status = "active"
