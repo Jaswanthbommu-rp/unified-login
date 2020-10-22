5386e814fe64c535a22be4401101088fc3e35815
Vivek Vishal <vivek.vishal@RealPage.com>
2020-10-22 13:45:48 -0500

    Merged PR 41446: 212588,216015- Add Employee ID as a standard field to the User Details page
    212588,216015- Add Employee ID as a standard field to the User Details page

--- Enterprise/Subsystem/ProductLauncher/Service/SharedObjects/IdentityConfig/IProfileDetail.cs
+++ Enterprise/Subsystem/ProductLauncher/Service/SharedObjects/IdentityConfig/IProfileDetail.cs
@@ -121,11 +121,6 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Component.SharedObjects.Identi
 		/// <summary>
 		/// EmployeeId
 		/// </summary>
-		string EmployeeId { get; set; }
-
-		/// <summary>
-		/// EmployeeId
-		/// </summary>
 		int UserEmployeeId { get; set; }
 	}
 }
