ca5a9f2336ac2c978bd801ffcdcc5081d4a28027
Cristian Gutierrez <cristian.gutierrez@realpage.com>
2020-10-27 17:09:36 -0500

    Defect fixed, new RealPageId and UserType fields added

--- Enterprise/Subsystem/ProductLauncher/Service/SharedObjects/Enterprise/ProductDetailCommon.cs
+++ Enterprise/Subsystem/ProductLauncher/Service/SharedObjects/Enterprise/ProductDetailCommon.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using System;
 
 namespace RP.Enterprise.Subsystem.ProductLauncher.Component.SharedObjects.Enterprise
 {
@@ -21,5 +22,17 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Component.SharedObjects.Enterp
         /// </summary>
         [JsonProperty("Company", NullValueHandling = NullValueHandling.Ignore)]
         public string Company { get; set; }
+
+        /// <summary>
+        /// RealPageId
+        /// </summary>
+        [JsonProperty("RealPageId", NullValueHandling = NullValueHandling.Ignore)]
+        public Guid? RealPageId { get; set; }
+
+        /// <summary>
+        /// RealPageId
+        /// </summary>
+        [JsonProperty("UserType", NullValueHandling = NullValueHandling.Ignore)]
+        public string UserType { get; set; }
     }
 }
