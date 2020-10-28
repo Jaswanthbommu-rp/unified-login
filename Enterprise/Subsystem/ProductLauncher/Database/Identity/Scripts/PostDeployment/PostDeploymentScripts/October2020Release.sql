a446aea3e1971226d3d03754e0a17ffde53d7307
Vivek Vishal <vivek.vishal@realpage.com>
2020-10-28 17:38:41 +0530

    549229- Renovation Manager: Update info tile icon text details

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/October2020Release.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/October2020Release.sql
@@ -259,3 +259,28 @@ BEGIN
 	(@RightId, @PartyId, 9, @CreatedById, @Now)
 END
 GO
+
+--Updating Renovation Manager Description
+DECLARE @ProductSettingTypeId int,
+		@ProductId int= 55;
+		
+IF EXISTS (SELECT 1 FROM Enterprise.Product  WHERE ProductId = @ProductId)
+BEGIN
+   SELECT @ProductSettingTypeId=ProductSettingTypeId 
+   FROM Enterprise.ProductSettingType WHERE [Name] = 'LearnMore'
+
+	UPDATE Enterprise.Product
+	SET [Description]='Renovation Manager is a tool that assists users in reducing vacancy loss, ensuring the asset is executing the plan as expected and allows for comprehensive analysis of capital performance with executed rent, expense and ROI comparisons against budget, prior lease and un-renovated market rent'
+    WHERE ProductId=@ProductId
+
+	--Removing LearnMore for Renovation Manger
+	IF EXISTS (select 1 from Enterprise.ProductSetting where ProductId=@ProductId and ProductSettingTypeId=@ProductSettingTypeId AND ThruDate IS NULL)
+	BEGIN
+		UPDATE Enterprise.ProductSetting 
+		SET ThruDate='2020-10-27 20:23:29.250' 
+		WHERE ProductId=@ProductId and ProductSettingTypeId=@ProductSettingTypeId
+	END
+
+
+END
+GO
