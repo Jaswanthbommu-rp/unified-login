cc137fd6fd1cebb82f0672a82eeaacea143016f9
KalyanaChakravarthy Bingi <kalyanachakravarthy.bingi@RealPage.com>
2020-12-01 03:03:44 -0600

    Merged PR 48508: 606568: Unable to clone roles issue fix
    606568: Unable to clone roles issue fix. Added SP into Security Schema

---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/RoleRightsManagement/Stored Procedures/ListSecurityStatus.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [Security].[ListSecurityStatus]
+AS
+   BEGIN
+        SELECT STCT.Name 'Category' , STC.Name 'CategoryType', ST.Name 'Status', ST.StatusTypeId
+		FROM Enterprise.StatusTypeCategoryType STCT
+			JOIN Enterprise.StatusTypeCategory  STC
+				ON STC.StatusTypeCategoryTypeId = STCT.StatusTypeCategoryTypeId
+			JOIN Enterprise.StatusTypeCategoryClassification  STCC
+				ON STCC.StatusTypeCategoryId = STC.StatusTypeCategoryId
+			JOIN Enterprise.StatusType ST
+				ON ST.StatusTypeId = STCC.StatusTypeId
+		WHERE STCT.name = 'Security'    
+   END;
