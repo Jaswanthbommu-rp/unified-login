af62d7fc2d017cabac5b0494fc0834157b5a8027
Vivek Vishal <vivek.vishal@realpage.com>
2020-12-09 15:04:01 +0530

    added creator role for realpage system admin

--- Enterprise/Subsystem/ProductLauncher/Component/Landing/Logic/Product/ManageUPFMProductsIntegration.cs
+++ Enterprise/Subsystem/ProductLauncher/Component/Landing/Logic/Product/ManageUPFMProductsIntegration.cs
@@ -564,6 +564,10 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Component.Landing.Logic.Produc
 					{
 						superUserRoleId = gbAllRoles.First(a => a.Name.Equals("Property Admin", StringComparison.OrdinalIgnoreCase)).ID;
 					}
+					else if(_productId == (int)ProductEnum.HOTS)
+                    {
+						superUserRoleId = gbAllRoles.First(a => a.Name.Equals("Creator", StringComparison.OrdinalIgnoreCase)).ID;
+					}
 					else
 					{
 						superUserRoleId = gbAllRoles.First(a => a.Name.Equals("Portfolio Manager", StringComparison.OrdinalIgnoreCase)).ID;
