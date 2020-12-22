d00b7d2dbce2adb8a6969e6f59ed69833c67ee77
Vivek Vishal <vivek.vishal@realpage.com>
2020-12-22 20:01:46 +0530

    Bug 633657: PME-199997/UDR, INC/User data refresh failing on OneSite UL DB

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Enterprise/Tables/UserEmployeeId.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Enterprise/Tables/UserEmployeeId.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [Enterprise].[UserEmployeeId] (
     [UserEmployeeId] INT   IDENTITY (1, 1) NOT NULL,
     [UserLoginPersonaId] BIGINT  NOT NULL,
-    [Employee] NVARCHAR(40),
+    [Employee] NVARCHAR(200),
     CONSTRAINT [PK_UserEmployee_UserEmployeeId] PRIMARY KEY CLUSTERED ([UserEmployeeId] ASC) WITH (FILLFACTOR = 80),
 	CONSTRAINT [FK_UserEmployeeId_UserLoginPersona] FOREIGN KEY (UserLoginPersonaId) REFERENCES [Ident].[UserLoginPersona]([UserLoginPersonaId])
 );
