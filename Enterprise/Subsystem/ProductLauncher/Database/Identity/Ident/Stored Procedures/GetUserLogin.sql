981a34f7488996159c6a06cf4d39e231125e7081
Jaswanth Bommu <Jaswanth.Bommu@RealPage.com>
2021-01-09 00:39:43 -0600

    Merged PR 54170: Update GetUserLogin.sql
    Update GetUserLogin.sql
    
    Cherry picked from !54092
    
    Cherry picked from !54102

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Ident/Stored Procedures/GetUserLogin.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Ident/Stored Procedures/GetUserLogin.sql
@@ -4,64 +4,40 @@
 )
 AS
      BEGIN
-         IF @RealPageId IS NOT NULL
-             BEGIN
-                 SELECT DISTINCT
-                        ul.UserId,
-                        ul.PersonPartyId AS PartyId,
-                        ul.[LoginName],
-                        p.RealPageId,
-                        ULP.StatusTypeId AS StatusId,
-                        ul.PasswordModifiedDate,
-                        ul.PasswordHash,
-                        ul.PasswordSalt,
-                        ULP.FromDate,
-                        ULP.ThruDate,
-                        ULP.StatusThruDate,
-                        ul.LastLoginDate 'LastLogin',
-                        MS.Value 'TimeZoneOffset'
-                 FROM Ident.UserLogin ul
-                      INNER JOIN Ident.UserLoginPersona ULP ON ULP.UserLoginId = Ul.UserId
-                      JOIN Enterprise.Party p ON p.PartyId = ul.PersonPartyId
-                      INNER JOIN Enterprise.MasterConfiguration MC ON MC.AttributeId = UL.UserId
-                      INNER JOIN Enterprise.MasterConfigurationSetting MCS ON MC.MasterConfigurationId = MCS.MasterConfigurationId
-                      INNER JOIN Enterprise.MasterSetting MS ON MCS.MasterSettingId = MS.MasterSettingId
-                      INNER JOIN Enterprise.MasterSettingType MST ON MST.MasterSettingTypeId = MS.MasterSettingTypeId
-                      INNER JOIN Enterprise.MasterConfigurationType MCT ON MCT.MasterConfigurationTypeId = MST.MasterConfigurationTypeId
-                 WHERE(p.RealPageId = @RealPageId)
-                      AND MCT.Name = 'UserLogin'
-                      AND MST.Name = 'TimeZone'
-					  AND ULP.PrimaryOrganization = 1 -- ONLY JOIN TO THE PRIMARY ORG FOR THIS PROC  ;
-             END;
-             ELSE
-         IF @UserId IS NOT NULL
-             BEGIN
-                 SELECT DISTINCT
-                        ul.UserId,
-                        ul.PersonPartyId AS PartyId,
-                        ul.[LoginName],
-                        p.RealPageId,
-                        ULP.StatusTypeId AS StatusId,
-                        ul.PasswordModifiedDate,
-                        ul.PasswordHash,
-                        ul.PasswordSalt,
-                        ULP.FromDate,
-                        ULP.ThruDate,
-                        ULP.StatusThruDate,
-                        ul.LastLoginDate 'LastLogin',
-                        MS.Value 'TimeZoneOffset'
-                 FROM Ident.UserLogin ul
-                      INNER JOIN Ident.UserLoginPersona ULP ON ULP.UserLoginId = Ul.UserId
-                      JOIN Enterprise.Party p ON p.PartyId = ul.PersonPartyId
-                      INNER JOIN Enterprise.MasterConfiguration MC ON MC.AttributeId = UL.UserId
-                      INNER JOIN Enterprise.MasterConfigurationSetting MCS ON MC.MasterConfigurationId = MCS.MasterConfigurationId
-                      INNER JOIN Enterprise.MasterSetting MS ON MCS.MasterSettingId = MS.MasterSettingId
-                      INNER JOIN Enterprise.MasterSettingType MST ON MST.MasterSettingTypeId = MS.MasterSettingTypeId
-                      INNER JOIN Enterprise.MasterConfigurationType MCT ON MCT.MasterConfigurationTypeId = MST.MasterConfigurationTypeId
-                 WHERE(UL.UserId = @UserId)
-                      AND MCT.Name = 'UserLogin'
-                      AND MST.Name = 'TimeZone'
-					  AND ULP.PrimaryOrganization = 1 -- ONLY JOIN TO THE PRIMARY ORG FOR THIS PROC  ;
-             END;
+     	IF @RealPageId IS NULL
+		BEGIN
+			SELECT top 1 @RealPageId = p.RealPageId
+			FROM Ident.UserLogin ul
+				JOIN Enterprise.Party p ON p.PartyId = ul.PersonPartyId
+			WHERE(UL.UserId = @UserId)
+		END
+
+        SELECT DISTINCT
+            ul.UserId,
+            ul.PersonPartyId AS PartyId,
+            ul.[LoginName],
+            p.RealPageId,
+            ULP.StatusTypeId AS StatusId,
+            ul.PasswordModifiedDate,
+            ul.PasswordHash,
+            ul.PasswordSalt,
+            ULP.FromDate,
+            ULP.ThruDate,
+            ULP.StatusThruDate,
+            ul.LastLoginDate [LastLogin],
+            MS.Value [TimeZoneOffset]
+        FROM Ident.UserLogin ul
+            INNER JOIN Ident.UserLoginPersona ULP ON ULP.UserLoginId = Ul.UserId
+            JOIN Enterprise.Party p ON p.PartyId = ul.PersonPartyId
+            INNER JOIN Enterprise.MasterConfiguration MC ON MC.AttributeId = UL.UserId
+            INNER JOIN Enterprise.MasterConfigurationSetting MCS ON MC.MasterConfigurationId = MCS.MasterConfigurationId
+            INNER JOIN Enterprise.MasterSetting MS ON MCS.MasterSettingId = MS.MasterSettingId
+            INNER JOIN Enterprise.MasterSettingType MST ON MST.MasterSettingTypeId = MS.MasterSettingTypeId
+            INNER JOIN Enterprise.MasterConfigurationType MCT ON MCT.MasterConfigurationTypeId = MST.MasterConfigurationTypeId
+        WHERE(p.RealPageId = @RealPageId)
+            AND MCT.Name = 'UserLogin'
+            AND MST.Name = 'TimeZone'
+			AND ULP.PrimaryOrganization = 1 -- ONLY JOIN TO THE PRIMARY ORG FOR THIS PROC  ;
+
      END;
 
