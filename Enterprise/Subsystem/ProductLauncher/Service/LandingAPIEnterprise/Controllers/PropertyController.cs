2703a1047048d5e7e2cda6339001d2d987f66a06
govindarao-ommi <govindarao.ommi@realpage.com>
2021-01-11 18:57:36 +0530

    Fix for HAAS GetAll properties returning the wrong HAAS company ID

--- Enterprise/Subsystem/ProductLauncher/Service/LandingAPIEnterprise/Controllers/PropertyController.cs
+++ Enterprise/Subsystem/ProductLauncher/Service/LandingAPIEnterprise/Controllers/PropertyController.cs
@@ -191,17 +191,18 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Service.LandingAPIEnterprise.C
             IManageUserLogin manageUserLogin = new ManageUserLogin(_userClaims);
             List<UserCompaniesProperties> userCompaniesProperties = new List<UserCompaniesProperties>();
 
-            var companyResponse = manageUserLogin.GetUserPersonaOrganization(_userClaims.LoginName);
+            var companyResponse = manageUserLogin.GetUserPersonaOrganization(_userClaims.LoginName);            
             var upfmProduct = ProductEnumHelper.GetUPFMProductEnum(productId);
 
             foreach (var company in companyResponse)
             {
+                var compnayInstanceSourceId = upfmProductIntegration.GetProductCompanyInstanceId(company.OrganizationRealPageId, company.BooksCustomerMasterId, "HAAS", "Primary");
                 propertyResponse = upfmProductIntegration.GetUPFMProperties(company.PersonaId, upfmProduct, null, "haas", company.OrganizationRealPageId.ToString());
-                if (propertyResponse.Records.Count == 0) return Request.CreateResponse(HttpStatusCode.ExpectationFailed, $"Properties are not loaded from Blue Book {propertyResponse.ErrorReason}");
+                if (propertyResponse.Records == null || propertyResponse.Records.Count == 0) return Request.CreateResponse(HttpStatusCode.ExpectationFailed, $"Properties are not loaded from Blue Book {propertyResponse.ErrorReason}");
 
                 var userCompanyProperties = new UserCompaniesProperties()
                 {
-                    Id = company.BooksCustomerMasterId,
+                    Id = compnayInstanceSourceId,
                     OrganizationName = company.OrganizationName,
                     InstanceId = company.OrganizationRealPageId,
                     Properties = new List<Properties>()
