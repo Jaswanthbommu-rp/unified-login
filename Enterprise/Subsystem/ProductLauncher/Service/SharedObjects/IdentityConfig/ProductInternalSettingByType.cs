5224678e4865b4a011bdb2eb3307643c0ae5fcd3
James <james.reames@realpage.com>
2021-01-18 10:41:40 -0600

    adding new api for getting setting types

---
+++ Enterprise/Subsystem/ProductLauncher/Service/SharedObjects/IdentityConfig/ProductInternalSettingByType.cs
@@ -0,0 +1,11 @@
+﻿namespace RP.Enterprise.Subsystem.ProductLauncher.Component.SharedObjects.IdentityConfig
+{
+    public class ProductInternalSettingByType : ProductInternalSetting
+    {
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string BooksProductCode { get; set; }
+    }
+}
