8165b3fb12b24ffda2625d193f4330159f880ae1
Kalyana Chakravarthy Bingi <kalyanachakravarthy.bingi@realpage.com>
2021-01-19 13:17:12 +0530

    removing conflicts

--- Enterprise/Subsystem/ProductLauncher/Service/SharedObjects/Enum/ProductEnum.cs
+++ Enterprise/Subsystem/ProductLauncher/Service/SharedObjects/Enum/ProductEnum.cs
@@ -95,6 +95,7 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Component.SharedObjects.Enum
 				case ProductEnum.HospitalityService: return "resident-services"; // Temp image until product has defined their final production
 				case ProductEnum.PMEDasboard: return "product-updates";
 				case ProductEnum.HOTS: return "hots";
+				case ProductEnum.P2EngagementQueue: return "video-call-laptop";
 				case ProductEnum.AoMarketAnalytics: return "marketing-analytics";
 			}
 			return null;
