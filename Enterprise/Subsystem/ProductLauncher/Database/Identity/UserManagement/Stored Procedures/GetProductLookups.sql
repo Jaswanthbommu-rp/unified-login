4acf39f27c8eb912630e0a99776223f0f5010594
Jhuman <jhuman.chhetri@realpage.com>
2021-01-29 11:59:32 -0600

    added srpco [Enterprise].[ProductLookup]  to get product lookups

---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/UserManagement/Stored Procedures/GetProductLookups.sql
@@ -0,0 +1,11 @@
+﻿
+-- =============================================
+-- Author:		Jhuman Chhetri
+-- Create date: 
+-- Description: Gets the product look up from ProductType, ControlType and ProductPageType
+-- =============================================
+CREATE procedure [Enterprise].[ProductLookup]  
+as    
+ select ProductTypeId, Name, Description from Enterprise.ProductType with (nolock) where ParentProductTypeId is null;  
+ select ControlTypeId, Name, Description from [UserManagement].[ControlType] with (nolock);  
+ select ProductPageTypeId, value, Description from [UserManagement].[ProductPageType] with (nolock);  
