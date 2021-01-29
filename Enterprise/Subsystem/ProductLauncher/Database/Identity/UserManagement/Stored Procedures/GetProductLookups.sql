c7ffec92c41a512cb94ff1fac61bc93eced84f96
Jhuman <jhuman.chhetri@realpage.com>
2021-01-29 13:08:09 -0600

    adding begin and end

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/UserManagement/Stored Procedures/GetProductLookups.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/UserManagement/Stored Procedures/GetProductLookups.sql
@@ -5,7 +5,9 @@
 -- Description: Gets the product look up from ProductType, ControlType and ProductPageType
 -- =============================================
 CREATE procedure [Enterprise].[ProductLookup]  
-AS    
+AS  
+BEGIN
  SELECT ProductTypeId, Name, Description from Enterprise.ProductType with (nolock) where ParentProductTypeId is null;  
  SELECT ControlTypeId, Name, Description from [UserManagement].[ControlType] with (nolock);  
  SELECT ProductPageTypeId, value, Description from [UserManagement].[ProductPageType] with (nolock);  
+END
