3506b12096fb598715a3670eb4ba388cd29bc1d3
Jhuman <jhuman.chhetri@realpage.com>
2021-01-29 12:41:02 -0600

    productlookup sql

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/UserManagement/Stored Procedures/GetProductLookups.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/UserManagement/Stored Procedures/GetProductLookups.sql
@@ -5,7 +5,7 @@
 -- Description: Gets the product look up from ProductType, ControlType and ProductPageType
 -- =============================================
 CREATE procedure [Enterprise].[ProductLookup]  
-as    
- select ProductTypeId, Name, Description from Enterprise.ProductType with (nolock) where ParentProductTypeId is null;  
- select ControlTypeId, Name, Description from [UserManagement].[ControlType] with (nolock);  
- select ProductPageTypeId, value, Description from [UserManagement].[ProductPageType] with (nolock);  
+AS    
+ SELECT ProductTypeId, Name, Description from Enterprise.ProductType with (nolock) where ParentProductTypeId is null;  
+ SELECT ControlTypeId, Name, Description from [UserManagement].[ControlType] with (nolock);  
+ SELECT ProductPageTypeId, value, Description from [UserManagement].[ProductPageType] with (nolock);  
