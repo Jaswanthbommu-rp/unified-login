32ca8e7d8a2ee4951b5a6fc8f3ed693936f6dc11
Jhuman <jhuman.chhetri@realpage.com>
2021-01-29 18:55:17 -0600

    new sproc, getAllProducts

---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Enterprise/Stored Procedures/GetAllProducts.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [Enterprise].[GetAllProducts] 
+AS
+BEGIN
+SELECT pt.ProductTypeId as TypeId, 
+	   pt.Name as TypeName, p.ProductId, p.ProductGUID, p.Name, 
+	   p.Description, p.ProductTypeId,p.BooksProductCode,
+	   p.UDMSourceCode, p.AssignToAllUsers
+FROM Enterprise.Product p
+	LEFT JOIN Enterprise.ProductType pt on pt.ProductTypeId = p.ProductTypeId
+ORDER BY pt.Name
+END
