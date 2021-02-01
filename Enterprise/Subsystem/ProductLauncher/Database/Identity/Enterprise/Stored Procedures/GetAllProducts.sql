482d27684896b56a2dfc601feffa286480c6e4a8
Jhuman <jhuman.chhetri@realpage.com>
2021-02-01 13:22:35 -0600

    modified proc to get product type name

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Enterprise/Stored Procedures/GetAllProducts.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Enterprise/Stored Procedures/GetAllProducts.sql
@@ -5,7 +5,8 @@ SELECT pt.ProductTypeId as TypeId,
 	   pt.Name as TypeName, p.ProductId, p.ProductGUID, p.Name, 
 	   p.Description, p.ProductTypeId,p.BooksProductCode,
 	   p.UDMSourceCode, p.AssignToAllUsers
-FROM Enterprise.Product p
-	LEFT JOIN Enterprise.ProductType pt on pt.ProductTypeId = p.ProductTypeId
+FROM Enterprise.ProductType pt
+	JOIN Enterprise.ProductType pt2 on pt.ProductTypeId = pt2.ParentProductTypeId
+	RIGHT JOIN Enterprise.Product p on p.ProductTypeId = pt2.ProductTypeId
 ORDER BY pt.Name
 END
