360a702b6d41e28ea6e3dbbc4e89a24f758c9e72
James <james.reames@realpage.com>
2021-02-01 16:31:46 -0600

    updating procs

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Ident/Stored Procedures/GetUserLogin.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Ident/Stored Procedures/GetUserLogin.sql
@@ -25,19 +25,19 @@ AS
             ULP.ThruDate,
             ULP.StatusThruDate,
             ul.LastLoginDate [LastLogin],
-            MS.Value [TimeZoneOffset]
+            '' AS [TimeZoneOffset],--MS.Value [TimeZoneOffset],
+			ul.TwoFactorEnabled [TwoFactorEnabled]
         FROM Ident.UserLogin ul
             INNER JOIN Ident.UserLoginPersona ULP ON ULP.UserLoginId = Ul.UserId
             JOIN Enterprise.Party p ON p.PartyId = ul.PersonPartyId
-            INNER JOIN Enterprise.MasterConfiguration MC ON MC.AttributeId = UL.UserId
-            INNER JOIN Enterprise.MasterConfigurationSetting MCS ON MC.MasterConfigurationId = MCS.MasterConfigurationId
-            INNER JOIN Enterprise.MasterSetting MS ON MCS.MasterSettingId = MS.MasterSettingId
-            INNER JOIN Enterprise.MasterSettingType MST ON MST.MasterSettingTypeId = MS.MasterSettingTypeId
-            INNER JOIN Enterprise.MasterConfigurationType MCT ON MCT.MasterConfigurationTypeId = MST.MasterConfigurationTypeId
+            --INNER JOIN Enterprise.MasterConfiguration MC ON MC.AttributeId = UL.UserId
+            --INNER JOIN Enterprise.MasterConfigurationSetting MCS ON MC.MasterConfigurationId = MCS.MasterConfigurationId
+            --INNER JOIN Enterprise.MasterSetting MS ON MCS.MasterSettingId = MS.MasterSettingId
+            --INNER JOIN Enterprise.MasterSettingType MST ON MST.MasterSettingTypeId = MS.MasterSettingTypeId
+            --INNER JOIN Enterprise.MasterConfigurationType MCT ON MCT.MasterConfigurationTypeId = MST.MasterConfigurationTypeId
         WHERE(p.RealPageId = @RealPageId)
-            AND MCT.Name = 'UserLogin'
-            AND MST.Name = 'TimeZone'
+            --AND MCT.Name = 'UserLogin'
+            --AND MST.Name = 'TimeZone'
 			AND ULP.PrimaryOrganization = 1 -- ONLY JOIN TO THE PRIMARY ORG FOR THIS PROC  ;
 
      END;
-
--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Ident/Stored Procedures/GetUserLoginByName.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Ident/Stored Procedures/GetUserLoginByName.sql
@@ -1,4 +1,4 @@
-﻿CREATE   PROCEDURE [Ident].[GetUserLoginByName](
+﻿CREATE PROCEDURE [Ident].[GetUserLoginByName](
 	 @EnterpriseUserName VARCHAR(255)
 )
 AS
@@ -16,18 +16,19 @@ BEGIN
         ULP.ThruDate,
 		ULP.StatusThruDate AS StatusThruDate,
 		ul.LastLoginDate 'LastLogin',
-        MS.Value 'TimeZoneOffset'
+        '' AS [TimeZoneOffset],--MS.Value [TimeZoneOffset],
+		ul.TwoFactorEnabled [TwoFactorEnabled]
     FROM Ident.UserLogin ul
         JOIN Enterprise.Party p ON p.PartyId = UL.PersonPartyId
 		INNER JOIN Ident.UserLoginPersona ULP ON ULP.UserLoginId = UL.UserId AND ULP.PrimaryOrganization = 1 -- ONLY JOIN TO THE PRIMARY ORG FOR THIS PROC
-        INNER JOIN Person.Persona PE ON ULP.UserLoginPersonaId = PE.UserLoginPersonaId
-        INNER JOIN Enterprise.MasterConfiguration MC ON MC.AttributeId = UL.UserId
-        INNER JOIN Enterprise.MasterConfigurationSetting MCS ON MC.MasterConfigurationId = MCS.MasterConfigurationId
-        INNER JOIN Enterprise.MasterSetting MS ON MCS.MasterSettingId = MS.MasterSettingId
-        INNER JOIN Enterprise.MasterSettingType MST ON MST.MasterSettingTypeId = MS.MasterSettingTypeId
-        INNER JOIN Enterprise.MasterConfigurationType MCT ON MCT.MasterConfigurationTypeId = MST.MasterConfigurationTypeId
+        --INNER JOIN Person.Persona PE ON ULP.UserLoginPersonaId = PE.UserLoginPersonaId
+        --INNER JOIN Enterprise.MasterConfiguration MC ON MC.AttributeId = UL.UserId
+        --INNER JOIN Enterprise.MasterConfigurationSetting MCS ON MC.MasterConfigurationId = MCS.MasterConfigurationId
+        --INNER JOIN Enterprise.MasterSetting MS ON MCS.MasterSettingId = MS.MasterSettingId
+        --INNER JOIN Enterprise.MasterSettingType MST ON MST.MasterSettingTypeId = MS.MasterSettingTypeId
+        --INNER JOIN Enterprise.MasterConfigurationType MCT ON MCT.MasterConfigurationTypeId = MST.MasterConfigurationTypeId
     WHERE ul.[LoginName] = @EnterpriseUserName
-        AND MCT.Name = 'UserLogin'
-        AND MST.Name = 'TimeZone';
+        --AND MCT.Name = 'UserLogin'
+        --AND MST.Name = 'TimeZone';
 END;
 
