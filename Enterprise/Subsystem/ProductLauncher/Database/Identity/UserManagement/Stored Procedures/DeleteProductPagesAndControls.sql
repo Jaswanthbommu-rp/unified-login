a04f8c734b1083da9cc2bed389cef9b72e0b0c6b
Jhuman <jhuman.chhetri@realpage.com>
2021-02-22 09:10:32 -0600

    updated sprocs

---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/UserManagement/Stored Procedures/DeleteProductPagesAndControls.sql
@@ -0,0 +1,62 @@
+﻿CREATE PROCEDURE [UserManagement].DeleteProductPagesAndControls (                
+  @ProductId int  
+)                 
+AS                 
+begin              
+ BEGIN TRY                
+        BEGIN TRANSACTION;   
+    --get all the product controls  
+   WITH  SelfJoinCTE AS (   
+     SELECT    
+    c.[ControlId]   
+     FROM [UserManagement].[Control]  c  
+    INNER JOIN [UserManagement].[ControlType] ct ON c.[ControlTypeId] = ct.[ControlTypeId]    
+    inner join UserManagement.ProductPageControl pc on pc.ControlId=c.ControlId  
+    inner join UserManagement.ProductPage pp on pp.ProductPageId=pc.ProductPageId  
+     WHERE pp.ProductId=@ProductId  
+     UNION ALL    
+     SELECT    
+     [Control].[ControlId]   
+     FROM [UserManagement].[Control]    
+    INNER JOIN [UserManagement].[ControlType] ON [Control].[ControlTypeId] = [ControlType].[ControlTypeId]    
+    INNER JOIN SelfJoinCTE ON ([UserManagement].[Control].[ParentControlId] = SelfJoinCTE.[ControlId])    
+    )    
+    select DISTINCT CTE.[ControlId] into #conrolids FROM SelfJoinCTE CTE  
+    
+   --delete the pages  
+   delete a from   
+    UserManagement.ProductPageControl a join  
+    UserManagement.ProductPage b   
+     on a.ProductPageId=b.ProductPageId  
+    where b.ProductId=@ProductId  
+  
+   delete from   
+    UserManagement.ProductPage  
+    where ProductId=@ProductId   
+  
+   --delete the controls  
+   delete from [UserManagement].[ControlDependency]   
+   where MasterControlId in   
+    (SELECT controlid from #conrolids);  
+  
+   delete from [UserManagement].ControlAttribute   
+   where ControlId in   
+    (SELECT controlid from #conrolids );   
+   
+   delete from [UserManagement].Control   
+   where ControlId in   
+    (SELECT controlid from #conrolids );  
+  
+  COMMIT;                
+ END TRY                  
+ BEGIN CATCH           
+  print ERROR_MESSAGE()            
+        ROLLBACK;          
+        DECLARE @ErrorLogID INT;                
+        EXEC dbo.LogError @ErrorLogID = @ErrorLogID OUTPUT;                
+        SELECT  0 AS Id,                
+   ErrorMessage                
+        FROM    dbo.ErrorLog                
+        WHERE   ErrorLogID = @ErrorLogID;                
+ END CATCH                
+END;
--- Enterprise/Subsystem/ProductLauncher/Database/Identity/UserManagement/Stored Procedures/SetProductControl.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/UserManagement/Stored Procedures/SetProductControl.sql
@@ -1,87 +1,90 @@
-﻿CREATE PROCEDURE [UserManagement].[SetProductControl] (            
-  @ControlId INT OUTPUT           
- ,@ParentControlId INT            
- ,@ProductPageId int          
- ,@ControlTypeId INT            
- ,@UIId NVARCHAR(510)            
- ,@DisplayName NVARCHAR(510)            
- ,@DataSource NTEXT            
- ,@Sequence TINYINT            
- ,@CreatedBy BIGINT            
-)             
-AS             
-begin          
- BEGIN TRY            
-        BEGIN TRANSACTION;        
-   IF (@ControlId is not null)          
-   BEGIN            
- select 0      
-     UPDATE [UserManagement].[Control] SET             
-     [ParentControlId] = @ParentControlId            
-     ,[ControlTypeId] = @ControlTypeId            
-     ,[UIId] = @UIId            
-     ,[DisplayName] = @DisplayName            
-     ,[DataSource] = @DataSource            
-     ,[Sequence] = @Sequence            
-     ,[CreatedBy] = @CreatedBy            
-     ,[CreatedDate] = getdate()            
-    WHERE            
-     [ControlId] = @ControlId           
-   END            
-   ELSE            
-   BEGIN            
-    --if this is a new item make sure that if it has no parent that we dont have a record in [UserManagement].ProductPageControl so we don't have orphen records          
-    if(ISNULL(@ParentControlId,-1)>0          
-     or (ISNULL(@ParentControlId,-1)<1 and           
-      ISNULL(@ProductPageId,-1)>0 and          
-      (not exists(select top 1 1 from [UserManagement].ProductPageControl where ProductPageId=@ProductPageId))))          
-    begin             
-     INSERT INTO [UserManagement].[Control] (            
-      [ParentControlId]            
-      ,[ControlTypeId]            
-      ,[UIId]            
-      ,[DisplayName]            
-      ,[DataSource]            
-      ,[Sequence]            
-      ,[CreatedBy]            
-      ,[CreatedDate])            
-     VALUES(            
-      @ParentControlId            
-      ,@ControlTypeId            
-      ,@UIId            
-      ,@DisplayName            
-      ,@DataSource            
-      ,@Sequence            
-      ,@CreatedBy            
-      ,getdate());            
-     SET @ControlId = SCOPE_IDENTITY()          
-          
-     --add the control to the page if this is a root control          
-     --if there is no parent control then this is a root item that is under the page          
-     if(ISNULL(@ParentControlId,-1)<1)          
-     begin          
-      insert into [UserManagement].ProductPageControl(ProductPageId          
-       ,ControlId          
-       ,CreatedBy          
-       ,CreatedDate)          
-      values(@ProductPageId          
-       ,@ControlId          
-       ,@CreatedBy          
-       ,getdate())          
-     end          
-    end          
-   END;          
-           
-  COMMIT;            
- END TRY              
- BEGIN CATCH       
-  print ERROR_MESSAGE()        
-        ROLLBACK;      
-        DECLARE @ErrorLogID INT;            
-        EXEC dbo.LogError @ErrorLogID = @ErrorLogID OUTPUT;            
-        SELECT  0 AS Id,            
-   ErrorMessage            
-        FROM    dbo.ErrorLog            
-        WHERE   ErrorLogID = @ErrorLogID;            
- END CATCH            
+﻿CREATE PROCEDURE [UserManagement].[SetProductControl] (                
+ @ControlId INT OUTPUT               
+ ,@ParentControlId INT                
+ ,@ProductPageId int              
+ ,@ControlTypeId INT                
+ ,@UIId NVARCHAR(510)                
+ ,@DisplayName NVARCHAR(510)                
+ ,@DataSource NTEXT                
+ ,@Sequence TINYINT   
+ ,@CreatedBy BIGINT        
+ ,@ProductControlsAttribute [UserManagement].[ProductControlAttributeType] READONLY     
+)                 
+AS                 
+begin              
+ BEGIN TRY                
+        BEGIN TRANSACTION;   
+   IF (@ControlId is not null)              
+   BEGIN                
+ select 0          
+     UPDATE [UserManagement].[Control] SET                 
+     [ParentControlId] = @ParentControlId                
+     ,[ControlTypeId] = @ControlTypeId                
+     ,[UIId] = @UIId                
+     ,[DisplayName] = @DisplayName                
+     ,[DataSource] = @DataSource                
+     ,[Sequence] = @Sequence                
+     ,[CreatedBy] = @CreatedBy                
+     ,[CreatedDate] = getdate()                
+    WHERE                
+     [ControlId] = @ControlId               
+   END                
+   ELSE                
+   BEGIN                
+    --if this is a new item make sure that if it has no parent that we dont have a record in [UserManagement].ProductPageControl so we don't have orphen records              
+    if(ISNULL(@ParentControlId,-1)>0              
+     or (ISNULL(@ParentControlId,-1)<1 and               
+      ISNULL(@ProductPageId,-1)>0 and              
+      (not exists(select top 1 1 from [UserManagement].ProductPageControl where ProductPageId=@ProductPageId))))              
+    begin                 
+     INSERT INTO [UserManagement].[Control] (                
+      [ParentControlId]                
+      ,[ControlTypeId]                
+      ,[UIId]                
+      ,[DisplayName]                
+      ,[DataSource]                
+      ,[Sequence]                
+      ,[CreatedBy]                
+      ,[CreatedDate])                
+     VALUES(                
+      @ParentControlId                
+      ,@ControlTypeId                
+      ,@UIId                
+      ,@DisplayName                
+      ,@DataSource                
+      ,@Sequence                
+      ,@CreatedBy                
+      ,getdate());                
+     SET @ControlId = SCOPE_IDENTITY()              
+              
+     --add the control to the page if this is a root control              
+     --if there is no parent control then this is a root item that is under the page              
+     if(ISNULL(@ParentControlId,-1)<1)              
+     begin              
+      insert into [UserManagement].ProductPageControl(ProductPageId              
+       ,ControlId              
+       ,CreatedBy              
+       ,CreatedDate)              
+      values(@ProductPageId              
+       ,@ControlId              
+       ,@CreatedBy              
+       ,getdate())              
+     end       
+  insert into UserManagement.ControlAttribute(ControlId,[Key],Value,CreatedBy,CreatedDate)  
+  select @ControlId, [key], VALUE, @CreatedBy, GETDATE() from @ProductControlsAttribute  
+    end              
+   END;              
+               
+  COMMIT;                
+ END TRY                  
+ BEGIN CATCH           
+  print ERROR_MESSAGE()            
+        ROLLBACK;          
+        DECLARE @ErrorLogID INT;                
+        EXEC dbo.LogError @ErrorLogID = @ErrorLogID OUTPUT;                
+        SELECT  0 AS Id,                
+   ErrorMessage                
+        FROM    dbo.ErrorLog                
+        WHERE   ErrorLogID = @ErrorLogID;                
+ END CATCH                
 END;
