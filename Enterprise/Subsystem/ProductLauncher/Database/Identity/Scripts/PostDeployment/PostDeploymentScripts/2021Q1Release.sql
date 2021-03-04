0b60714607cb2169b67aa7373688d9896371f858
Vijayasekher Manellore <Vijayasekher.Manellore@RealPage.com>
2021-03-04 08:50:23 -0600

    Merged PR 65219: Rool back Accounting LG
    Rool back Accounting LG
    
    Related work items: #99636

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/2021Q1Release.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/2021Q1Release.sql
@@ -464,83 +464,83 @@ end
 
 GO
 --Accounting Location Group
-Declare @FSMasterControlId int,@FSLocationGroupControlId int,@MaxControlId int,@MaxControlAttributeId int
-DECLARE @UserId bigint,
-	@ProductId int ,
-	@Now datetime = GETDATE()
+--Declare @FSMasterControlId int,@FSLocationGroupControlId int,@MaxControlId int,@MaxControlAttributeId int
+--DECLARE @UserId bigint,
+--	@ProductId int ,
+--	@Now datetime = GETDATE()
 
-SELECT	@UserId = UserId
-FROM	Ident.UserLogin
-WHERE	LoginName LIKE 'realpagead@%'
+--SELECT	@UserId = UserId
+--FROM	Ident.UserLogin
+--WHERE	LoginName LIKE 'realpagead@%'
 
-Select @FSMasterControlId = ControlId From UserManagement.Control 
-Where UIId = 'FinancialSuiteProductAccessTabGroupUIId' AND ControlTypeId = 8
+--Select @FSMasterControlId = ControlId From UserManagement.Control 
+--Where UIId = 'FinancialSuiteProductAccessTabGroupUIId' AND ControlTypeId = 8
 
-Select @FSLocationGroupControlId = ControlId From UserManagement.Control 
-Where UIId = 'FinancialSuiteProductAccessLocationGroupTabUIId' AND ControlTypeId = 9
+--Select @FSLocationGroupControlId = ControlId From UserManagement.Control 
+--Where UIId = 'FinancialSuiteProductAccessLocationGroupTabUIId' AND ControlTypeId = 9
 
-update [UserManagement].[Control] set Sequence = 8
-where uiid = 'FinancialSuiteProductAccessEntitiesTabUIId' and ControlTypeId = 9
+--update [UserManagement].[Control] set Sequence = 8
+--where uiid = 'FinancialSuiteProductAccessEntitiesTabUIId' and ControlTypeId = 9
 
-IF NOT EXISTS (SELECT TOP 1 1 FROM[UserManagement].[Control] WHERE ControlId = @FSLocationGroupControlId)
-BEGIN
+--IF NOT EXISTS (SELECT TOP 1 1 FROM[UserManagement].[Control] WHERE ControlId = @FSLocationGroupControlId)
+--BEGIN
 	
-	SET IDENTITY_INSERT [UserManagement].[Control] ON 
-	SELECT @MaxControlId = max(ControlId) from UserManagement.Control
+--	SET IDENTITY_INSERT [UserManagement].[Control] ON 
+--	SELECT @MaxControlId = max(ControlId) from UserManagement.Control
 
-	INSERT [UserManagement].[Control] ([ControlId], [ParentControlId], [ControlTypeId], [UIId], [DisplayName], [DataSource], [Sequence], [CreatedBy], [CreatedDate])
-	VALUES (@MaxControlId +1, @FSMasterControlId, 9, N'FinancialSuiteProductAccessLocationGroupTabUIId', N'Location Group', NULL, 7, @UserId, @Now)
+--	INSERT [UserManagement].[Control] ([ControlId], [ParentControlId], [ControlTypeId], [UIId], [DisplayName], [DataSource], [Sequence], [CreatedBy], [CreatedDate])
+--	VALUES (@MaxControlId +1, @FSMasterControlId, 9, N'FinancialSuiteProductAccessLocationGroupTabUIId', N'Location Group', NULL, 7, @UserId, @Now)
 
-	INSERT [UserManagement].[Control] ([ControlId], [ParentControlId], [ControlTypeId], [UIId], [DisplayName], [DataSource], [Sequence], [CreatedBy], [CreatedDate])
-	VALUES (@MaxControlId +2, @MaxControlId +1, 3, N'FinancialSuiteProductAccessLocationGroupMultiSelectGridUIId', NULL, NULL, 1, @UserId, @Now)
+--	INSERT [UserManagement].[Control] ([ControlId], [ParentControlId], [ControlTypeId], [UIId], [DisplayName], [DataSource], [Sequence], [CreatedBy], [CreatedDate])
+--	VALUES (@MaxControlId +2, @MaxControlId +1, 3, N'FinancialSuiteProductAccessLocationGroupMultiSelectGridUIId', NULL, NULL, 1, @UserId, @Now)
 
-	INSERT [UserManagement].[Control] ([ControlId], [ParentControlId], [ControlTypeId], [UIId], [DisplayName], [DataSource], [Sequence], [CreatedBy], [CreatedDate])
-	VALUES (@MaxControlId +3, @MaxControlId +2, 10, N'FinancialSuiteProductAccessLocationGroupCheckboxUIId', NULL, N'isAssigned', 2, @UserId, @Now)
+--	INSERT [UserManagement].[Control] ([ControlId], [ParentControlId], [ControlTypeId], [UIId], [DisplayName], [DataSource], [Sequence], [CreatedBy], [CreatedDate])
+--	VALUES (@MaxControlId +3, @MaxControlId +2, 10, N'FinancialSuiteProductAccessLocationGroupCheckboxUIId', NULL, N'isAssigned', 2, @UserId, @Now)
 
-	INSERT [UserManagement].[Control] ([ControlId], [ParentControlId], [ControlTypeId], [UIId], [DisplayName], [DataSource], [Sequence], [CreatedBy], [CreatedDate])
-	VALUES (@MaxControlId +4, @MaxControlId +2, 5, N'FinancialSuiteProductAccessLocationGroupLabelUIId', N'Location Group', N'name', 3, @UserId, @Now)
+--	INSERT [UserManagement].[Control] ([ControlId], [ParentControlId], [ControlTypeId], [UIId], [DisplayName], [DataSource], [Sequence], [CreatedBy], [CreatedDate])
+--	VALUES (@MaxControlId +4, @MaxControlId +2, 5, N'FinancialSuiteProductAccessLocationGroupLabelUIId', N'Location Group', N'name', 3, @UserId, @Now)
 
-	--PGSlide
-			INSERT [UserManagement].[Control] ([ControlId], [ParentControlId], [ControlTypeId], [UIId], [DisplayName], [DataSource], [Sequence], [CreatedBy], [CreatedDate])
-			VALUES (@MaxControlId +5, @MaxControlId +2, 11, N'FinancialSuiteProductAccessLocationGroupIconUIId', NULL, N'InfoIcon', 4, @UserId, @Now)
+--	--PGSlide
+--			INSERT [UserManagement].[Control] ([ControlId], [ParentControlId], [ControlTypeId], [UIId], [DisplayName], [DataSource], [Sequence], [CreatedBy], [CreatedDate])
+--			VALUES (@MaxControlId +5, @MaxControlId +2, 11, N'FinancialSuiteProductAccessLocationGroupIconUIId', NULL, N'InfoIcon', 4, @UserId, @Now)
 
-			INSERT [UserManagement].[Control] ([ControlId], [ParentControlId], [ControlTypeId], [UIId], [DisplayName], [DataSource], [Sequence], [CreatedBy], [CreatedDate])
-			VALUES (@MaxControlId +6, @MaxControlId +5, 5, N'FinancialSuiteProductAccessLocationGroupDetailsLabelUIId', N'Location Group Details', NULL, 1, @UserId, @Now)
+--			INSERT [UserManagement].[Control] ([ControlId], [ParentControlId], [ControlTypeId], [UIId], [DisplayName], [DataSource], [Sequence], [CreatedBy], [CreatedDate])
+--			VALUES (@MaxControlId +6, @MaxControlId +5, 5, N'FinancialSuiteProductAccessLocationGroupDetailsLabelUIId', N'Location Group Details', NULL, 1, @UserId, @Now)
 
-			INSERT [UserManagement].[Control] ([ControlId], [ParentControlId], [ControlTypeId], [UIId], [DisplayName], [DataSource], [Sequence], [CreatedBy], [CreatedDate])
-			VALUES (@MaxControlId +7, @MaxControlId +5, 12, N'FinancialSuiteProductAccessLocationGroupDetailsGridUIId', NULL, NULL, 2, @UserId, @Now)
+--			INSERT [UserManagement].[Control] ([ControlId], [ParentControlId], [ControlTypeId], [UIId], [DisplayName], [DataSource], [Sequence], [CreatedBy], [CreatedDate])
+--			VALUES (@MaxControlId +7, @MaxControlId +5, 12, N'FinancialSuiteProductAccessLocationGroupDetailsGridUIId', NULL, NULL, 2, @UserId, @Now)
 
-			INSERT [UserManagement].[Control] ([ControlId], [ParentControlId], [ControlTypeId], [UIId], [DisplayName], [DataSource], [Sequence], [CreatedBy], [CreatedDate])
-			VALUES (@MaxControlId +8, @MaxControlId +7, 5, N'FinancialSuiteProductAccessLocationGroupDetailsPropertyLabelUIId', N'Entity', N'name', 2, @UserId, @Now)
+--			INSERT [UserManagement].[Control] ([ControlId], [ParentControlId], [ControlTypeId], [UIId], [DisplayName], [DataSource], [Sequence], [CreatedBy], [CreatedDate])
+--			VALUES (@MaxControlId +8, @MaxControlId +7, 5, N'FinancialSuiteProductAccessLocationGroupDetailsPropertyLabelUIId', N'Entity', N'name', 2, @UserId, @Now)
 			
-			SET IDENTITY_INSERT [UserManagement].[Control] OFF
-END
+--			SET IDENTITY_INSERT [UserManagement].[Control] OFF
+--END
 
-		SET IDENTITY_INSERT [UserManagement].[ControlAttribute] ON 
+--		SET IDENTITY_INSERT [UserManagement].[ControlAttribute] ON 
             
-			SELECT @MaxControlAttributeId = max(ControlAttributeId) from UserManagement.ControlAttribute
-			Declare @FSLGSelallID int,@FSLGinfoiconID int
+--			SELECT @MaxControlAttributeId = max(ControlAttributeId) from UserManagement.ControlAttribute
+--			Declare @FSLGSelallID int,@FSLGinfoiconID int
 
-			Select @FSLGSelallID = ControlId From UserManagement.Control 
-            Where UIId = 'FinancialSuiteProductAccessLocationGroupMultiSelectGridUIId' AND ControlTypeId = 3
+--			Select @FSLGSelallID = ControlId From UserManagement.Control 
+--            Where UIId = 'FinancialSuiteProductAccessLocationGroupMultiSelectGridUIId' AND ControlTypeId = 3
 
-            Select @FSLGinfoiconID = ControlId From UserManagement.Control 
-            Where UIId = 'FinancialSuiteProductAccessLocationGroupIconUIId' AND ControlTypeId = 11
+--            Select @FSLGinfoiconID = ControlId From UserManagement.Control 
+--            Where UIId = 'FinancialSuiteProductAccessLocationGroupIconUIId' AND ControlTypeId = 11
 
-			IF NOT EXISTS ( SELECT TOP 1 1 FROM [UserManagement].[ControlAttribute] WHERE ControlId = @FSLGSelallID)
-			BEGIN
-				INSERT [UserManagement].[ControlAttribute] ([ControlAttributeId], [ControlId], [Key], [Value], [CreatedBy], [CreatedDate]) 
-				VALUES (@MaxControlAttributeId + 1, @FSLGSelallID, N'ShowSelectAll', N'False', @UserId, @Now)
-			END
+--			IF NOT EXISTS ( SELECT TOP 1 1 FROM [UserManagement].[ControlAttribute] WHERE ControlId = @FSLGSelallID)
+--			BEGIN
+--				INSERT [UserManagement].[ControlAttribute] ([ControlAttributeId], [ControlId], [Key], [Value], [CreatedBy], [CreatedDate]) 
+--				VALUES (@MaxControlAttributeId + 1, @FSLGSelallID, N'ShowSelectAll', N'False', @UserId, @Now)
+--			END
 
-			--SELECT @MaxControlAttributeId = max(ControlAttributeId) from UserManagement.ControlAttribute
-			IF NOT EXISTS ( SELECT TOP 1 1 FROM [UserManagement].[ControlAttribute] WHERE ControlId = @FSLGinfoiconID)
-			BEGIN
-				INSERT [UserManagement].[ControlAttribute] ([ControlAttributeId], [ControlId], [Key], [Value], [CreatedBy], [CreatedDate]) 
-				VALUES (@MaxControlAttributeId + 2, @FSLGinfoiconID, N'InfoIcon', N'Slide', @UserId, @Now)		
-			END
+--			--SELECT @MaxControlAttributeId = max(ControlAttributeId) from UserManagement.ControlAttribute
+--			IF NOT EXISTS ( SELECT TOP 1 1 FROM [UserManagement].[ControlAttribute] WHERE ControlId = @FSLGinfoiconID)
+--			BEGIN
+--				INSERT [UserManagement].[ControlAttribute] ([ControlAttributeId], [ControlId], [Key], [Value], [CreatedBy], [CreatedDate]) 
+--				VALUES (@MaxControlAttributeId + 2, @FSLGinfoiconID, N'InfoIcon', N'Slide', @UserId, @Now)		
+--			END
 			
-            SET IDENTITY_INSERT [UserManagement].[ControlAttribute] OFF
+--            SET IDENTITY_INSERT [UserManagement].[ControlAttribute] OFF
 
 GO
 
@@ -1586,18 +1586,18 @@ BEGIN
 END
 GO
 
--- 679135
-UPDATE enterprise.product SET Description = 'Axiometrics is the industry leader in providing multifamily and student housing data at individual properties down to the floorplan level. Covering markets of all sizes across the country, Axiometrics delivers detailed reports and analytics on tens of thousands of assets every month.' WHERE productid = 33
-GO
+---- 679135
+--UPDATE enterprise.product SET Description = 'Axiometrics is the industry leader in providing multifamily and student housing data at individual properties down to the floorplan level. Covering markets of all sizes across the country, Axiometrics delivers detailed reports and analytics on tens of thousands of assets every month.' WHERE productid = 33
+--GO
 
-UPDATE ps
-SET ps.Value = 'https://www.realpage.com/asset-optimization/market-analytics/'
-FROM enterprise.ProductSetting ps 
-INNER JOIN enterprise.ProductSettingType pst ON pst.ProductSettingTypeId = ps.ProductSettingTypeId 
-WHERE ProductId = 33 AND pst.Name = 'LearnMore'
--- 679135
+--UPDATE ps
+--SET ps.Value = 'https://www.realpage.com/asset-optimization/market-analytics/'
+--FROM enterprise.ProductSetting ps 
+--INNER JOIN enterprise.ProductSettingType pst ON pst.ProductSettingTypeId = ps.ProductSettingTypeId 
+--WHERE ProductId = 33 AND pst.Name = 'LearnMore'
+---- 679135
 
-GO
+--GO
 
 -- ILMLM/ILMLA Authentication for GB API
 DECLARE @ServerName SYSNAME = @@SERVERNAME
