647219b0f391350edd8c203bbd036a4c2014a738
Jhuman <jhuman.chhetri@realpage.com>
2021-03-11 16:52:14 -0600

    updated proc

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Enterprise/Stored Procedures/SetProductSetting.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Enterprise/Stored Procedures/SetProductSetting.sql
@@ -4,83 +4,78 @@
  @ProductSettingTypeId INT,                
  @Value NVARCHAR(1000)              
 )                
-AS
+AS                
 BEGIN                
-BEGIN TRY                  
-BEGIN TRAN;
-DECLARE   
- @ConfigurationId int,
-@productConfigurationId int,               
- @now datetime,              
- @nowPlusSec datetime,              
- @TempSettingId INT              
-             
- SET @now = getdate()              
- SET @nowPlusSec=DATEADD (ss, 1, @now)                    
-  
-
---1. check if given setting exists, if not add one
-IF NOT EXISTS(select top 1 * from Enterprise.ProductSetting Where ProductId = @ProductId AND ProductSettingTypeId = @ProductSettingTypeId AND Value = @Value)
-       BEGIN
-              --This setting does not exists so inserting new one
-              INSERT INTO Enterprise.ProductSetting(ProductId, ProductSettingTypeId, Value, FromDate, ThruDate)              
-                     VALUES(@ProductId, @ProductSettingTypeId, @Value, @nowPlusSec, null)              
-              SELECT @ProductSettingId = SCOPE_IDENTITY()  
-       END
-ELSE
-       BEGIN 
-              SELECT @ProductSettingId = ProductSettingId 
-              FROM Enterprise.ProductSetting
-              WHERE ProductId = @ProductId 
-              AND ProductSettingTypeId = @ProductSettingTypeId
-              AND [Value] = @Value
-              AND ThruDate is Null
-       END
-
---2. check if product already has global config and get cofigId, if not add new and get the Id
-IF EXISTS(SELECT TOP 1 * FROM Enterprise.GlobalProductConfiguration WHERE productId = @ProductId and ThruDate is Null)
-       BEGIN
-              --This product already has Global Config ID
-              SELECT TOP 1 @ConfigurationId = ConfigurationId from Enterprise.GlobalProductConfiguration where productId = @ProductId and ThruDate is Null
-       END
-ELSE
-       BEGIN
-              --Insert New Global Config
-              INSERT INTO Enterprise.Configuration(CreateDate) VALUES (@now)              
-              SET @ConfigurationId = SCOPE_IDENTITY()              
+ BEGIN TRY                  
+  BEGIN TRAN;                
+   --Check the config id              
+   DECLARE               
+   @ConfigurationId int,              
+   @now datetime,              
+   @nowPlusSec datetime,              
+   @TempSettingId INT              
+              
+   set @now = getdate()              
+   SET @nowPlusSec=DATEADD (ss, 1, @now)             
+     
+   select top 1 @ConfigurationId = ConfigurationId from Enterprise.GlobalProductConfiguration               
+    where productId =@ProductId and @now between FromDate and Isnull(ThruDate, @nowPlusSec)     
                 
-              INSERT INTO Enterprise.GlobalProductConfiguration(ConfigurationId, ProductId, FromDate, ThruDate)              
-              VALUES (@ConfigurationId, @ProductId, @now, null)       
-       END
-
---3 check if there is already a setting for this type product combo   
-SELECT TOP 1 @TempSettingId = a.ProductSettingId, @productConfigurationId = b.ProductConfigurationId
-FROM Enterprise.ProductSetting a    
-JOIN Enterprise.ProductConfiguration b    
-       ON a.ProductSettingId = b.ProductSettingId 
-       AND b.ConfigurationId = @ConfigurationId     
-       AND b.ThruDate IS null    
-WHERE ProductSettingTypeId = @ProductSettingTypeId               
-       AND ProductId = @ProductId              
-       AND @now BETWEEN a.FromDate AND Isnull(a.ThruDate, @nowPlusSec)    
-
---4 if exists then end the existing prodconfig and add new else, insert into prodconfig
-IF(@TempSettingId IS NOT NULL)              
-       BEGIN
-              --Combo exists
-              --Kill old product configuration
-              UPDATE Enterprise.ProductConfiguration
-              SET ThruDate = GETUTCDATE()
-              WHERE ProductSettingId = @TempSettingId
-                     AND ConfigurationId = @ConfigurationId
-                     AND ProductConfigurationId = @productConfigurationId
-    END 
-
---Inserting new configuration
-INSERT INTO Enterprise.ProductConfiguration(ConfigurationId, ProductSettingId, FromDate, ThruDate)              
-VALUES(@ConfigurationId, @ProductSettingId, @nowPlusSec, null)        
-
-COMMIT;                 
+   if(not exists(select top 1 1 from   
+ Enterprise.ProductSetting a  
+ join Enterprise.ProductConfiguration b  
+  on a.ProductSettingId=b.ProductSettingId and b.ConfigurationId=@ConfigurationId     
+   and b.ThruDate is null    
+    where a.ThruDate is null and Value = @Value and ProductSettingTypeId=@ProductSettingTypeId        
+     and ProductId=@ProductId))        
+   Begin      
+               
+    --if this is a new product that has no settings create a new config for this product              
+    if(@ConfigurationId is null)              
+    begin              
+     insert into Enterprise.Configuration(CreateDate) values (@now)              
+     SET @ConfigurationId=SCOPE_IDENTITY()              
+                
+     insert into Enterprise.GlobalProductConfiguration(ConfigurationId, ProductId, FromDate, ThruDate)              
+     VALUES (@ConfigurationId, @ProductId, @now, null)              
+    END              
+                    
+    --check if there is already a setting for this type product combo       
+      
+ select top 1 @TempSettingId=a.ProductSettingId     
+ from Enterprise.ProductSetting a    
+  join Enterprise.ProductConfiguration b    
+   on a.ProductSettingId=b.ProductSettingId and b.ConfigurationId=@ConfigurationId     
+    and b.ThruDate is null    
+    where ProductSettingTypeId = @ProductSettingTypeId               
+     and ProductId = @ProductId              
+     and @now between a.FromDate and Isnull(a.ThruDate, @nowPlusSec)      
+    
+    
+    
+              
+    --if exists then close it by setting the ThruDate to now              
+    if(@TempSettingId is not null)              
+    begin              
+     update Enterprise.ProductSetting              
+     set ThruDate = @now              
+     where ProductSettingId=@TempSettingId              
+                  
+     update Enterprise.ProductConfiguration              
+     set ThruDate = @now              
+     where ProductSettingId=@TempSettingId and ConfigurationId=@ConfigurationId              
+    END              
+              
+    --Add the Setting              
+    INSERT INTO Enterprise.ProductSetting(ProductId, ProductSettingTypeId, Value, FromDate, ThruDate)              
+    VALUES(@ProductId, @ProductSettingTypeId, @Value, @nowPlusSec, null)              
+    SELECT @ProductSettingId = SCOPE_IDENTITY()              
+              
+    --Add the Setting config               
+    INSERT INTO Enterprise.ProductConfiguration(ConfigurationId, ProductSettingId, FromDate, ThruDate)              
+    VALUES(@ConfigurationId, @ProductSettingId, @nowPlusSec, null)              
+   END        
+  COMMIT;                 
     END TRY                  
     BEGIN CATCH                  
         DECLARE @ErrorLogID INT;                  
@@ -94,4 +89,3 @@ COMMIT;
         ROLLBACK;                  
     END CATCH;                  
 END;
-
