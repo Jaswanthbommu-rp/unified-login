2c6986e66cceb26c951e0a024e168b6e0cb7c7c8
Bryan Way <bryan.way@realpage.com>
2021-03-19 15:49:42 -0500

    Updated GetProductProperties and GetProductRoles to use new ProductIntegrationType

---
+++ Enterprise/Subsystem/ProductLauncher/Component/Landing/Logic/ProductIntegration/Factory/IIntegrationTypeFactory.cs
@@ -0,0 +1,10 @@
+﻿using RP.Enterprise.Subsystem.ProductLauncher.Component.Landing.Logic.ProductIntegration.Types;
+using RP.Enterprise.Subsystem.ProductLauncher.Component.SharedObjects.Landing;
+
+namespace RP.Enterprise.Subsystem.ProductLauncher.Component.Landing.Logic.ProductIntegration.Factory
+{
+    public interface IIntegrationTypeFactory
+    {
+        IIntegrationType GetIntegration(int productId, DefaultUserClaim userClaims);
+    }
+}
