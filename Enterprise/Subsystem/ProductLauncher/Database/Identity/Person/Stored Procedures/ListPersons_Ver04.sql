80747d71b838f7f9ca2eeafe6d4781f869ffce2e
Vivek Vishal <vivek.vishal@RealPage.com>
2021-03-23 07:29:46 -0500

    Merged PR 68949: Defect 761979: Newly Created Regular User (No Email) without Notification emails users are not displaying in user list page
    Defect 761979: Newly Created Regular User (No Email) without Notification emails users are not displaying in user list page
    
    Related work items: #761979

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Person/Stored Procedures/ListPersons_Ver04.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Person/Stored Procedures/ListPersons_Ver04.sql
@@ -473,8 +473,16 @@ BEGIN
 		  FROM #UserLogin ulp  
 			 INNER JOIN Person.Person p ON p.PartyId = ulp.PersonPartyId  
 			 INNER JOIN Enterprise.Party pa ON p.PartyId = pa.PartyID  
-			 INNER JOIN Enterprise.PartyContactMechanism PCM ON ulp.PersonPartyId = PCM.PartyId AND CAST(PCM.ThruDate AS Date) > GetDate()
-			 INNER JOIN Enterprise.ElectronicAddress EA ON PCM.ContactMechanismId=EA.ContactMechanismID
+			 LEFT JOIN
+				(SELECT * 
+				 FROM
+					(
+						SELECT *,ROW_NUMBER() OVER(PARTITION BY PartyId ORDER BY FromDate DESC) AS RowNo
+						FROm Enterprise.PartyContactMechanism
+						WHERE ThruDate > GETUTCDATE()
+					) X
+				WHERE X.RowNo = 1) PCM ON ulp.PersonPartyId = PCM.PartyId
+			 LEFT JOIN Enterprise.ElectronicAddress EA ON PCM.ContactMechanismId=EA.ContactMechanismID
 			 INNER JOIN Enterprise.PartyRelationship prs ON prs.PartyIdFrom = ulp.PersonPartyId AND prs.PartyIdTo = @PartyId  
 			 INNER JOIN Enterprise.RelationshipType rst ON rst.RelationshipTypeId = prs.PartyRelationshipTypeId  
 			 INNER JOIN Enterprise.RoleType rt ON (rt.PartyRoleTypeId = rst.RoleTypeIdValidFrom)  
