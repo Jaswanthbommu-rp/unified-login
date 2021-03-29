38e8aa9b0cc225583f3e7bb5aeddad3122b57def
Vijayasekher Manellore <Vijayasekher.Manellore@RealPage.com>
2021-03-29 16:51:27 -0500

    Merged PR 70092: api update to save orgnization use primary property setting
    api update to save orgnization use primary property setting
    
    Related work items: #712882

---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Enterprise/Stored Procedures/GetOrganizationSettingValue.sql
@@ -0,0 +1,18 @@
+﻿Create PROCEDURE [Enterprise].[GetOrganizationSettingValue] (
+	@PartyId bigint ,
+	@SettingName Nvarchar(50),
+	@SettingValue nvarchar(4000) OUTPUT
+)
+AS
+BEGIN
+	
+	SELECT @SettingValue = MS.Value            
+        FROM Enterprise.MasterConfigurationSetting mcs
+        INNER JOIN Enterprise.MasterConfiguration mc ON mc.MasterConfigurationId = mcs.MasterConfigurationId
+        INNER JOIN Enterprise.MasterSetting ms ON mcs.MasterSettingId = ms.MasterSettingId
+        INNER JOIN Enterprise.MasterSettingType mst ON mst.MasterSettingTypeId = ms.MasterSettingTypeId
+        INNER JOIN Enterprise.MasterConfigurationType mct ON mct.MasterConfigurationTypeId = mst.MasterConfigurationTypeId
+	WHERE MST.Name = @SettingName
+	AND MCT.Name = 'Organization'
+	AND  MC.AttributeId = @PartyId;
+END;
