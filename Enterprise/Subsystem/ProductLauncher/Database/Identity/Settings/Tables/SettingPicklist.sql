76e86fd55252ecdef91d49e254da9d126c6183a8
RRI\vmanellore <test@test.com>
2021-04-09 15:35:07 -0500

    new Settings Management tables and procs

---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Settings/Tables/SettingPicklist.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [Settings].[SettingPicklist]
+(
+	[PicklistID] [int] IDENTITY(1,1) NOT NULL,
+	[CategoryName] [nvarchar](50) NOT NULL,
+	[MappingName] [nvarchar](50) NOT NULL,
+	[MappingValue] [int] NULL,
+	[Description] [nvarchar](100) NULL,
+	[ModifiedBy] [int] NOT NULL,
+	[ModifiedDate] [datetime] NOT NULL,
+	[ThruDate] [datetime] NULL
+) ON [PRIMARY]
+GO
---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Settings/Tables/SettingsMapping.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [Settings].[SettingsMapping]
+(
+	[SettingsMappingId] INT IDENTITY(1,1) NOT NULL,
+	[SettingCategoryTypeId] SMALLINT NOT NULL,
+	[SettingsMappingType] varchar(20) NOT NULL,
+	[ModifiedBy] BIGINT NOT NULL,
+	[CreatedDate] DATETIME   DEFAULT (getdate()) NOT NULL, 
+	[UpdatedDate] DATETIME NULL,
+	CONSTRAINT [PK_SettingsMapping] PRIMARY KEY ([SettingsMappingId]), 
+    CONSTRAINT [FK_SettingsMapping_Category] FOREIGN KEY ([SettingCategoryTypeId]) REFERENCES [Settings].[SettingCategoryType]([SettingCategoryTypeId]) ON DELETE CASCADE ON UPDATE CASCADE
+)
