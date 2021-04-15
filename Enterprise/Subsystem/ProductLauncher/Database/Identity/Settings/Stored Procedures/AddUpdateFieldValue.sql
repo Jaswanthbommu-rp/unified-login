2079952875973604e9e5d47f2d32e29802f4e8a6
Vijayasekher Manellore <Vijayasekher.Manellore@RealPage.com>
2021-04-15 08:11:59 -0500

    Merged PR 73106: new Settings Management tables and procs
    new Settings Management tables and procs
    
    Related work items: #296179

---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Settings/Stored Procedures/AddUpdateFieldValue.sql
@@ -0,0 +1,63 @@
+﻿Create PROCEDURE [Settings].[AddUpdateFieldValue] (
+	 @JSON nvarchar(MAX),
+	 @CreatedBy bigint
+) 
+AS 
+SET NOCOUNT ON 
+BEGIN
+	BEGIN TRY
+		DECLARE @UTCDATE datetime = GETUTCDATE()
+		DECLARE @Inserted TABLE (
+			Id int
+		)
+
+		MERGE [Settings].[SettingTableRowValue] AS target
+		USING (
+			SELECT	[FieldValueId],
+					[UserLoginPersonaId],
+					[FieldId],
+					[Value]
+			FROM OPENJSON(@JSON)
+			WITH (
+				[FieldValueId] bigint '$.FieldValueId',
+				[UserLoginPersonaId] bigint '$.UserLoginPersonaId',
+				[FieldId] bigint '$.FieldId',
+				[Value] nvarchar(100) '$.Value'
+			)) AS source
+		ON (target.SettingTableRowValueId = source.FieldValueId)
+		WHEN MATCHED AND LEN(source.[Value]) = 0 THEN
+			DELETE
+		WHEN MATCHED THEN
+			UPDATE
+			SET	[Value] = source.[Value],
+					[UpdatedDate] = @UTCDATE,
+					[ModifiedBy] = @CreatedBy
+		WHEN NOT MATCHED AND LEN([Value]) > 0 THEN
+			INSERT (
+				UserLoginPersonaId,
+				SettingTableRowId,
+				Value,
+				CreatedDate,
+				ModifiedBy
+			)
+			VALUES (
+				source.UserLoginPersonaId,
+				source.FieldId,
+				source.Value,
+				@UTCDATE,
+				@CreatedBy
+			);
+
+		SELECT	@@ROWCOUNT AS Id,
+						'' AS ErrorMessage
+	END TRY  
+	BEGIN CATCH
+        DECLARE @ErrorLogID int;
+        EXEC dbo.LogError @ErrorLogID = @ErrorLogID OUTPUT;
+
+        SELECT	0 AS Id,
+					ErrorMessage
+        FROM	dbo.ErrorLog
+        WHERE	ErrorLogID = @ErrorLogID;
+	END CATCH
+END
---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Settings/Stored Procedures/UpdateUnifiedSetting.sql
@@ -0,0 +1,134 @@
+﻿CREATE PROCEDURE [Settings].[UpdateUnifiedSetting] (
+	@PartyId bigint,
+	@Category Varchar(50),
+	@JsonUnifiedSettings nvarchar(max),
+	@CreatedBy bigint)
+AS
+BEGIN
+	BEGIN TRY
+		DECLARE @settings TABLE (
+			Id int identity,
+			MappingName varchar(200),
+			MappingValue varchar(100),
+			Editable bit,
+			Hidden bit
+		)
+
+		Insert Into @settings(MappingName,MappingValue,Editable,Hidden)
+		SELECT	Name,Value,Editable,Hidden
+		FROM	OPENJSON (@JsonUnifiedSettings)
+		WITH (	Name varchar(max) '$.Name',
+				Value varchar(max) '$.Value',
+				Editable bit '$.Editable',
+				Hidden bit '$.Hidden')
+		WHERE	ISJSON(@JsonUnifiedSettings) > 0
+
+		declare @MAX_ID INT
+		declare @Current_ID INT = 1
+		declare @mappingName varchar(200)
+		Declare @mappingValue varchar(100)
+		Declare @editable bit,@hidden bit
+		Declare @categoryId int
+
+		Select @categoryId = SettingCategoryTypeId 
+		From [Settings].[SettingCategoryType] 
+		Where [Name] = @Category
+
+		IF (@categoryId IS NULL)
+		BEGIN
+			INSERT INTO [Settings].[SettingCategoryType] ([Name])
+			SELECT @Category
+
+			SET @categoryId = SCOPE_IDENTITY();
+		END
+
+		select @MAX_ID = max(Id) from @settings
+
+		WHILE @Current_ID <= @MAX_ID
+		begin
+			select @mappingName = MappingName,
+				   @mappingValue = MappingValue,
+				   @editable = Editable,@hidden = Hidden
+				from @settings where Id = @Current_ID
+			
+			
+			IF ((NULLIF(LTRIM(RTRIM(@mappingName)), '') IS NOT NULL) AND
+				(NULLIF(LTRIM(RTRIM(@mappingValue)), '') IS NOT NULL))
+			BEGIN
+				IF @mappingName IN ('Login', 'ForcedLock', 'NewUserRegistration')
+				BEGIN
+				
+					UPDATE iac SET	iac.ActivityTokenExpirationMinutes =
+							CASE
+								WHEN @mappingValue > 0 THEN @mappingValue
+								ELSE iac.ActivityTokenExpirationMinutes
+							END
+					FROM [Ident].[ActivityConfiguration] iac
+					INNER JOIN Ident.ActivityType iat ON iat.ActivityTypeId = iac.ActivityTypeId	
+					WHERE iat.ActivityCode = 'ForcedLock'
+					AND @mappingName = 'ForcedLock'
+					AND PartyId = @PartyId
+
+					UPDATE iac SET	iac.MaxActivityAttemptCount =
+							CASE
+								WHEN @mappingValue > 0 THEN @mappingValue
+								ELSE iac.MaxActivityAttemptCount
+							END
+					FROM [Ident].[ActivityConfiguration] iac
+					INNER JOIN Ident.ActivityType iat ON iat.ActivityTypeId = iac.ActivityTypeId	
+					WHERE iat.ActivityCode = 'Login'
+					AND @mappingName = 'Login'
+					AND PartyId = @PartyId
+
+					UPDATE iac SET	iac.ActivityTokenExpirationMinutes =
+							CASE
+								WHEN @mappingValue > 0 AND @mappingValue % 1 = 0 THEN @mappingValue * 1440
+								WHEN @mappingValue IS NOT NULL THEN @mappingValue
+								ELSE iac.ActivityTokenExpirationMinutes
+							END
+					FROM [Ident].[ActivityConfiguration] iac
+					INNER JOIN Ident.ActivityType iat ON iat.ActivityTypeId = iac.ActivityTypeId	
+					WHERE iat.ActivityCode = 'NewUserRegistration'
+					AND @mappingName = 'NewUserRegistration'
+					AND PartyId = @PartyId
+				END
+				ELSE
+				BEGIN
+					IF NOT EXISTS (SELECT 1 FROM [Settings].[OrganizationSettings]
+								WHERE MappingName = @mappingName
+								And PartyId = @PartyId
+								And SettingCategoryTypeId = @categoryId)
+
+					BEGIN
+						INSERT INTO [Settings].[OrganizationSettings](PartyId,SettingCategoryTypeId,MappingName,
+							MappingValue,Editable,Hidden,CreatedBy,CreatedDate)
+						SELECT @PartyId,@categoryId,@mappingName,@mappingValue,@editable,@hidden,@CreatedBy,GETUTCDATE()
+					END
+					ELSE
+					BEGIN
+						UPDATE [Settings].[OrganizationSettings] SET MappingValue = @mappingValue,
+								UpdatedDate = GETUTCDATE()
+						WHERE MappingName = @mappingName
+						And PartyId = @PartyId
+						And SettingCategoryTypeId = @categoryId
+					END
+				END
+				
+			END
+			set @Current_ID = @Current_ID + 1
+		end
+		SELECT	COUNT(Id) AS Id,
+					'' AS ErrorMessage
+		FROM @settings
+
+	END TRY
+	BEGIN CATCH
+        DECLARE @ErrorLogID int;
+        EXEC dbo.LogError @ErrorLogID = @ErrorLogID OUTPUT;
+
+        SELECT	0 AS Id,
+					ErrorMessage
+        FROM	dbo.ErrorLog
+        WHERE	ErrorLogID = @ErrorLogID;
+	END CATCH
+END
