21a34615eff511693d8e1587b8135f94d9f0f880
Siva Muni Venkatasiva <siva.savaram@realpage.com>
2021-04-16 19:14:54 +0530

    Reverting Code Changes for ILM and ILMLA User Story

--- Enterprise/Subsystem/ProductLauncher/Component/Landing/Logic/ProductIntegration/ManageProductInvokerBase.cs
+++ Enterprise/Subsystem/ProductLauncher/Component/Landing/Logic/ProductIntegration/ManageProductInvokerBase.cs
@@ -49,7 +49,7 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Component.Landing.Logic.Produc
         protected string ProductApiBaseUrl { get; set; }
         protected string CompanyInstanceSourceId { get; set; }
         protected IList<ProductInternalSetting> ProductInternalSettingList { get; set; }
-
+        protected bool CreateUpdateMultiCompanyUserRequiresPMC { get; private set; }
         /// <summary>
         /// Correlation Id used for logging
         /// </summary>
@@ -558,14 +558,17 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Component.Landing.Logic.Produc
         public virtual string CreateUpdateProductUser(ProductUserRolePropertiesGroups userRolePropertiesRegion, BatchProcessType batchProcessType = BatchProcessType.CreateUpdateProductUser)
         {
             string result;
+            bool isProductUser = false;
 
             WriteToDiagnosticLog(
                 $"ManageProductInvokerBase.CreateUpdateProductUser - Product {ProductType} editorPersona id - {EditorUserDetails.PersonaId}. At beginning of method.");
 
             // Get product user object 
             var newProductUser = GenerateProductUserObject(userRolePropertiesRegion);
+            var productUser = GetBaseUserDataFromProduct(newProductUser.LoginName);
+            isProductUser = productUser != null && !string.IsNullOrEmpty(productUser.LoginName);
 
-            if (string.IsNullOrEmpty(SubjectUserDetails.ProductUserName))
+            if (string.IsNullOrEmpty(SubjectUserDetails.ProductUserName) && !isProductUser)
             {
                 WriteToDiagnosticLog(
                     $"ManageProductInvokerBase.CreateUpdateProductUser - Product {ProductType} editorPersona id - {EditorUserDetails.PersonaId}. Calling CreateUser.");
@@ -584,18 +587,100 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Component.Landing.Logic.Produc
                 result = CreateUser(newProductUser);
 
             }
+            else if (string.IsNullOrEmpty(SubjectUserDetails.ProductUserName) && isProductUser && CreateUpdateMultiCompanyUserRequiresPMC)
+            {
+                result = CreateMultiCompanyUser(productUser);
+            }
             else
             {
                 WriteToDiagnosticLog(
                     $"ManageProductInvokerBase.CreateUpdateProductUser - Product {ProductType} editorPersona id - {EditorUserDetails.PersonaId}. Calling UpdateUser.");
                 // Update user with Id/Login from product
-                newProductUser.UserId = SubjectUserDetails.ProductUserId;
-                newProductUser.LoginName = SubjectUserDetails.ProductUserName;
-
+                if (isProductUser)
+                {
+                    newProductUser.UserId = productUser.UserId;
+                    newProductUser.LoginName = productUser.LoginName;
+                }
+                else
+                {
+                    newProductUser.UserId = SubjectUserDetails.ProductUserId;
+                    newProductUser.LoginName = SubjectUserDetails.ProductUserName;
+                }
                 result = UpdateUser(newProductUser, batchProcessType);
             }
 
             return result;
+        }
+
+        #region private
+        public virtual IntegrationProductUser GetBaseUserDataFromProduct(string loginNameToCheck, string baseUrlAndQuery = null)
+        {
+            if (string.IsNullOrEmpty(baseUrlAndQuery))
+                baseUrlAndQuery = GetOperationEndPoint(ProductEntityEndpointKeyEnum.GetUserEndpoint);
+
+            bool isCompanyIdRequiredToQuery = baseUrlAndQuery.Contains("{0}");
+            if (isCompanyIdRequiredToQuery)
+            {
+                baseUrlAndQuery = string.Format(baseUrlAndQuery, CompanyInstanceSourceId, loginNameToCheck);
+            }
+            else
+            {
+                baseUrlAndQuery = string.Format(baseUrlAndQuery, loginNameToCheck);
+            }
+            WriteToDiagnosticLog(
+              $"ManageProductInvokerBase.GetProductUser - Product {ProductType} editorPersona id - {EditorUserDetails.PersonaId}. At beginning of the method.");
+
+            var productUser = GetResultFromApi<IntegrationProductUser>(baseUrlAndQuery, false);
+
+            WriteToDiagnosticLog(
+                $"ManageProductInvokerBase.GetProductUser - Product {ProductType} editorPersona id - {EditorUserDetails.PersonaId}. Calling API - {baseUrlAndQuery}.");
+
+            return productUser;
+        }
+        #endregion
+
+
+        /// <summary>
+        /// Create a user in the product
+        /// </summary>
+        protected virtual string CreateMultiCompanyUser(IntegrationProductUser productUser)
+        {
+            WriteToDiagnosticLog(
+                $"ManageProductInvokerBase.CreateUser - Product {ProductType} editorPersona id - {EditorUserDetails.PersonaId}. At beginning of the method.");
+
+            var baseUrlAndQuery = GetOperationEndPoint(ProductEntityEndpointKeyEnum.PostUserEndpoint);
+
+            WriteToDiagnosticLog(
+                $"ManageProductInvokerBase.CreateUser - Product {ProductType} editorPersona id - {EditorUserDetails.PersonaId}. Calling API - {baseUrlAndQuery}.");
+
+            // dump api info
+            DumpApiCallInfoToDiagnosticLog(baseUrlAndQuery, productUser);
+
+            var integration = new ApiIntegration(_httpClient, baseUrlAndQuery);
+            var result = integration.PutEntity<IntegrationProductUser>(productUser);
+
+            if (result.IsSuccessStatusCode)
+            {
+                WriteToDiagnosticLog(
+                    $"ManageProductInvokerBase.CreateUser - Product {ProductType} editorPersona id - {EditorUserDetails.PersonaId}. Received success. Updating Geenbook mapping.");
+
+                // map product user in green book
+                _dataCollector.CreateProductUserInGreenBook(SubjectUserDetails.PersonaId, result.Content, ProductId, productUser.LoginName);
+
+                // OPTIONAL - If product needs more attributes than userid or loginName then override in the product (e.g. PAM uses)
+                CreateAdditionalSamlUserAttribute(SubjectUserDetails.PersonaId, ProductId, productUser);
+
+                // activity logging
+                ProductActivityLogger.WriteCreateUserActivityLog(EditorUserDetails, SubjectUserDetails, BlueBookGbProductMap.Name, BlueBookGbProductMap.BooksProductCode,
+                    CorrelationId);
+
+                return string.Empty;
+            }
+
+            WriteToErrorLog(
+                $"ManageProductInvokerBase.CreateUser - Product {ProductType} editorPersona id - {EditorUserDetails.PersonaId}. result received - {result}.");
+
+            return result.Content;
         }
 
         /// <summary>
--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/2021Q1_WMU2Release.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/2021Q1_WMU2Release.sql
@@ -7,3 +7,79 @@ UPDATE Enterprise.ProductSetting SET value = '/{0}/users?loginName={1}'
 WHERE ProductId IN (40, 41) AND ProductSettingTypeId = @ProductSettingTypeId
 END
 GO
+
+--Adding Product Setting Type for ILM & ILM LA
+			   
+  BEGIN TRAN
+
+-- Add ProductIcon product settings
+
+IF NOT EXISTS (SELECT TOP 1 1 FROM Enterprise.ProductSettingType WHERE [Name] = 'CreateUpdateMultiCompanyUserRequiresPMC')
+BEGIN
+	INSERT INTO Enterprise.ProductSettingType ([Name], [Description], SensitiveData)
+	VALUES ('CreateUpdateMultiCompanyUserRequiresPMC', 'Create Update MultiCompany User Requires PMC', 0);
+END
+
+DECLARE @NOW DATETIME = GETUTCDATE();
+declare @productlist table ( entid int identity, productid int, productsettingtype varchar(500), productsettingvalue varchar(100))
+insert into @productlist values
+(40,  'CreateUpdateMultiCompanyUserRequiresPMC','1'),
+(41,  'CreateUpdateMultiCompanyUserRequiresPMC','1');
+
+declare @MAX_ID INT
+declare @Current_ID INT = 1
+declare @CurrentProductId INT = 1
+
+select @MAX_ID = max(entid) from @productlist
+
+while @Current_ID <= @MAX_ID
+begin
+	declare @currentSettingType varchar(500)
+	declare @currentsettingValue varchar(2000)
+
+	select @CurrentProductId = productid , @currentSettingType = productsettingtype, @currentSettingValue = productsettingvalue
+		from @productlist where entid = @Current_ID
+
+	--print 'productid = ' + convert(varchar,@currentproductid)
+
+	if not exists (
+	select top 1 1 
+		FROM Enterprise.GlobalProductConfiguration gpc  
+		JOIN Enterprise.ProductConfiguration pc ON pc.ConfigurationId = gpc.ConfigurationId  
+		JOIN Enterprise.ProductSetting ps ON ps.ProductSettingId = pc.ProductSettingId  
+		JOIN Enterprise.ProductSettingType pst ON pst.ProductSettingTypeId = ps.ProductSettingTypeId  
+			WHERE  gpc.ProductId = @CurrentProductId  
+		AND ((@NOW BETWEEN gpc.FromDate AND gpc.ThruDate) OR (@NOW >= gpc.FromDate AND gpc.ThruDate IS NULL))  
+		AND ((@NOW BETWEEN pc.FromDate AND pc.ThruDate) OR (@NOW >= pc.FromDate AND pc.ThruDate IS NULL))  
+		AND ((@NOW BETWEEN ps.FromDate AND ps.ThruDate) OR (@NOW >= ps.FromDate AND ps.ThruDate IS NULL))  
+		AND pst.Name = @currentSettingType
+		AND ps.Value = @currentsettingValue
+	)
+	begin
+		declare @currentproductconfigurationid INT
+		select distinct top 1 @currentproductconfigurationid = pc.configurationid
+			FROM Enterprise.GlobalProductConfiguration gpc  
+			JOIN Enterprise.ProductConfiguration pc ON pc.ConfigurationId = gpc.ConfigurationId  
+			JOIN Enterprise.ProductSetting ps ON ps.ProductSettingId = pc.ProductSettingId  
+			JOIN Enterprise.ProductSettingType pst ON pst.ProductSettingTypeId = ps.ProductSettingTypeId  
+				WHERE  gpc.ProductId = @CurrentProductId
+			AND ((@NOW BETWEEN gpc.FromDate AND gpc.ThruDate) OR (@NOW >= gpc.FromDate AND gpc.ThruDate IS NULL))  
+			AND ((@NOW BETWEEN pc.FromDate AND pc.ThruDate) OR (@NOW >= pc.FromDate AND pc.ThruDate IS NULL))  
+			AND ((@NOW BETWEEN ps.FromDate AND ps.ThruDate) OR (@NOW >= ps.FromDate AND ps.ThruDate IS NULL))  
+		order by pc.ConfigurationId desc
+
+		if (@currentproductconfigurationid is not null)
+		begin
+			insert into enterprise.ProductSetting ( productid, ProductSettingTypeId, value, FromDate )
+				select @CurrentProductId, productsettingtypeid, @currentSettingValue, GETUTCDATE()
+					from enterprise.ProductSettingType where name = @currentSettingType
+			insert into enterprise.ProductConfiguration ( ConfigurationId, ProductSettingId, FromDate, ThruDate )
+				values ( @currentproductconfigurationid, @@IDENTITY, GETUTCDATE(), null )
+		end
+	end
+	
+	set @Current_ID = @Current_ID + 1
+end
+
+COMMIT TRAN;
+GO
