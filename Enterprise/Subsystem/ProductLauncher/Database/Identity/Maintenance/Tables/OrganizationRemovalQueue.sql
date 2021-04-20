1e6f921dcc954e6b92ec1951f2c75cb575b0fad9
James Reames <james.reames@realpage.com>
2021-04-20 16:49:28 -0500

    adding new org delete tables

---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Maintenance/Tables/OrganizationRemovalQueue.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [Maintenance].[OrganizationRemovalQueue]
+(
+	[OrganizationRemovalQueueId] INT NOT NULL,
+	[OrganizationPartyId] BIGINT NOT NULL,
+	[OrganizationRealPageId] UNIQUEIDENTIFIER NOT NULL,
+	[OrganizationRemovalQueueStatusId] INT NOT NULL DEFAULT 0, 
+	[AddedDate] DATETIME2 NOT NULL DEFAULT GETUTCDATE(), 
+	[CompletedDate] DATETIME2 NULL,
+	CONSTRAINT [PK_OrganizationRemovalQueueId] PRIMARY KEY CLUSTERED ([OrganizationRemovalQueueId]),
+    CONSTRAINT [FK_OrganizationRemovalQueue_OrganizationRemovalQueueStatus_OrganizationRemovalQueueStatusId] FOREIGN KEY ([OrganizationRemovalQueueStatusId]) REFERENCES [Maintenance].[OrganizationRemovalQueueStatus]([OrganizationRemovalQueueStatusId]),
+	CONSTRAINT [FK_OrganizationRemovalQueue_Organization_OrganizationPartyId] FOREIGN KEY ([OrganizationPartyId]) REFERENCES [Enterprise].[Organization]([PartyId]),
+)
