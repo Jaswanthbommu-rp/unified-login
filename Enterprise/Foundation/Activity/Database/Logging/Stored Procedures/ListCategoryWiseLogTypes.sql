64d3686ada96292dc66a2c0790b9b1e88a7189e5
Markandeyulu Kativarapu <markandeyulu.kativarapu@RealPage.com>
2021-04-22 01:05:47 -0500

    Merged PR 74087: User Story 775005: UL: Add new Customer Facing Activity types for Settings,User Story 772341: UL: Create new "application ID" for Settings Admin Console with its own list of activity types.
    Related work items: #772341, #775005

---
+++ Enterprise/Foundation/Activity/Database/Logging/Stored Procedures/ListCategoryWiseLogTypes.sql
@@ -0,0 +1,20 @@
+﻿
+CREATE PROCEDURE [Logging].[ListCategoryWiseLogTypes]
+(
+	 @LogCategoryTypeId INT 
+)
+AS
+BEGIN
+SET NOCOUNT ON 
+ 
+	SELECT 
+		LogTypeId
+		,LogcategoryTypeId
+		,[Name]
+		,[Description]
+	FROM 
+		[Logging].[LogType] 
+	WHERE
+		LogCategoryTypeId  = @LogCategoryTypeId
+	 
+END
