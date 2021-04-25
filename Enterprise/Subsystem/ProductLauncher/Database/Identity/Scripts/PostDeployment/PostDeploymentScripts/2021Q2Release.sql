1a331c4737d5ec2ccef4f015af50bbfd5bf2c8d1
Rohith Vundyala <Rohith.Vundyala@RealPage.com>
2021-04-25 11:36:07 -0500

    Script for add products/dependent products automatically on company create

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/2021Q2Release.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/2021Q2Release.sql
@@ -1322,3 +1322,86 @@ BEGIN
 	SET IDENTITY_INSERT [UserManagement].[Control] OFF
 END
 GO
+--START : script for userstory #771257
+--AlwaysEnableProductForOrgType 
+if not exists ( select top 1 1 from Enterprise.ProductSettingType where name = 'AlwaysEnableProductForOrgType' )
+begin
+	insert into enterprise.ProductSettingType ( name, Description, SensitiveData ) values ( 'AlwaysEnableProductForOrgType', 'Always Enable Products For OrgType', 0)
+end
+--EnableProductOnOtherProductsActivation
+if not exists ( select top 1 1 from Enterprise.ProductSettingType where name = 'EnableProductOnOtherProductsActivation' )
+begin
+	insert into enterprise.ProductSettingType ( name, Description, SensitiveData ) values ( 'EnableProductOnOtherProductsActivation', 'Enable Product On other Products Activation', 0)
+end
+
+DECLARE @NOW DATETIME = GETUTCDATE(); 
+declare @productlist table ( entid int identity, productid int, productsettingtype varchar(500), productsettingvalue varchar(2000))
+insert into @productlist values 
+	(3, 'AlwaysEnableProductForOrgType', 'Multifamily,Vendor,Other'),
+	(14, 'AlwaysEnableProductForOrgType', 'Multifamily,Other'),
+	(19, 'AlwaysEnableProductForOrgType', 'Multifamily,Vendor,Other'),
+	(27, 'AlwaysEnableProductForOrgType', 'Multifamily,Other'),
+	(28, 'AlwaysEnableProductForOrgType', 'Multifamily,Vendor,Other'),
+	(38, 'AlwaysEnableProductForOrgType', 'Vendor'),
+	(45, 'AlwaysEnableProductForOrgType', 'Multifamily,Vendor,Other'),
+	(56, 'AlwaysEnableProductForOrgType', 'Multifamily,Vendor,Other'),
+	(39, 'EnableProductOnOtherProductsActivation', '1')
+	
+--select * from @productlist
+
+declare @MAX_ID INT
+declare @Current_ID INT = 1
+declare @CurrentProductId INT = 1
+
+select @MAX_ID = max(entid) from @productlist
+
+while @Current_ID <= @MAX_ID
+begin
+	declare @currentSettingType varchar(500)
+	declare @currentsettingValue varchar(2000)
+
+	select @CurrentProductId = productid , @currentSettingType = productsettingtype, @currentSettingValue = productsettingvalue
+		from @productlist where entid = @Current_ID
+
+	--print 'productid = ' + convert(varchar,@currentproductid)
+
+	if not exists (
+	select top 1 1 
+		FROM Enterprise.GlobalProductConfiguration gpc  
+		JOIN Enterprise.ProductConfiguration pc ON pc.ConfigurationId = gpc.ConfigurationId  
+		JOIN Enterprise.ProductSetting ps ON ps.ProductSettingId = pc.ProductSettingId  
+		JOIN Enterprise.ProductSettingType pst ON pst.ProductSettingTypeId = ps.ProductSettingTypeId  
+			WHERE  gpc.ProductId = @CurrentProductId  
+		AND ((@NOW BETWEEN gpc.FromDate AND gpc.ThruDate) OR (@NOW >= gpc.FromDate AND gpc.ThruDate IS NULL))  
+		AND ((@NOW BETWEEN pc.FromDate AND pc.ThruDate) OR (@NOW >= pc.FromDate AND pc.ThruDate IS NULL))  
+		AND ((@NOW BETWEEN ps.FromDate AND ps.ThruDate) OR (@NOW >= ps.FromDate AND ps.ThruDate IS NULL))  
+		AND pst.Name = @currentSettingType
+		AND ps.Value = @currentsettingValue
+	)
+	begin
+		declare @currentproductconfigurationid INT
+		select distinct top 1 @currentproductconfigurationid = pc.configurationid
+			FROM Enterprise.GlobalProductConfiguration gpc  
+			JOIN Enterprise.ProductConfiguration pc ON pc.ConfigurationId = gpc.ConfigurationId  
+			JOIN Enterprise.ProductSetting ps ON ps.ProductSettingId = pc.ProductSettingId  
+			JOIN Enterprise.ProductSettingType pst ON pst.ProductSettingTypeId = ps.ProductSettingTypeId  
+				WHERE  gpc.ProductId = @CurrentProductId
+			AND ((@NOW BETWEEN gpc.FromDate AND gpc.ThruDate) OR (@NOW >= gpc.FromDate AND gpc.ThruDate IS NULL))  
+			AND ((@NOW BETWEEN pc.FromDate AND pc.ThruDate) OR (@NOW >= pc.FromDate AND pc.ThruDate IS NULL))  
+			AND ((@NOW BETWEEN ps.FromDate AND ps.ThruDate) OR (@NOW >= ps.FromDate AND ps.ThruDate IS NULL))  
+		order by pc.ConfigurationId desc
+
+		if (@currentproductconfigurationid is not null)
+		begin
+			insert into enterprise.ProductSetting ( productid, ProductSettingTypeId, value, FromDate )
+				select @CurrentProductId, productsettingtypeid, @currentSettingValue, GETUTCDATE()
+					from enterprise.ProductSettingType where name = @currentSettingType
+			insert into enterprise.ProductConfiguration ( ConfigurationId, ProductSettingId, FromDate, ThruDate )
+				values ( @currentproductconfigurationid, @@IDENTITY, GETUTCDATE(), null )
+		end
+	end
+	
+	set @Current_ID = @Current_ID + 1
+end
+GO
+--END : script for userstory #771257
