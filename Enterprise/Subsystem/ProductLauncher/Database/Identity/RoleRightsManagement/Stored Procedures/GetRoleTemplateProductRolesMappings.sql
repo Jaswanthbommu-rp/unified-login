dcf72c742d6bead4d3d08bc533a5944e9948692c
Rohith Vundyala <rohith.vundyala@RealPage.com>
2021-05-12 08:51:44 -0500

    Merged PR 79814: Role template proc change
    Role template proc change
    
    Related work items: #804807

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/RoleRightsManagement/Stored Procedures/GetRoleTemplateProductRolesMappings.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/RoleRightsManagement/Stored Procedures/GetRoleTemplateProductRolesMappings.sql
@@ -1,4 +1,3 @@
---exec [Security].[GetRoleTemplateProductRolesMappings] 11,350
 CREATE PROCEDURE [Security].[GetRoleTemplateProductRolesMappings]
 (
 	@RoleTemplateId int, 
@@ -10,16 +9,16 @@ SELECT
 	,rt.PartyID
 	,rt.RoleTemplateName
 	,rt.RoleTemplateDescription
-	,rtp.RoleTemplateProductId
-	,rtp.ProductId
+	,ISNULL(rtp.RoleTemplateProductId,0) AS RoleTemplateProductId
+	,ISNULL(rtp.ProductId,0) as ProductId
 	,ISNULL(rprm.RoleTemplateProductRoleMappingId,0) AS RoleTemplateProductRoleMappingId
-	,rprm.ProductRoleId
+	,ISNULL(rprm.ProductRoleId,0) as ProductRoleId
 	,rprm.ProductRoleName
 	,ISNULL(rtprm.RoleTemplateAdditionalProductRoleMappingId,0) AS RoleTemplateAdditionalProductRoleMappingId
 	,rtprm.AttributeName
 	,rtprm.AttributeValue
 FROM Security.RoleTemplate rt
-	INNER JOIN Security.RoleTemplateProduct rtp ON rt.RoleTemplateId = rtp.RoleTemplateId
+	LEFT OUTER  JOIN Security.RoleTemplateProduct rtp ON rt.RoleTemplateId = rtp.RoleTemplateId
 	LEFT OUTER JOIN Security.RoleTemplateProductRoleMapping rprm ON rprm.RoleTemplateProductId = rtp.RoleTemplateProductId
 	LEFT OUTER JOIN Security.RoleTemplateAdditionalProductRoleMapping rtprm ON rtprm.RoleTemplateProductId = rtp.RoleTemplateProductId
 
