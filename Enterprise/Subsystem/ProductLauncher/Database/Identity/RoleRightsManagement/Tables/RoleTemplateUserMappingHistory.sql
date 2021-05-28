19c7659e4622d8e96068437e9d8fda7eb643f1de
Rohith Vundyala <rohith.vundyala@RealPage.com>
2021-05-28 14:31:10 -0500

    Merged PR 83713: Added History table For RoleTemplateUserMapping
    Added History table For RoleTemplateUserMapping
    
    Related work items: #833663

---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/RoleRightsManagement/Tables/RoleTemplateUserMappingHistory.sql
@@ -0,0 +1,11 @@
+CREATE TABLE [Security].[RoleTemplateUserMappingHistory] (
+	 [RoleTemplateUserMappingId]		INT IDENTITY
+	,[RoleTemplateId]					INT				NOT NULL
+    ,[PersonaId]						BIGINT			NOT NULL
+	,[SysStartDateTime]					DATETIME2		NOT NULL
+	,[SysEndDateTime]					DATETIME2		NOT NULL
+)
+
+GO
+CREATE CLUSTERED INDEX [ix_RoleTemplateUserMappingHistory]
+    ON [Security].[RoleTemplateUserMappingHistory]([SysEndDateTime] ASC, [SysStartDateTime] ASC) WITH (DATA_COMPRESSION = PAGE);
