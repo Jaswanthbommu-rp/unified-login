f2c341d5e96fdab3a5308cae37d85d287213b537
Rohith Vundyala <rohith.vundyala@RealPage.com>
2021-05-28 13:30:50 -0500

    Merged PR 83693: Create Role Template User Mapping table
    Create Role Template User Mapping table
    
    Related work items: #833663

---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/RoleRightsManagement/Tables/RoleTemplateUserMapping.sql
@@ -0,0 +1,55 @@
+CREATE TABLE [Security].[RoleTemplateUserMapping] (
+	[RoleTemplateUserMappingId]		INT IDENTITY
+	,[RoleTemplateId]				INT				NOT NULL
+    ,[PersonaId]					BIGINT			NOT NULL
+	,[SysStartDateTime]				DATETIME2 GENERATED ALWAYS AS ROW START NOT NULL
+	,[SysEndDateTime]				DATETIME2 GENERATED ALWAYS AS ROW END NOT NULL
+	,PERIOD FOR SYSTEM_TIME (SysStartDateTime,SysEndDateTime)
+    ,CONSTRAINT [PK_RoleTemplateUserMappingId] PRIMARY KEY CLUSTERED ([RoleTemplateUserMappingId] ASC)
+    ,CONSTRAINT [FK_RoleTemplateUserMapping_RoleTemplateId] FOREIGN KEY ([RoleTemplateId]) REFERENCES [Security].[RoleTemplate] ([RoleTemplateId])
+	,CONSTRAINT [FK_RoleTemplateUserMapping_PersonaId] FOREIGN KEY ([PersonaId]) REFERENCES [Person].[Persona] ([PersonaId])
+)
+WITH (SYSTEM_VERSIONING = ON (HISTORY_TABLE=[Security].[RoleTemplateUserMappingHistory], DATA_CONSISTENCY_CHECK=ON));
+GO
+
+EXECUTE sp_addextendedproperty
+	@name = N'MS_Description',
+	@value = N'This table contains Role Template User Mapping',
+	@level0type = N'SCHEMA',
+	@level0name = N'Security',
+	@level1type = N'TABLE',
+	@level1name = N'RoleTemplateUserMapping';
+GO
+
+EXECUTE sp_addextendedproperty
+	@name = N'MS_Description',
+	@value = N'RoleTemplate User Mapping Id.',
+	@level0type = N'SCHEMA',
+	@level0name = N'Security',
+	@level1type = N'TABLE',
+	@level1name = N'RoleTemplateUserMapping',
+	@level2type = N'COLUMN',
+	@level2name = N'RoleTemplateUserMappingId';
+GO
+
+EXECUTE sp_addextendedproperty
+	@name = N'MS_Description',
+	@value = N'Role Template Id',
+	@level0type = N'SCHEMA',
+	@level0name = N'Security',
+	@level1type = N'TABLE',
+	@level1name = N'RoleTemplateUserMapping',
+	@level2type = N'COLUMN',
+	@level2name = N'RoleTemplateId';
+GO
+
+EXECUTE sp_addextendedproperty
+	@name = N'MS_Description',
+	@value = N'Persona Id',
+	@level0type = N'SCHEMA',
+	@level0name = N'Security',
+	@level1type = N'TABLE',
+	@level1name = N'RoleTemplateUserMapping',
+	@level2type = N'COLUMN',
+	@level2name = N'PersonaId';
+GO
