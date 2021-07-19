ea95f6ef8d6d3e285e2f5324c84381c6be0e2019
Jaswanth Bommu <Jaswanth.Bommu@RealPage.com>
2021-07-19 01:58:19 -0500

    Merged PR 92997: Added new property endpoint
    Manual merge! Added new property endpoint
    
    Related work items: #826517

--- Enterprise/Subsystem/ProductLauncher/Component/Landing/Logic/ProductIntegration/Types/LegacyIntegrationType.cs
+++ Enterprise/Subsystem/ProductLauncher/Component/Landing/Logic/ProductIntegration/Types/LegacyIntegrationType.cs
@@ -390,23 +390,7 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Component.Landing.Logic.Produc
             return result;
         }
 
-        public ListResponse GetEnterpriseProperties(long userPersonaId, string include)
-        {
-            ListResponse productResponse = null;
-
-            switch (_productId)
-            {
-                case (int)ProductEnum.OpsBuyer:
-                    IManageProductOps manageProductOps = new ManageProductOps(_userClaims);
-                    productResponse = manageProductOps.GetCompanyAssets(_userClaims.PersonaId, 0, false, null);
-                    break;
-                case (int)ProductEnum.UnifiedPlatform:
-                    productResponse = _manageUnifiedLogin.GetEnterpriseProperties(_userClaims.PersonaId, include);
-                    break;
-            }
-
-            return productResponse;
-        }
+        public ListResponse GetEnterpriseProperties(long userPersonaId) => GetProperties(userPersonaId, userPersonaId, new RequestParameter());
 
         public ListResponse GetRightsForRole(long editorPersonaId, long userPersonaId, long roleId, long partyId, bool assignedToRoleOnly, RequestParameter dataFilter)
         {
