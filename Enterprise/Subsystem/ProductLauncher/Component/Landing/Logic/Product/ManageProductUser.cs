0aab9f659416d8ba55ad97929d7611e41481b0b4
Jhuman Chhetri <jhuman.chhetri@RealPage.com>
2021-07-21 09:38:03 -0500

    Merged PR 93682: uppercase
    uppercase
    
    Related work items: #761282

--- Enterprise/Subsystem/ProductLauncher/Component/Landing/Logic/Product/ManageProductUser.cs
+++ Enterprise/Subsystem/ProductLauncher/Component/Landing/Logic/Product/ManageProductUser.cs
@@ -720,7 +720,7 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Component.Landing.Logic.Produc
                     {
                         var message = GenerateQueueMessage(fromUserLogInfo, toUserLogInfo, failedRecords, false);
                         PushToQueue(fromUserLogInfo, toUserLogInfo, message);
-                        SendNotification(message + " Please contact RealPage support for assistance.", fromPersonaId);
+                        SendNotification(message + " Please contact RealPage Support for assistance.", fromPersonaId);
                     }
 
                     //update status
