79a282f61800bda25dd1aa6eb43181e212136e6e
Rohith Vundyala <rohith.vundyala@RealPage.com>
2021-08-16 16:03:13 -0500

    Merged PR 98500: Add user count column on enterprise role edit
    Add user count column on enterprise role edit
    
    Related work items: #800312

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/RoleRightsManagement/Stored Procedures/GetRoleTemplateProductRolesMappings.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/RoleRightsManagement/Stored Procedures/GetRoleTemplateProductRolesMappings.sql
@@ -5,6 +5,20 @@ CREATE PROCEDURE [Security].[GetRoleTemplateProductRolesMappings]
     @PartyId BIGINT = NULL
 )
 AS
+BEGIN
+WITH
+RoleTemplateUser   
+ AS    
+ (    
+  SELECT     
+   RT.RoleTemplateId as RoleTemplateId     
+   ,count(RTP.PersonaId) as UserCount    
+  FROM Security.RoleTemplate RT    
+  LEFT OUTER JOIN Security.RoleTemplateUserMapping RTP on RTP.RoleTemplateId = RT.roleTemplateId   
+  LEFT OUTER JOIN Person.Persona P ON P.PersonaId = RTP.PersonaId  
+  GROUP BY RT.RoleTemplateId    
+ ) 
+
 SELECT 
 	rt.RoleTemplateId
 	,rt.PartyID
@@ -18,14 +32,16 @@ SELECT
 	,ISNULL(rtprm.RoleTemplateAdditionalProductRoleMappingId,0) AS RoleTemplateAdditionalProductRoleMappingId
 	,rtprm.AttributeName
 	,rtprm.AttributeValue
+	,RTUM.UserCount as Users 
 FROM Security.RoleTemplate rt
 	INNER JOIN Enterprise.Party P ON
 		P.PartyId = rt.PartyID
 	LEFT OUTER  JOIN Security.RoleTemplateProduct rtp ON rt.RoleTemplateId = rtp.RoleTemplateId
 	LEFT OUTER JOIN Security.RoleTemplateProductRoleMapping rprm ON rprm.RoleTemplateProductId = rtp.RoleTemplateProductId
 	LEFT OUTER JOIN Security.RoleTemplateAdditionalProductRoleMapping rtprm ON rtprm.RoleTemplateProductId = rtp.RoleTemplateProductId
-
+	LEFT OUTER JOIN RoleTemplateUser RTUM ON RTUM.RoleTemplateId = RT.RoleTemplateId
 WHERE rt.RoleTemplateId = @RoleTemplateId 
 	AND (P.RealPageId = @OrganizationRealPageId OR @OrganizationRealPageId IS NULL)
 	AND (P.PartyId = @PartyId OR @PartyId IS NULL)
 ORDER BY rtp.ProductId
+END
