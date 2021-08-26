826b271c380a130cb20586167381b68752401363
GABRIEL BALANOV <gabriel.balanov@RealPage.com>
2021-08-26 13:43:54 -0500

    Merged PR 100501: Created tables for ADgroups
    Created tables for ADgroups
    
    Related work items: #918428

---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/RoleRightsManagement/Tables/ADGroup.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [Security].[ADGroup](
+	[ADGroupId] [int] IDENTITY(1,1) NOT NULL,
+	[DisplayName] [nvarchar](255) NULL,
+	[ActiveDirectoryId] [uniqueidentifier] ROWGUIDCOL  NOT NULL,
+	[CreatedBy] [nvarchar](25) NOT NULL,
+	[CreatedDate] [datetime] NOT NULL
+ CONSTRAINT [PK_ADGroup] PRIMARY KEY CLUSTERED 
+(
+	[ADGroupId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [Security].[ADGroup] ADD  CONSTRAINT [DF_ADGroup_CreatedDate]  DEFAULT (getdate()) FOR [CreatedDate]
+GO
---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/RoleRightsManagement/Tables/ADGroupRight.sql
@@ -0,0 +1,29 @@
+﻿CREATE TABLE [Security].[ADGroupRight](
+	[ADGroupRightId] [int] IDENTITY(1,1) NOT NULL,
+	[ADGroupId] [int] NOT NULL,
+	[RightId] [int] NOT NULL,
+	[CreatedBy] [nvarchar](25) NOT NULL,
+	[CreatedDate] [datetime] NOT NULL
+ CONSTRAINT [PK_ADGroupRight] PRIMARY KEY CLUSTERED 
+(
+	[ADGroupRightId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [Security].[ADGroupRight] ADD  CONSTRAINT [DF_ADGroupRight_CreatedDate]  DEFAULT (getdate()) FOR [CreatedDate]
+GO
+
+ALTER TABLE [Security].[ADGroupRight]  WITH CHECK ADD  CONSTRAINT [FK_ADGroupRight_ADGroup] FOREIGN KEY([ADGroupId])
+REFERENCES [Security].[ADGroup] ([ADGroupId])
+GO
+
+ALTER TABLE [Security].[ADGroupRight] CHECK CONSTRAINT [FK_ADGroupRight_ADGroup]
+GO
+
+ALTER TABLE [Security].[ADGroupRight] WITH CHECK ADD  CONSTRAINT [FK_ADGroupRight_Right] FOREIGN KEY([RightId])
+REFERENCES [Security].[Right] ([RightId])
+GO
+
+ALTER TABLE [Security].[ADGroupRight] CHECK CONSTRAINT [FK_ADGroupRight_Right]
+GO
