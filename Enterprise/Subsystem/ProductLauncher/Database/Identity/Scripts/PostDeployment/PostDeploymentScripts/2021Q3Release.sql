c3e41706523854066c2232105a11f3faa8f14dd8
KalyanaChakravarthy Bingi <kalyanachakravarthy.bingi@RealPage.com>
2021-08-31 01:46:36 -0500

    Merged PR 101057: 107196: renaming the all properties toggle for onsite
    107196: renaming the all properties toggle for onsite
    
    Related work items: #107196

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/2021Q3Release.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/2021Q3Release.sql
@@ -2291,3 +2291,12 @@ BEGIN
 END
 
 GO
+ 
+Declare @ControlId INT 
+SELECT @ControlId = ControlId FROM UserManagement.Control WHERE UIId='On-SiteProductAccessAssignnewpropertiesautomaticallyPropertiesSwitchUIId' AND DisplayName = 'Assign new properties automatically'
+IF (@ControlId IS NOT NULL)
+BEGIN
+UPDATE UserManagement.Control SET DisplayName = 'Assign current and new properties automatically' Where ControlId = @ControlId
+END
+ 
+GO
