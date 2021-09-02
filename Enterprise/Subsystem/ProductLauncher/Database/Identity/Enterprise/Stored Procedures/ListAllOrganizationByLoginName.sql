5780976adfe11af694e7b09e46a303a58b6e6b04
Vivek Vishal <vivek.vishal@RealPage.com>
2021-09-02 04:28:38 -0500

    Merged PR 101587: cherry-pick! Bug 920489: Internal: Primary Company Regular user is getting deactivated simultaneously with their Secondary company External User that has a User expires date set
    cherry-pick! Bug 920489: Internal: Primary Company Regular user is getting deactivated simultaneously with their Secondary company External User that has a User expires date set
    
    Related work items: #920489

---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Enterprise/Stored Procedures/ListAllOrganizationByLoginName.sql
@@ -0,0 +1,31 @@
+﻿CREATE PROCEDURE [Enterprise].[ListAllOrganizationByLoginName](
+	@LoginName varchar(255)
+)
+AS
+BEGIN
+	DECLARE @NOW  DATETIME = GETUTCDATE();
+
+	SELECT
+		ep.PartyId AS 'OrganizationPartyId',
+		ep.RealPageId AS 'OrganizationRealPageId',
+		ert.PartyRoleTypeId,
+		ert.Name,
+		ULP.PrimaryOrganization,
+		edim.MasterId AS 'BooksMasterId',
+		Edim.CompanyMasterId AS 'BooksCustomerMasterId',
+		P.PersonaId,  
+		O.[Name] OrganizationName  
+	FROM		
+		Enterprise.Party ep
+		INNER JOIN Ident.UserLoginPersona ULP ON ULP.OrganizationPartyId = ep.PartyId
+		INNER JOIN Ident.UserLogin iul ON (ULP.UserLoginId = iul.UserId)
+		INNER JOIN Person.Persona P ON P.UserLoginPersonaId = ULP.UserLoginPersonaId  
+		INNER JOIN Enterprise.Organization O ON O.PartyId = ULP.OrganizationPartyId 
+		INNER JOIN Enterprise.PartyRelationship epr ON (iul.PersonPartyId = epr.PartyIdFrom) and ULP.OrganizationPartyId = epr.PartyIdTo
+		INNER JOIN Enterprise.RoleType ert ON (epr.RoleTypeIdFrom = ert.PartyRoleTypeId)
+		INNER JOIN Enterprise.RoleType eprt ON (ert.ParentPartyRoleTypeId = eprt.PartyRoleTypeId)
+		LEFT OUTER JOIN Enterprise.VW_DataImportMapping edim ON (ep.PartyId = edim.PartyId)
+	WHERE	
+		iul.LoginName = @LoginName
+		
+END;
