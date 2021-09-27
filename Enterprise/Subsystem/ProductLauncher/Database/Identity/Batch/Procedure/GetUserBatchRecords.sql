9ad6cf8d3b251e0314af645a375700087556b930
Jhuman Chhetri <jhuman.chhetri@RealPage.com>
2021-09-27 14:12:12 -0500

    Merged PR 105809: 961042
    961042
    
    Related work items: #961042

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Batch/Procedure/GetUserBatchRecords.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Batch/Procedure/GetUserBatchRecords.sql
@@ -10,5 +10,6 @@ Begin
  join Batch.BatchProcessorGroup bg on bg.BatchProcessorGroupId = bp.BatchProcessorGroupId                   
  where bp.BatchProcessorGroupId = @batchProcessorGroupId    
  and SubjectUserPersonaId = @subjectUserPersonId        
- and EditorUserPersonaId = @editorUserPersonId        
+ and EditorUserPersonaId = @editorUserPersonId
+ and bp.ProductId <> 42 -- ignoring 42, salesforce, since its a dummy product
 End 
