1b33e5027e1d061a5b982ba416ba093e59c6b363
Srinivas Dyasani <srinivasreddy.dyasani@realpage.com>
2021-09-28 13:39:41 +0530

    Changes done for activity log api

--- Enterprise/Foundation/Activity/Service/Reader/Controllers/ActivityController.cs
+++ Enterprise/Foundation/Activity/Service/Reader/Controllers/ActivityController.cs
@@ -300,13 +300,12 @@ namespace RP.Enterprise.Foundation.Activity.Service.Logging.Reader.Controllers
         [SwaggerResponse(HttpStatusCode.OK, Description = "List activity by criteria to send data related to Pagination", Type = typeof(ListResponse<ActivityDetailMessage>))]
         [Route("api/v1/listactivitylog")]
         [HttpPost]
-        public HttpResponseMessage ListActivityLogDetails(ActivityLogFilterCriteria filterCriteria)
+        public HttpResponseMessage ListActivityLogDetails(ActivityLogFilterCriteria filterCriteria,bool isArchived = false)
         {
             var result = new ListResponse<ActivityDetailMessage>();
             try
             {
                 Log.Information($"Getting Activity Log Detail");
-                var isArchived = Request.GetQueryNameValuePairs().Any(x=>x.Key == "isArchived" && x.Value == "true");
                 if (filterCriteria == null)
                 {
                     result.IsError = true;
