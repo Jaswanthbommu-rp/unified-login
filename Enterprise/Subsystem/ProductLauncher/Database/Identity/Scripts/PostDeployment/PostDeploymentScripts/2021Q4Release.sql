1218a51c7864b9c591ee071a8467da24bf46d058
Siva Savaram <siva.savaram@realpage.com>
2021-09-30 10:14:05 -0500

    Merged PR 106554: Adding new Right for Community Rewards
    Adding new Right for Community Rewards
    
    Related work items: #769255

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/2021Q4Release.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/2021Q4Release.sql
@@ -2494,3 +2494,27 @@ begin
 end
 GO
 --END : script for userstory #944865
+
+
+
+
+Declare @RightId bigint,@RoleID bigint,@UserId bigint;
+
+SELECT	@UserId = UserId
+	FROM	Ident.UserLogin
+	WHERE	LoginName LIKE 'realpagead@%'
+
+If Not Exists (Select Top 1 1 from Security.[Right] where RightName = 'ManageCommunityRewardsProductAccess')
+Begin
+insert into Security.[Right] values ('ManageCommunityRewardsProductAccess', 'Manage Community Rewards Product Access','Manage Community Rewards Product Access',13,9,3,77,6357,GETDATE())
+END
+
+Select @RightId = RightId from Security.[Right] where RightName = 'ManageCommunityRewardsProductAccess';
+Select @RoleID = RoleID from Security.Role where RoleName = 'User Administrator';
+
+If Not Exists (Select Top 1 1 from Security.RoleRight where RoleId =@RoleID and RightID = @RightId)
+Begin
+ Insert into Security.RoleRight values (@RoleID,@RightId,@UserId,GETDATE())
+End
+
+GO
