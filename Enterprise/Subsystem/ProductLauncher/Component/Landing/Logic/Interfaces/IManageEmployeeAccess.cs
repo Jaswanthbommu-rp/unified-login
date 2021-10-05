c7985c1f2af83dbd09d0df7db99b428149896dc6
Jhuman Chhetri <jhuman.chhetri@RealPage.com>
2021-10-05 09:55:48 -0500

    Merged PR 107198: Ability to create RP Employees as external users in PMC's with new link in from Support tool
    Related work items: #946929

--- Enterprise/Subsystem/ProductLauncher/Component/Landing/Logic/Interfaces/IManageEmployeeAccess.cs
+++ Enterprise/Subsystem/ProductLauncher/Component/Landing/Logic/Interfaces/IManageEmployeeAccess.cs
@@ -1,4 +1,6 @@
 ﻿using RP.Enterprise.Subsystem.ProductLauncher.Component.SharedObjects.Landing;
+using RP.Enterprise.Subsystem.ProductLauncher.Component.SharedObjects.Product.EmployeeAccess;
+using System;
 
 namespace RP.Enterprise.Subsystem.ProductLauncher.Component.Landing.Logic.Interfaces
 {
@@ -13,6 +15,8 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Component.Landing.Logic.Interf
         /// Returns Users in Unified Login
         /// </summary>
         ListResponse GetUsers(long editorPersonaId, string filter);
+
+        EmployeePersona GetOrCreateEmployeePersonaId(Guid companyRealPageId, string loginName);
 
     }
 }
