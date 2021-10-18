fa3dd7e54a04e59d6949f587c41d5bdad73f1422
GABRIEL BALANOV <gabriel.balanov@RealPage.com>
2021-10-18 15:24:24 -0500

    Merged PR 109659: Add/Update Roles
    Add/Update Roles
    
    Related work items: #966754

---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/RoleRightsManagement/Stored Procedures/AddRoleDetailsInternal.sql
@@ -0,0 +1,46 @@
+﻿CREATE PROCEDURE [Security].[AddRoleDetailsInternal]
+	@RoleName nvarchar(255),
+	@Description nvarchar(255),
+	@ProductId int,
+	@RoleTypeId int,
+	@OrgPartyId  bigint NULL,
+	@ShortName nvarchar(255),
+	@CreatedBy nvarchar(255)
+AS
+    BEGIN
+		BEGIN TRY
+		BEGIN TRANSACTION;
+			INSERT INTO [Security].[Role]
+					(RoleName,
+					 [Description],
+					 ProductId,
+					 RoleTypeID,
+					 OrgPartyID,
+					 ShortName,
+					 CreatedBy,
+					 CreatedDate
+					)
+			     VALUES
+					(@RoleName,
+					 @Description,
+					 @ProductId,
+					 @RoleTypeId,
+					 @OrgPartyId,
+					 @ShortName,
+					 @CreatedBy,
+					 GETUTCDATE()
+					);
+		COMMIT;
+		END TRY
+		BEGIN CATCH
+			ROLLBACK;
+
+			DECLARE @ErrorLogID INT;
+			EXEC dbo.LogError @ErrorLogID = @ErrorLogID OUTPUT;
+
+			SELECT  0 AS Id,
+					ErrorMessage
+			FROM    [dbo].ErrorLog
+			WHERE   ErrorLogID = @ErrorLogID;
+		END CATCH
+    END;
---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/RoleRightsManagement/Stored Procedures/UpdateRoleDetailsInternal.sql
@@ -0,0 +1,40 @@
+﻿CREATE PROCEDURE [Security].[UpdateRoleDetailsInternal]
+    @RoleId int,
+	@RoleName nvarchar(255),
+	@Description nvarchar(255),
+	@ProductId int NULL,
+	@RoleTypeId int NULL,
+	@OrgPartyId  bigint NULL,
+	@ShortName nvarchar(255) 
+AS
+    BEGIN
+		BEGIN TRY
+			SET NOCOUNT ON;
+			BEGIN TRANSACTION;
+
+			UPDATE R
+			SET [RoleName] = CASE WHEN @RoleName IS NULL THEN R.RoleName ELSE LTRIM(RTRIM(@RoleName)) END,
+				[Description] = CASE WHEN @Description IS NULL THEN R.[Description] ELSE LTRIM(RTRIM(@Description)) END,
+				ProductId = CASE WHEN @ProductId IS NULL THEN R.[ProductId] ELSE @ProductId END,
+				RoleTypeID = CASE WHEN @RoleTypeId IS NULL THEN R.RoleTypeID ELSE @RoleTypeId END,
+				ShortName = CASE WHEN @ShortName IS NULL THEN R.ShortName ELSE @ShortName END,
+				OrgPartyID =  @OrgPartyId
+			FROM [Security].[Role] R
+			WHERE R.RoleId = @RoleId
+
+			SET NOCOUNT OFF;
+			COMMIT;
+			SELECT @RoleId AS RoleId
+		END TRY
+		BEGIN CATCH
+			ROLLBACK;
+
+			DECLARE @ErrorLogID INT;
+			EXEC dbo.LogError @ErrorLogID = @ErrorLogID OUTPUT;
+
+			SELECT  0 AS Id,
+					ErrorMessage
+			FROM    [dbo].ErrorLog
+			WHERE   ErrorLogID = @ErrorLogID;
+		END CATCH
+    END;
