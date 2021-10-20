f6a38b4b4dec309720220e61d8503113057e1939
Jhuman Chhetri <jhuman.chhetri@RealPage.com>
2021-10-20 08:30:07 -0500

    Merged PR 109591: sprocs for AdGroups
    sprocs for AdGroups
    
    Related work items: #968858

---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Security/GetADGroupsProductRightByPersonaId.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [Security].[GetADGroupsProductRightByPersonaId] (@personaId bigint)
+AS
+BEGIN
+	SELECT g.ADGroupId, g.DisplayName as ADGroupName, pd.ProductsCount, rt.RightsCount
+	FROM SECURITY.ADGroupUser u
+	JOIN SECURITY.ADGroup g ON g.ADGroupId = u.ADGroupId
+	CROSS APPLY (
+		SELECT COUNT(art.ADGroupId) AS RightsCount
+		FROM SECURITY.ADGroupRight art
+	
+		WHERE art.ADGroupId = g.ADGroupId) AS rt
+	CROSS APPLY(
+		SELECT COUNT(apd.ADGroupId) AS ProductsCount
+		FROM SECURITY.[ADGroupProduct] apd
+		WHERE apd.ADGroupId = g.ADGroupId
+		) AS pd
+	WHERE u.PersonaId = @personaId
+	ORDER BY g.DisplayName
+END
---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Security/GetProductsForADGroup.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [Security].[GetProductsForADGroup] @adGroupId int  
+AS  
+BEGIN  
+SELECT p.ProductId as Id, p.Name  
+FROM Security.[ADGroupProduct] ap  
+JOIN Enterprise.[Product] p on p.ProductId = ap.ProductId  
+WHERE AP.ADGroupId = @adGroupId  
+ORDER BY p.Name  
+END
