f4cf4e3087bf4df4896074fc804be8f7058fbc6f
Jhuman Chhetri <jhuman.chhetri@realpage.com>
2021-10-25 16:33:53 -0500

    Sproc for Get Rights DETAILS

---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Security/GetRightsByRoleId.sql
@@ -0,0 +1,29 @@
+﻿Create Procedure [Security].[GetRightsByRoleId] (@roleId int)
+AS
+Begin
+	Select r.RightId, RightName, r.Description, p.ProductId, p.Name as ProductName, 
+	r.TargetProductId, p1.Name as TargetProductName, 
+	st.StatusTypeId, st.Name as StatusType, 
+	r.VisibilityStatusId, st1.Name as Visibility
+	from Security.[Right] r
+		join Security.[RoleRight] rr on rr.RightId = r.RightId
+		join Enterprise.Product p on p.ProductId = r.ProductId 
+		join Enterprise.Product p1 on p1.ProductId = r.TargetProductId
+		join Enterprise.StatusType st on st.StatusTypeId = r.StatusTypeId
+		join Enterprise.StatusType st1 on st1.StatusTypeId = r.VisibilityStatusId
+	where RoleId = @roleId
+End
+
+
+
+
+
+
+
+
+
+
+
+
+
+
