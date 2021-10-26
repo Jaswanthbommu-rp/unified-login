69c8dacb456a9e9ed39f07a6038ea24dcd3e0e29
Jhuman Chhetri <jhuman.chhetri@RealPage.com>
2021-10-26 17:20:32 -0500

    Merged PR 111166: proc
    proc
    
    Related work items: #983601, #983608, #983614, #983615

---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Security/GetRightDetailsByRightId.sql
@@ -0,0 +1,39 @@
+﻿CREATE PROCEDURE [Security].[GetRightDetailsByRightId] (@rightId int)
+AS
+BEGIN
+	Select r.RightId, 
+		r.RightName, 
+		r.Description,
+		p.ProductId,
+		p.Name as ProductName,
+		t.ProductId as TargetProductId,
+		t.Name as TargetProductName,
+		s.StatusTypeId,
+		s.Name as StatusTypeName,
+		v.StatusTypeId as VisibilityStatusId,
+		v.Name as VisibilityStatusName
+	FROM Security.[Right] r
+	JOIN Enterprise.Product p on p.ProductId = r.ProductId
+	JOIN Enterprise.Product t on t.ProductId = r.TargetProductId
+	JOIN Enterprise.StatusType s on s.StatusTypeId = r.StatusTypeId
+	JOIN Enterprise.StatusType v on v.StatusTypeId = r.VisibilityStatusId
+	WHERE r.RightId = @rightId
+
+	SELECT rl.RoleId, rl.RoleName, p.ProductId, p.Name as ProductName
+	FROM SECURITY.[RoleRight] rr
+		JOIN SECURITY.[Role] rl on rl.RoleId = rr.RoleRightId
+		JOIN Enterprise.Product p on p.ProductId = rl.ProductId
+	WHERE rr.RightId = @rightId
+	
+	
+	SELECT r.RouteId, RouteValue as RouteName
+	FROM SECURITY.[RightRoute] rr
+		JOIN SECURITY.[Route] r on r.RouteId = rr.RouteId
+	WHERE rr.RightId = @rightId
+	
+	
+	SELECT org.PartyId as OrganizationId, org.Name as CompanyName
+	FROM Security.OrganizationOverRideRight r
+	JOIN Enterprise.Organization org on org.PartyId = r.OrgPartyId
+	WHERE r.RightId = @rightId
+END
