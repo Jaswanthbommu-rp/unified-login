3273b8bbf5a00c3f05bec35fe85dc2ffdcb9f211
Jhuman Chhetri <jhuman.chhetri@RealPage.com>
2021-11-03 08:47:24 -0500

    Merged PR 112456: Proc for Route with right count
    Proc for Route with right count
    
    Related work items: #988245

---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Security/GetRoutesWithRightCount.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [Security].[GetRoutesWithRightCount]
+AS
+BEGIN
+SELECT ro.RouteId, ro.RouteValue as RouteName, ro.Description as RouteDescription, count(r.RightId) as RightCount
+FROM SECURITY.[Right] r
+JOIN SECURITY.[RightRoute] rr on rr.RightId = r.RightId
+JOIN SECURITY.[Route] ro on ro.RouteId = rr.RouteId
+GROUP BY ro.RouteId, ro.RouteValue, ro.Description
+End
