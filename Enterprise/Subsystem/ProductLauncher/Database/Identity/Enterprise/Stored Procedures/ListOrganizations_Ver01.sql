c6682e8e5904716875da9f4c4993fb725c3dce54
Markandeyulu Kativarapu <markandeyulu.kativarapu@RealPage.com>
2021-11-03 09:25:40 -0500

    Merged PR 112584: Bug 994946: Companies list is not loading in support tool
    Bug 994946: Companies list is not loading in support tool
    
    Related work items: #994946

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Enterprise/Stored Procedures/ListOrganizations_Ver01.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Enterprise/Stored Procedures/ListOrganizations_Ver01.sql
@@ -1,30 +1,89 @@
-﻿CREATE PROCEDURE [Enterprise].[ListOrganizations_Ver01] 
-    @RealPageId UNIQUEIDENTIFIER = NULL,
-    @Filter VARCHAR(100) = NULL
-AS
-BEGIN
+﻿
+CREATE PROCEDURE [Enterprise].[ListOrganizations_Ver01] 
+(
+    @RealPageId UNIQUEIDENTIFIER = NULL,  
+    @Filter VARCHAR(100) = NULL 
+)
+AS  
+BEGIN 
 
-         SELECT O.PartyId,
-                O.[Name],
-                O.IsActive,
-                P.RealPageId 'OrganizationRealPageId',
-                COALESCE(ISNULL(D.MasterId, 0),0)  AS 'BooksMasterId',
-				COALESCE(ISNULL(D.CompanyMasterId, 0), 0)  AS 'BooksCustomerMasterId',
-                'RealPageEmployeeAccessID' AS 'SettingName',
-                P2.RealPageId AS 'PersonRealPageId',
-                UL.LoginName,
-				OD.[Name] as Domain
-         FROM Enterprise.Organization O
-              INNER JOIN Enterprise.Party P ON O.PartyId = P.PartyId
-              INNER JOIN Enterprise.VW_DataImportMapping D ON O.PartyId = D.PartyId
-			  INNER JOIN Enterprise.OrganizationDomain OD on O.OrganizationDomainId = OD.OrganizationDomainId
-              INNER JOIN Enterprise.OrganizationAdminUser OAU ON OAU.OrganizationPartyId = O.PartyId
-			  INNER JOIN Ident.UserLoginPersona ULP ON oau.UserLoginPersonaId = ulp.UserLoginPersonaId
-			  INNER JOIN Ident.UserLogin UL ON UL.UserId = ulp.UserLoginId
-			  INNER JOIN Enterprise.Party P2 ON P2.PartyId = UL.PersonPartyId
-               AND (@Filter IS NULL OR o.Name like '%'+@Filter+'%')
-			   AND (@RealPageID IS NULL OR P.RealPageId = @RealPageId)
-		ORDER BY O.[Name]
-        OPTION (RECOMPILE)
+	IF (@RealPageId IS NOT NULL)
+	BEGIN
 
-END; 
+		SELECT 
+			O.PartyId,  
+			O.[Name],  
+			O.IsActive,  
+			P.RealPageId 'OrganizationRealPageId',  
+			ISNULL(D.MasterId, 0)  AS 'BooksMasterId',  
+			ISNULL(D.CompanyMasterId, 0)  AS 'BooksCustomerMasterId',  
+			'RealPageEmployeeAccessID' AS 'SettingName',  
+			P2.RealPageId AS 'PersonRealPageId'
+		FROM 
+			Enterprise.Organization O WITH (NOLOCK) 
+		INNER JOIN Enterprise.Party P WITH (NOLOCK) ON O.PartyId = P.PartyId  
+		INNER JOIN Enterprise.VW_DataImportMapping D WITH (NOLOCK) ON O.PartyId = D.PartyId  
+		INNER JOIN Enterprise.OrganizationAdminUser OAU WITH (NOLOCK) ON OAU.OrganizationPartyId = O.PartyId  
+		INNER JOIN Ident.UserLoginPersona ULP WITH (NOLOCK) ON oau.UserLoginPersonaId = ulp.UserLoginPersonaId  
+		INNER JOIN Ident.UserLogin UL WITH (NOLOCK) ON UL.UserId = ulp.UserLoginId  
+		INNER JOIN Enterprise.Party P2 WITH (NOLOCK) ON P2.PartyId = UL.PersonPartyId  
+		WHERE 
+			P.RealPageId = @RealPageId
+		ORDER BY 
+			O.[Name]  
+
+	END
+
+	ELSE IF (@Filter IS NOT NULL)
+	BEGIN
+		
+		SELECT 
+			O.PartyId,  
+			O.[Name],  
+			O.IsActive,  
+			P.RealPageId 'OrganizationRealPageId',  
+			ISNULL(D.MasterId, 0)  AS 'BooksMasterId',  
+			ISNULL(D.CompanyMasterId, 0)  AS 'BooksCustomerMasterId',  
+			'RealPageEmployeeAccessID' AS 'SettingName',  
+			P2.RealPageId AS 'PersonRealPageId'
+		FROM 
+			Enterprise.Organization O WITH (NOLOCK) 
+		INNER JOIN Enterprise.Party P WITH (NOLOCK) ON O.PartyId = P.PartyId  
+		INNER JOIN Enterprise.VW_DataImportMapping D WITH (NOLOCK) ON O.PartyId = D.PartyId  
+		INNER JOIN Enterprise.OrganizationAdminUser OAU WITH (NOLOCK) ON OAU.OrganizationPartyId = O.PartyId  
+		INNER JOIN Ident.UserLoginPersona ULP WITH (NOLOCK) ON oau.UserLoginPersonaId = ulp.UserLoginPersonaId  
+		INNER JOIN Ident.UserLogin UL WITH (NOLOCK) ON UL.UserId = ulp.UserLoginId  
+		INNER JOIN Enterprise.Party P2 WITH (NOLOCK) ON P2.PartyId = UL.PersonPartyId  
+		WHERE 
+			O.[Name] LIKE '%'+@Filter+'%'
+		ORDER BY 
+			O.[Name] 
+
+	END
+
+	ELSE 
+	BEGIN
+
+		SELECT 
+			O.PartyId,  
+			O.[Name],  
+			O.IsActive,  
+			P.RealPageId 'OrganizationRealPageId',  
+			ISNULL(D.MasterId, 0)  AS 'BooksMasterId',  
+			ISNULL(D.CompanyMasterId, 0)  AS 'BooksCustomerMasterId',  
+			'RealPageEmployeeAccessID' AS 'SettingName',  
+			P2.RealPageId AS 'PersonRealPageId'
+		FROM 
+			Enterprise.Organization O WITH (NOLOCK) 
+		INNER JOIN Enterprise.Party P WITH (NOLOCK) ON O.PartyId = P.PartyId  
+		INNER JOIN Enterprise.VW_DataImportMapping D WITH (NOLOCK) ON O.PartyId = D.PartyId  
+		INNER JOIN Enterprise.OrganizationAdminUser OAU WITH (NOLOCK) ON OAU.OrganizationPartyId = O.PartyId  
+		INNER JOIN Ident.UserLoginPersona ULP WITH (NOLOCK) ON oau.UserLoginPersonaId = ulp.UserLoginPersonaId  
+		INNER JOIN Ident.UserLogin UL WITH (NOLOCK) ON UL.UserId = ulp.UserLoginId  
+		INNER JOIN Enterprise.Party P2 WITH (NOLOCK) ON P2.PartyId = UL.PersonPartyId  
+		ORDER BY 
+			O.[Name]  
+
+	END
+
+END;
