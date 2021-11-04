1a11e7bca5aa8a621b29cafa80e9d85942f9d2a3
GABRIEL BALANOV <gabriel.balanov@RealPage.com>
2021-11-04 10:52:25 -0500

    Merged PR 112799: API: Roles and Rights Setup - Ability to query Azure for AD group by ID
    API: Roles and Rights Setup - Ability to query Azure for AD group by ID
    
    Related work items: #999460

---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/RoleRightsManagement/Stored Procedures/GetAdGroupByActiveDirectoryId.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [Security].[GetAdGroupByActiveDirectoryId] (  
+ @ActiveDirectoryId uniqueidentifier
+) 
+AS
+BEGIN
+	SELECT  ADGroupId,
+			DisplayName,
+			ActiveDirectoryId,
+			CreatedBy,
+			CreatedDate
+	FROM [Security].[ADGroup]
+	WHERE ActiveDirectoryId = @ActiveDirectoryId
+END
