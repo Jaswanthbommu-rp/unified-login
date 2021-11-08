b8653915dd950f1e30d592b66c03ac0a58b1f78b
Jhuman Chhetri <jhuman.chhetri@RealPage.com>
2021-11-08 15:38:16 -0600

    Merged PR 113289: updated the categorytype name
    Related work items: #996361

--- Enterprise/Foundation/Activity/DatabaseV2/AuditArchiveDB/Scripts/PostDeployment/PostDeploymentScripts/AddNewLogType.sql
+++ Enterprise/Foundation/Activity/DatabaseV2/AuditArchiveDB/Scripts/PostDeployment/PostDeploymentScripts/AddNewLogType.sql
@@ -22,7 +22,7 @@ End
 
 GO
 
-IF NOT EXISTS (SELECT TOP 1 1 FROM Logging.LogCategoryType WHERE Name = 'Internal Product Settings')
+IF NOT EXISTS (SELECT TOP 1 1 FROM Logging.LogCategoryType WHERE Name = 'InternalProductSettings')
 BEGIN
 	Declare @catId int;
 
@@ -30,7 +30,7 @@ BEGIN
 	FROM Logging.LogCategoryType
 
 	Insert into Logging.LogCategoryType (LogCategoryTypeId, Name, Description)
-	values (@catId, 'Internal Product Settings','Internal Product Settings.')
+	values (@catId, 'InternalProductSettings','Internal Product Settings.')
 END
 
 GO
@@ -42,7 +42,7 @@ BEGIN
 	
 	SELECT @catId = LogCategoryTypeId
 	FROM logging.LogCategoryType 
-	WHERE NAME = 'Internal Product Settings'
+	WHERE NAME = 'InternalProductSettings'
 	
 	SELECT @logId = MAX(LogTypeId) + 1
 	FROM Logging.LogType
