8433edcd86f7fba2b784f400d39f78a22fe48804
Jhuman Chhetri <jhuman.chhetri@realpage.com>
2021-11-09 15:32:39 -0600

    updated proc

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Security/GetRightsAndProductsForADGroup.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Security/GetRightsAndProductsForADGroup.sql
@@ -1,12 +1,10 @@
 ﻿CREATE   PROCEDURE [Security].[GetRightsAndProductsForADGroup] @adGroupId int  
 AS  
 BEGIN  
- SELECT r.RightId, r.RightName, p.Name as ProductName  
- FROM Security.[ADGroupRight] ar  
- JOIN Security.[Right] r on r.RightId = ar.RightId  
- JOIN  Security.ADGroupProduct ap on ar.ADGroupId = ap.ADGroupId  
- JOIN Enterprise.Product p on p.ProductId = ap.ProductId
-  AND r.TargetProductId = p.ProductId
- WHERE ar.ADGroupId = @adGroupId  
- ORDER BY p.Name  
+ SELECT r.RightId, r.RightName, p.Name as ProductName      
+ FROM [Security].ADGroupRight ar
+ JOIN Security.[Right] r on r.RightId = ar.RightId
+ JOIN Enterprise.Product p on p.ProductId = r.TargetProductId
+ WHERE ar.ADGroupId = @adGroupId      
+ ORDER BY p.Name   
 END  
