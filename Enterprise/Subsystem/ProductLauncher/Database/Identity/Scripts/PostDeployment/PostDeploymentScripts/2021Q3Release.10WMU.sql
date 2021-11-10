583a5418dcf6456b293704a8b00c9c54812ae50d
KalyanaChakravarthy Bingi <kalyanachakravarthy.bingi@RealPage.com>
2021-11-10 03:32:43 -0600

    Merged PR 113644: added script for facilities product dependency
    added script for facilities product dependency
    
    Related work items: #696097

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/2021Q3Release.10WMU.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/2021Q3Release.10WMU.sql
@@ -1,4 +1,13 @@
 ﻿
+GO
+IF NOT EXISTS (SELECT TOP (1) 1 FROM Enterprise.ProductUserDependency WHERE ProductId = 75 and DependentProductId = 1)
+BEGIN
+INSERT INTO Enterprise.ProductUserDependency(ProductId,DependentProductId)
+VALUES(75,1)
+END
+GO
+
+
 Declare @RightId bigint,@RoleID bigint,@UserId bigint;
 SELECT	@UserId = UserId
 	FROM	Ident.UserLogin
