d35804acdfb07fd5db2dd3bc714035292ff24429
GABRIEL BALANOV <gabriel.balanov@RealPage.com>
2021-11-22 16:24:37 -0600

    Merged PR 115609: API: Roles and Rights Setup - Ability to delete a ROLE
    API: Roles and Rights Setup - Ability to delete a ROLE
    
    Related work items: #1009004

---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/RoleRightsManagement/Stored Procedures/GetPersonasAssignedToRole.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [Security].[GetPersonasAssignedToRole] (
+	@RoleId int)
+AS
+BEGIN
+  SELECT P.PersonaId, P.UserLoginPersonaId, P.PersonaTypeId, P.PersonaEnvironmentTypeId, P.FromDate, P.ThruDate, P.IsDefault
+  FROM [Security].[PersonaRole] PR
+  INNER JOIN [Person].[Persona] P ON P.PersonaId = PR.PersonaId
+  WHERE PR.RoleId = @RoleId
+END
