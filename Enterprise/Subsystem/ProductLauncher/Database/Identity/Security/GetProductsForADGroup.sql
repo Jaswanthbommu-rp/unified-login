d5c5bc7426eb23379bb4bab96be83ba9eabcdba6
GABRIEL BALANOV <gabriel.balanov@RealPage.com>
2021-11-23 16:08:33 -0600

    Merged PR 115855: Added AssignmentOrder
    Added AssignmentOrder
    
    Related work items: #1000225

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Security/GetProductsForADGroup.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Security/GetProductsForADGroup.sql
@@ -1,7 +1,7 @@
 ﻿CREATE PROCEDURE [Security].[GetProductsForADGroup] @adGroupId int  
 AS  
 BEGIN  
-SELECT p.ProductId as Id, p.Name  
+SELECT p.ProductId as Id, p.Name, ap.AssignmentOrder
 FROM Security.[ADGroupProduct] ap  
 JOIN Enterprise.[Product] p on p.ProductId = ap.ProductId  
 WHERE AP.ADGroupId = @adGroupId  
