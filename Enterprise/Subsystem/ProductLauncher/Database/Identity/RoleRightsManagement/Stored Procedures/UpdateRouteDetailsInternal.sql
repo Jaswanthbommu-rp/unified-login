183245dad14aadfdb9056e642005842462909564
GABRIEL BALANOV <gabriel.balanov@RealPage.com>
2021-11-23 10:32:15 -0600

    Merged PR 115803: Added RouteValue
    Added RouteValue
    
    Related work items: #1015083

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/RoleRightsManagement/Stored Procedures/UpdateRouteDetailsInternal.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/RoleRightsManagement/Stored Procedures/UpdateRouteDetailsInternal.sql
@@ -1,10 +1,11 @@
 ﻿CREATE PROCEDURE [Security].[UpdateRouteDetailsInternal]
     @RouteId int,
+	@RouteValue nvarchar(50),
 	@Description nvarchar(200)
 AS
 BEGIN
 	UPDATE [Security].[Route]
-	SET [Description] = @Description
+	SET [Description] = @Description, RouteValue = @RouteValue
 	WHERE RouteId = @RouteId
 	SELECT @RouteId AS RouteId
 END;
--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Security/GetRoutesWithRightCount.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Security/GetRoutesWithRightCount.sql
@@ -1,9 +1,9 @@
-﻿CREATE PROCEDURE [Security].[GetRoutesWithRightCount]
-AS
-BEGIN
-SELECT ro.RouteId, ro.RouteValue as RouteName, ro.Description as RouteDescription, count(r.RightId) as RightCount
-FROM SECURITY.[Right] r
-JOIN SECURITY.[RightRoute] rr on rr.RightId = r.RightId
-JOIN SECURITY.[Route] ro on ro.RouteId = rr.RouteId
-GROUP BY ro.RouteId, ro.RouteValue, ro.Description
+﻿CREATE   Procedure [Security].[GetRoutesWithRightCount]
+As
+Begin
+select ro.RouteId, ro.RouteValue as RouteName, ro.Description as RouteDescription, count(r.RightId) as RightCount
+from Security.[Right] r
+join Security.[RightRoute] rr on rr.RightId = r.RightId
+right join Security.[Route] ro on ro.RouteId = rr.RouteId
+group by ro.RouteId, ro.RouteValue, ro.Description
 End
