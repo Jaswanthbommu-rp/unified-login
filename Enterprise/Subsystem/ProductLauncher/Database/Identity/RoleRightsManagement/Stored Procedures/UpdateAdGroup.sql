ee9cb9b5ed4073b0e28f0245726baa36e21741f1
GABRIEL BALANOV <gabriel.balanov@RealPage.com>
2021-11-24 09:28:17 -0600

    Merged PR 116004: DB: Roles and Rights Setup - Ability to soft delete an AD Group
    DB: Roles and Rights Setup - Ability to soft delete an AD Group
    
    Related work items: #1011219

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/RoleRightsManagement/Stored Procedures/UpdateAdGroup.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/RoleRightsManagement/Stored Procedures/UpdateAdGroup.sql
@@ -1,10 +1,12 @@
 ﻿CREATE PROCEDURE [Security].[UpdateAdGroup]
 	@AdGroupId int,
-	@DisplayName nvarchar(255)
+	@DisplayName nvarchar(255) = NULL,
+	@IsActive bit = NULL
 AS
 BEGIN
 	UPDATE [Security].[ADGroup]
-	SET DisplayName = @DisplayName
+	SET DisplayName = ISNULL(@DisplayName, DisplayName),
+		IsActive = ISNULL(@IsActive, IsActive)
 	WHERE ADGroupId = @AdGroupId
 	SELECT @ADGroupId AS Id, '' AS ErrorMessage
 END;
