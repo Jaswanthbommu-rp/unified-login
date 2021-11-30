f7400b37e5879c365bf49872033b6e2cedd88df2
GABRIEL BALANOV <gabriel.balanov@RealPage.com>
2021-11-30 10:32:30 -0600

    Merged PR 116603: #1000092 DB
    #1000092 DB
    
    Related work items: #1000092

---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/RoleRightsManagement/Stored Procedures/GetRightsForRoute.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [Security].[GetRightsForRoute](
+    @RouteId int
+)
+AS
+    BEGIN
+		SELECT RT.RightId AS RightId,
+		RT.RightName AS RightName,
+		RT.[Value] AS [Value],
+		RT.[Description] AS [Description],
+		RT.ProductId AS ProductId,
+		P1.[Name] AS ProductName,
+		RT.TargetProductId AS TargetProductId,
+		P2.[Name] AS TargetProductName
+		FROM [Security].[RightRoute] RR
+		INNER JOIN [Security].[Right] RT ON RT.RightId = RR.RightId
+		INNER JOIN [Enterprise].[Product] P1 ON RT.ProductId = P1.ProductId
+		INNER JOIN [Enterprise].[Product] P2 ON RT.TargetProductId = P2.ProductId
+		WHERE RR.RouteId = @RouteId
+	END
