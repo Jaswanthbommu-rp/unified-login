f0b7585b4fe56d2d59bd4fcf52679f4539e228c1
Siva Savaram <siva.savaram@realpage.com>
2021-12-08 00:17:47 -0600

    Merged PR 117740: Allowing VendormarketPlace Resource tile for newly created Users with out right
    Allowing VendormarketPlace Resource tile for newly created Users with out right "Access to Vendor Marketplace".
    
    Cherry picked from !113158
    
    Related work items: #989388

--- Enterprise/Subsystem/ProductLauncher/Component/Landing/Repository/ProductRepository.cs
+++ Enterprise/Subsystem/ProductLauncher/Component/Landing/Repository/ProductRepository.cs
@@ -411,8 +411,8 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Component.Landing.Repository
                 }
 
                 //Remove Resource Vendor Marketplace if the user does not have the Ability to access Vendor Marketplace right
-                if (_userClaim.Rights.All(rght => !(rght.Equals("AccessVendorMarketplace", StringComparison.OrdinalIgnoreCase) ||
-                                                  rght.Equals("EmployeeAccessVendorMarketPlace", StringComparison.OrdinalIgnoreCase))))
+                if (_userClaim.Rights.All(rght => rght.Equals("EmployeeAccessVendorMarketPlace", StringComparison.OrdinalIgnoreCase)))
+                                                 
                 {
                     if (userProducts.Any(a => a.ProductId == (int)ProductEnum.VendorMarketplace))
                     {
