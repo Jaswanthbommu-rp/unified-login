9fe57277c8460375e36f2fc9da43fbb657eee500
Jaswanth Bommu <Jaswanth.Bommu@RealPage.com>
2022-02-09 10:18:12 -0600

    Merged PR 126425: Bug 1071829: Performance improvement for [Person].[ListUsersWithCompanyId] an...
    Bug 1071829: Performance improvement for [Person].[ListUsersWithCompanyId] and [Enterprise].[GetUserSyncJobTask] Procs
    
    Cherry picked from !126343
    
    Related work items: #1071829

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Enterprise/Stored Procedures/GetUserSyncJobTask.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Enterprise/Stored Procedures/GetUserSyncJobTask.sql
@@ -1,26 +1,45 @@
 ﻿CREATE PROCEDURE [Enterprise].[GetUserSyncJobTask]
-(@UserSyncJobTaskId	BIGINT)
+(@UserSyncJobTaskId BIGINT)
 AS
 BEGIN
-SET NOCOUNT ON;
-	DECLARE @NOW DATETIME= GETUTCDATE();
-	SELECT 
-		DISTINCT TOP (1)
-		sj.UserSyncJobId,
-		sua.PersonaId, 
-		sua.Value AS LoginName, 
-		p.ProductId,
-		ISNULL(p.UDMSourceCode, p.BooksProductCode) AS Source
-		FROM	
-			Ident.SamlUserAttribute sua
-			INNER JOIN Ident.SamlAttribute sa ON sua.SamlAttributeId = sa.SamlAttributeId
-			INNER JOIN Enterprise.UserSyncJob sj ON sj.PersonaId = sua.PersonaId
-			INNER JOIN Enterprise.UserSyncJobTask sjt ON sjt.UserSyncJobId = sj.UserSyncJobId
-			INNER JOIN Enterprise.Product p ON p.ProductId = sua.ProductId
-		WHERE 
-			sa.Name = 'UserId' AND 
-			((@NOW BETWEEN sua.FromDate AND sua.ThruDate) OR (@NOW >= sua.FromDate AND sua.ThruDate IS NULL)) AND
-			sjt.UserSyncJobTaskId = @UserSyncJobTaskId AND
-			sjt.Source = ISNULL(p.UDMSourceCode, p.BooksProductCode)
+    SET NOCOUNT ON;
+    DECLARE @NOW DATETIME = GETUTCDATE();
+    SELECT DISTINCT TOP (1)
+           sjt.UserSyncJobId,
+           x.PersonaId,
+           x.LoginName,
+           x.ProductId,
+           sjt.[Source]
+    FROM Enterprise.UserSyncJobTask sjt
+        CROSS APPLY
+    (
+        SELECT TOP 1000000000000
+               sua.PersonaId,
+               sua.Value AS LoginName,
+               p.ProductId,
+               ISNULL(p.UDMSourceCode, p.BooksProductCode) AS Source
+        FROM Enterprise.UserSyncJob sj
+            INNER JOIN Ident.SamlUserAttribute sua
+                ON sj.PersonaId = sua.PersonaId
+            INNER JOIN Enterprise.Product p
+                ON p.ProductId = sua.ProductId
+            INNER JOIN Ident.SamlAttribute sa
+                ON sua.SamlAttributeId = sa.SamlAttributeId
+        WHERE sjt.UserSyncJobId = sj.UserSyncJobId
+              AND sa.Name = 'UserId'
+              AND
+              (
+                  (@NOW
+              BETWEEN sua.FromDate AND sua.ThruDate
+                  )
+                  OR
+                  (
+                      @NOW >= sua.FromDate
+                      AND sua.ThruDate IS NULL
+                  )
+              )
+              AND sjt.Source = ISNULL(p.UDMSourceCode, p.BooksProductCode)
+    ) AS x
+    WHERE UserSyncJobTaskId = @UserSyncJobTaskId;
 END;
 GO
