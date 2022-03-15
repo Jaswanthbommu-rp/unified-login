b66cc01c557486a73001cc1c0d26e7a0edd2c9fc
Jaswanth Bommu <Jaswanth.Bommu@RealPage.com>
2022-03-15 14:19:12 -0500

    Merged PR 131570: User Story 979524: Unified Platform & Migration Tool: Upgrade apps using Support for .NET Framework 4.5.2, 4.6, and 4.6.1
    User Story 979524: Unified Platform & Migration Tool: Upgrade apps using Support for .NET Framework 4.5.2, 4.6, and 4.6.1
    
    Related work items: #979524

--- Enterprise/Subsystem/ProductLauncher/Web/IdentityServer/IdentityHelper/JsonConverters/ClaimsPrincipalLite.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RP.Enterprise.Subsystem.ProductLauncher.Web.IdentityHelper.JsonConverters
-{
-    public class ClaimsPrincipalLite
-    {
-        public string AuthenticationType { get; set; }
-        public ClaimLite[] Claims { get; set; }
-    }
-}
--- Enterprise/Subsystem/ProductLauncher/Web/IdentityServer/IdentityHelper/Model/Consent.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RP.Enterprise.Subsystem.ProductLauncher.Service.IdentityHelper.Model
-{
-    public class Consent
-    {
-        public string SubjectCode { get; set; }
-        public string ClientCode { get; set; }
-        public string Scopes { get; set; }
-    }
-}
