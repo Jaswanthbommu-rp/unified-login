3cf6c4e751b7e854c47a2ee66247455aad35e776
Jaswanth Bommu <Jaswanth.Bommu@RealPage.com>
2022-03-28 08:57:04 -0500

    Merged PR 133370: User Story 1052090: DB/API: Enhancement to persist rights based on the users role in employee company and AD group(s)
    User Story 1052090: DB/API: Enhancement to persist rights based on the users role in employee company and AD group(s)
    
    Related work items: #1052090

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/RoleRightsManagement/Tables/Right.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/RoleRightsManagement/Tables/Right.sql
@@ -9,7 +9,8 @@
 	[TargetProductId] [int] NULL,	
 	[CreatedBy] [nvarchar](255) NOT NULL,
 	[CreatedDate] [datetime] NOT NULL,
- CONSTRAINT [PK_SecurityRight] PRIMARY KEY CLUSTERED 
+ [PersistRight] BIT NOT NULL DEFAULT 0, 
+    CONSTRAINT [PK_SecurityRight] PRIMARY KEY CLUSTERED 
 (
 	[RightId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
