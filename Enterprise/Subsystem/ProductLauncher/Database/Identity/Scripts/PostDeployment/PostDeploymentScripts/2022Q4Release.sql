98a4de0ad6d05122d48dbfdd2d2da20e38d07268
Siva Savaram <siva.savaram@realpage.com>
2022-04-04 04:45:05 -0500

    Merged PR 134799: UL: Configure rights for Manage Templates link
    UL: Configure rights for Manage Templates link
    
    Related work items: #1019723

---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/2022Q4Release.sql
@@ -0,0 +1,11 @@
+﻿
+--1019723
+
+Declare @NavigationMenuID BIGINT, @RightID BIGINT;
+Select @RightID = RightId from Security.[Right] where value = 'Employee Access to Manage Settings Templates';
+Select @NavigationMenuID = Id  from Enterprise.NavigationMenu where Title = 'Manage Templates' and PageId ='manage-templates' and Origin ='unified-settings';
+IF NOT EXISTS (Select Top 1 1 from ENterprise.NavigationMenuRights where NavigationMenuId =@NavigationMenuID and RightId = @RightID)
+BEGIN
+INSERT INTO ENterprise.NavigationMenuRights (NavigationMenuId,RightId)
+                                 Values (@NavigationMenuID,@RightID);
+END
