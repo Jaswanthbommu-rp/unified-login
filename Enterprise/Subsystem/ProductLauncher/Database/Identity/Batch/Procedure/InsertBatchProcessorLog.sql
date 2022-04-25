2835bde1820ee5ef8598aa36737c9be4b6279046
Markandeyulu Kativarapu <markandeyulu.kativarapu@RealPage.com>
2022-04-25 08:50:11 -0500

    Merged PR 138053: User Story 1088657: Add a completed date time and Start datetime in DB - Batc...
    User Story 1088657: Add a completed date time and Start datetime in DB - Batch Process Log new table
    
    Related work items: #1088657

---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Batch/Procedure/InsertBatchProcessorLog.sql
@@ -0,0 +1,52 @@
+﻿
+CREATE PROCEDURE [Batch].[InsertBatchProcessorLog]
+(    
+	@BatchProcessorId BIGINT,
+	@StartDatetime DATETIME2,
+	@EndDatetime DATETIME2 
+)     
+AS    
+BEGIN   
+
+	SET NOCOUNT ON    
+      
+	BEGIN TRY  
+
+		IF @BatchProcessorId IS NULL 
+		RAISERROR('NULL Value is passed for BatchProcessorId',15,1)
+
+		IF @StartDatetime IS NULL 
+		RAISERROR('NULL Value is passed for StartDatetime',15,1)
+		
+		IF @EndDatetime IS NULL 
+		RAISERROR('NULL Value is passed for EndDatetime',15,1)
+
+		INSERT INTO [Batch].[BatchProcessorLog]    
+		(  
+			[BatchProcessorId],
+			StartDatetime,
+			EndDatetime
+		)  
+		VALUES  
+		(  
+			@BatchProcessorId,
+			@StartDatetime,
+			@EndDatetime			    
+		)   
+  
+	END TRY   
+ 
+	BEGIN CATCH          
+  
+		DECLARE @ErrorLogID INT;  
+		EXEC dbo.LogError @ErrorLogID = @ErrorLogID OUTPUT;  
+  
+		SELECT 0 AS Id,  
+			ErrorMessage  
+		FROM dbo.ErrorLog  
+		WHERE ErrorLogID = @ErrorLogID;  
+
+	END CATCH 
+
+END;
+
---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Batch/Table/BatchProcessorLog.sql
@@ -0,0 +1,25 @@
+﻿
+CREATE TABLE [Batch].[BatchProcessorLog]
+(
+	BatchProcessorLogId [bigint] IDENTITY(1,1) NOT NULL,
+	[BatchProcessorId] [bigint] NOT NULL,
+	StartDatetime DATETIME2 NOT NULL,
+	EndDatetime DATETIME2 NOT NULL,
+	[CreatedDateTime] [datetime] NOT NULL,
+ CONSTRAINT [PK_BatchProcessorLogId] PRIMARY KEY CLUSTERED 
+(
+	BatchProcessorLogId ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) 
+GO
+
+ALTER TABLE [Batch].[BatchProcessorLog] ADD  CONSTRAINT [DF_BatchProcessorLog_CreatedDateTime]  DEFAULT (getutcdate()) FOR [CreatedDateTime]
+GO
+
+ALTER TABLE [Batch].[BatchProcessorLog]  WITH CHECK ADD  CONSTRAINT [FK_BatchLog_BatchProcessor] FOREIGN KEY([BatchProcessorId])
+REFERENCES [Batch].[BatchProcessor] ([BatchProcessorId])
+GO
+
+ALTER TABLE [Batch].[BatchProcessorLog] CHECK CONSTRAINT [FK_BatchLog_BatchProcessor]
+GO
+
