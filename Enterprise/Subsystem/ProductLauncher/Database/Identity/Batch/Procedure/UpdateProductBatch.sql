158eb37524c574848e59893b72f0936a64dc50f5
James Reames <James.Reames@RealPage.com>
2022-05-02 09:57:16 -0500

    Merged PR 139246: updating product batch update to lock records during update to help with concurrency issue
    Update UpdateProductBatch.sql
    
    Related work items: #1131574

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Batch/Procedure/UpdateProductBatch.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Batch/Procedure/UpdateProductBatch.sql
@@ -1,5 +1,4 @@
-﻿
-CREATE PROCEDURE [Batch].[UpdateProductBatch]  
+﻿CREATE PROCEDURE [Batch].[UpdateProductBatch]  
 (@ProductBatchID INT,  
  @StatusTypeId   INT,  
  @InputJson      NVARCHAR(MAX) = NULL,  
@@ -12,6 +11,12 @@ AS
          DECLARE @RetryCount TINYINT;  
          BEGIN TRY  
              BEGIN TRAN;  
+
+             DECLARE @groupID AS BIGINT, @subjectUserPersonId AS BIGINT, @editoruserId AS BIGINT;  
+             SELECT @groupID = p.BatchProcessorGroupId, @subjectUserPersonId = p.SubjectUserPersonaId, @editoruserId = p.EditorUserPersonaId  
+             FROM Batch.BatchProcessor p WITH(UPDLOCK)
+             WHERE p.BatchProcessorId = @ProductBatchID;
+
              IF @StatusTypeId = 7 --Error  
                  BEGIN  
                      SELECT @RetryCount = RetryCount + 1  
@@ -38,40 +43,29 @@ AS
              WHERE BatchProcessorId = @ProductBatchID;  
   
              --Insert record in error table  
-             INSERT INTO Batch.[BatchProcessorError]  
-    ([BatchProcessorId],  
-     [Error]  
-    )  
-    VALUES  
-    (@ProductBatchID,  
-     @ErrorDetails  
-    );  
-  
-             DECLARE @groupID as bigint, @subjectUserPersonId as BIGINT, @editoruserId as BIGINT;  
+             INSERT INTO Batch.[BatchProcessorError] ([BatchProcessorId], [Error]) 
+				VALUES (@ProductBatchID,@ErrorDetails);  
   
              SELECT @groupID = p.BatchProcessorGroupId, @subjectUserPersonId = p.SubjectUserPersonaId, @editoruserId = p.EditorUserPersonaId  
              FROM Batch.BatchProcessor p  
              Where p.BatchProcessorId = @ProductBatchID;  
   
              SELECT   
-    CAST(CASE   
-     WHEN COUNT(*) > 0 THEN 0  
-    ELSE  
-     1  
-    END AS BIT)   
+				CAST(CASE WHEN COUNT(*) > 0 THEN 0 ELSE 1 END AS BIT)
              FROM Batch.BatchProcessor   
              WHERE BatchProcessorGroupId = @groupID
              AND SubjectUserPersonaId = @subjectUserPersonId  
 			 AND EditorUserPersonaId = @editoruserId
-    AND ( (StatusTypeId <> 8)   
-    AND (StatusTypeId <> 7 OR (StatusTypeId = 7 AND RetryCount < 3)))  
+			 AND ( (StatusTypeId <> 8)   
+			 AND (StatusTypeId <> 7 OR (StatusTypeId = 7 AND RetryCount < 3)))  
   
             COMMIT;   
          END TRY  
          BEGIN CATCH  
+             ROLLBACK;  
              DECLARE @ErrorLogID INT;  
              EXEC dbo.LogError  
                   @ErrorLogID = @ErrorLogID OUTPUT;  
-             ROLLBACK;  
+             
          END CATCH;  
      END;
