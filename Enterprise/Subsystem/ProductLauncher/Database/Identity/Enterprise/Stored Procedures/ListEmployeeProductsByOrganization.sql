1c26839bc6ec3f755e927079801953fe4e203106
Siva Savaram <siva.savaram@realpage.com>
2022-05-23 07:48:52 -0500

    Merged PR 142821: UL: Site Spend Management tile displays in Employee Dashboard
    UL: Site Spend Management tile displays in Employee Dashboard
    
    Related work items: #1097064

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Enterprise/Stored Procedures/ListEmployeeProductsByOrganization.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Enterprise/Stored Procedures/ListEmployeeProductsByOrganization.sql
@@ -41,7 +41,7 @@ BEGIN
 		INSERT INTO @CompanyOrganizationProduct ( ProductId , SupportsEmployeeCreation )
 		SELECT distinct ProductId , 0 from Enterprise.OrganizationProduct 
 		WHERE PartyId = @PartyId
-		And ProductId NOT IN (3,36,56,67)
+		And ProductId NOT IN (3,36,56,67,46)
 		AND ((@NOW BETWEEN FromDate AND ThruDate) OR (@NOW >= FromDate	AND ThruDate IS NULL))
 
 		--AD Groups
