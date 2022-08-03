c20f32c45ffacfb12f345fb28c438bb841bdb816
Jaswanth Bommu <Jaswanth.Bommu@RealPage.com>
2022-08-03 11:50:45 -0500

    Merged PR 154301: User Story 1213354: UL: External User - Add additional options for User Relat...
    User Story 1213354: UL: External User - Add additional options for User Relationship dropdown
    
    Related work items: #1213354

---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/1213354-add-operators.sql
@@ -0,0 +1,11 @@
+﻿IF NOT EXISTS(SELECT * FROM Enterprise.ThirdPartyRelationship WHERE ThirdPartyRelationship = 'Employee')
+BEGIN
+    INSERT INTO Enterprise.ThirdPartyRelationship(ThirdPartyRelationshipId,ThirdPartyRelationship)
+    VALUES(4,'Employee')
+END
+
+IF NOT EXISTS(SELECT * FROM Enterprise.ThirdPartyRelationship WHERE ThirdPartyRelationship = 'Other')
+BEGIN
+    INSERT INTO Enterprise.ThirdPartyRelationship(ThirdPartyRelationshipId,ThirdPartyRelationship)
+    VALUES(5,'Other')
+END
--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/Script.PostDeployment.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/Script.PostDeployment.sql
@@ -16,6 +16,7 @@ Post-Deployment Script Template
 :r .\PostDeploymentScripts\Batchprocessor-settings.sql
 :r .\PostDeploymentScripts\624898-independent-facilities.sql
 :r .\PostDeploymentScripts\AccessUDM-viaKongOrDirectly.sql
+:r .\PostDeploymentScripts\1213354-add-operators.sql
 
 :r .\PostDeploymentScripts\UpdateStatistics.sql
 :r .\PostDeploymentScripts\RecompileAllProcs.sql
