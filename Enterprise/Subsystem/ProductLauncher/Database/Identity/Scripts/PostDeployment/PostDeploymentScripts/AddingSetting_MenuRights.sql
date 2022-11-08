4325c363ec6b4f4fc38b2aa433348c3159b6d581
KalyanaChakravarthy Bingi <kalyanachakravarthy.bingi@RealPage.com>
2022-11-08 01:04:54 -0600

    Merged PR 170001: User Story 1202139: Settings cog in RealPage Employee company should be enabled with employee rights only
    User Story 1202139: Settings cog in RealPage Employee company should be enabled with employee rights only
    
    Related work items: #1202139

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/AddingSetting_MenuRights.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/AddingSetting_MenuRights.sql
@@ -1,79 +1,74 @@
 declare @RightId int
 declare @MenuId int
+--Setting 1
+select  @RightId = RightId from Security.[Right] where RightName = 'AccessSettingsAdmin'
+select @MenuId = Id from Enterprise.NavigationMenu where Title = 'Admin Console' and Origin = 'unified-settings'
 
---task 1
-select  @RightId = RightId from Security.[Right] where Value = 'Employee Access to Settings Admin Console'
-select @MenuId = Id from Enterprise.NavigationMenu where Title = 'Settings Activity Log' and Origin = 'unified-settings'
-select @RightId, @MenuId
 IF NOT EXISTS(select top 1 1 from Enterprise.NavigationMenuRights where NavigationMenuId = @MenuId and RightId = @RightId)
 BEGIN
 	INSERT INTO Enterprise.NavigationMenuRights(NavigationMenuId,RightId)
 	VALUES(@MenuId, @RightId)
 END
 
-
---task 2
-IF EXISTS (select top 1 1 from Security.[Right] where Value = 'Employee Access to Platform Settings and Service')
+--Setting 2
+IF EXISTS (select top 1 1 from Security.[Right] where Value = 'Employee Access to Login Page Setup')
 BEGIN
 	update Security.[Right] set Value ='Employee Access to Platform Settings and Service' where Value = 'Employee Access to Login Page Setup'
 END
-
-select @RightId = RightId from Security.[Right] where Value = 'Internal Admin Access to Manage Best Practice Settings Templates'
-
+select @RightId = RightId from Security.[Right] where Value = 'Employee Access to Platform Settings and Service'
 select @MenuId = Id from Enterprise.NavigationMenu where Title = 'Manage Settings' and Origin = 'unified-settings'
-
 IF NOT EXISTS(select top 1 1 from Enterprise.NavigationMenuRights where NavigationMenuId = @MenuId and RightId = @RightId)
 BEGIN
 INSERT INTO Enterprise.NavigationMenuRights(NavigationMenuId,RightId)
 VALUES(@MenuId, @RightId)
 END
 
-select @MenuId = Id from Enterprise.NavigationMenu where Title = 'settings-activity' and Origin = 'unified-settings'
-IF NOT EXISTS(select top 1 1 from Enterprise.NavigationMenuRights where NavigationMenuId = @MenuId and RightId = @RightId)
-BEGIN
-	INSERT INTO Enterprise.NavigationMenuRights(NavigationMenuId,RightId)
-	VALUES(@MenuId, @RightId)
-END
---task 3
-
-IF EXISTS (select top 1 1 from Security.[Right] where Value = 'Employee Access to Platform Settings and Service')
+--Setting 3
+IF EXISTS (select top 1 1 from Security.[Right] where Value = 'Employee Access to Manage Settings templates')
 BEGIN
 update Security.[Right] set Value ='Internal Admin Access to Manage Best Practice Settings Templates' where Value = 'Employee Access to Manage Settings templates'
 END
-
 select @RightId = RightId from Security.[Right] where Value = 'Internal Admin Access to Manage Best Practice Settings Templates'
 select @MenuId = Id from Enterprise.NavigationMenu where Title = 'Manage Templates' and Origin = 'unified-settings'
-
 IF NOT EXISTS(select top 1 1 from Enterprise.NavigationMenuRights where NavigationMenuId = @MenuId and RightId = @RightId)
 BEGIN
 	INSERT INTO Enterprise.NavigationMenuRights(NavigationMenuId,RightId)
 	VALUES(@MenuId, @RightId)
 END
 
---task4
-select   @RightId = RightId from Security.[Right] where Value ='View Settings Templates'
-select @MenuId = Id from Enterprise.NavigationMenu where Title = 'Manage Templates' and Origin = 'unified-settings'
+--Setting 4
 
+select   @RightId = RightId from Security.[Right] where Value ='Internal Admin Access to Unified Settings'
+select @MenuId = Id from Enterprise.NavigationMenu where Title = 'Manage Templates' and Origin = 'unified-settings'
 IF NOT EXISTS(select top 1 1 from Enterprise.NavigationMenuRights where NavigationMenuId = @MenuId and RightId = @RightId)
 BEGIN
 	INSERT INTO Enterprise.NavigationMenuRights(NavigationMenuId,RightId)
 	VALUES(@MenuId, @RightId)
 END
---task 5 (No need as of now)
---task 6
-IF NOT EXISTS(select top 1 1 from Security.[Right] where Value = 'View Settings Templates')
-BEGIN
-	INSERT INTO Security.[Right](RightName, [Description], [Value], StatusTypeId, VisibilityStatusId, ProductId, TargetProductId,CreatedBy,CreatedDate,PersistRight)
-	values('ViewSettingsTemplates',	'This right allows a user access to Settings > Manage Templates to view templates.',	'View Settings Templates',	13,	9,	3,	56,	4325,GETUTCDATE(),0)
-END
 
+--Setting 5 
 
-select   @RightId = RightId from Security.[Right] where Value ='View Settings Templates'
-
-select @MenuId = Id from Enterprise.NavigationMenu where Title = 'Manage Templates' and Origin = 'unified-settings'
+select   @RightId = RightId from Security.[Right] where Value ='Manage Settings Templates'
+select @MenuId = Id from Enterprise.NavigationMenu where Title = 'Manage Settings' and Origin = 'unified-settings'
 IF NOT EXISTS(select top 1 1 from Enterprise.NavigationMenuRights where NavigationMenuId = @MenuId and RightId = @RightId)
 BEGIN
 INSERT INTO Enterprise.NavigationMenuRights(NavigationMenuId,RightId)
 VALUES(@MenuId, @RightId)
 END
 
+
+--Setting 6
+Declare @userId int
+select @userId = userid from Ident.UserLogin where LoginName like '%realpagead@%'
+IF NOT EXISTS(select top 1 1 from Security.[Right] where Value = 'View Settings Templates')
+BEGIN
+	INSERT INTO Security.[Right](RightName, [Description], [Value], StatusTypeId, VisibilityStatusId, ProductId, TargetProductId,CreatedBy,CreatedDate,PersistRight)
+	values('ViewSettingsTemplates',	'This right allows a user access to Settings > Manage Templates to view templates.',	'View Settings Templates',	13,	9,	3,	56,	@userId,GETUTCDATE(),0)
+END
+select   @RightId = RightId from Security.[Right] where Value ='View Settings Templates'
+select @MenuId = Id from Enterprise.NavigationMenu where Title = 'Manage Settings' and Origin = 'unified-settings'
+IF NOT EXISTS(select top 1 1 from Enterprise.NavigationMenuRights where NavigationMenuId = @MenuId and RightId = @RightId)
+BEGIN
+INSERT INTO Enterprise.NavigationMenuRights(NavigationMenuId,RightId)
+VALUES(@MenuId, @RightId)
+END
