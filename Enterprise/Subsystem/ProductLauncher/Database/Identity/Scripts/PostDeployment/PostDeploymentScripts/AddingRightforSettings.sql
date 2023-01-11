834c8f190d70de60e05f530de1e8e83139a6574d
Siva Savaram <siva.savaram@realpage.com>
2023-01-11 09:28:36 +0000

    Updated AddingRightforSettings.sql

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/AddingRightforSettings.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/AddingRightforSettings.sql
@@ -3,10 +3,10 @@
  declare @rightId1 bigint,@rightId2 bigint,@rightId3 bigint,@rightId4 bigint,@rightId5 bigint,@rightId6 bigint,@rightId7 bigint,@rightId8 bigint;
  declare @MenuId1 Bigint,@MenuId2 Bigint,@MenuId3 Bigint,@MenuId4 Bigint;
 
-select @rightId1 = RightId from Security.[Right] where Value = 'Internal Admin Access to Manage Best Practice Settings Templates';
-select @rightId2 = RightId from Security.[Right] where Value = 'View all company-level settings';
-select @rightId3 = RightId from Security.[Right] where Value = 'View all property-level settings';
-select @rightId4 = RightId from Security.[Right] where Value = 'Manage property-level settings';
+select @rightId1 = RightId from Security.[Right] where RightName = 'EmployeeAccesstoManageSettingsTemplates';
+select @rightId2 = RightId from Security.[Right] where  RightName = 'Viewallcompanylevelsettings';
+select @rightId3 = RightId from Security.[Right] where RightName = 'Viewallpropertylevelsettings';
+select @rightId4 = RightId from Security.[Right] where RightName = 'Managepropertylevelsettings';
 
 select @MenuId1 = Id from Enterprise.NavigationMenu where Title = 'Manage Settings' and Origin = 'unified-settings'
 
