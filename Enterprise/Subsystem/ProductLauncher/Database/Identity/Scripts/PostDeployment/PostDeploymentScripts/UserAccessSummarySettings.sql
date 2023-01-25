34675b0c6b85c5cbf7716e4fadc988484997670e
James Reames <James.Reames@RealPage.com>
2023-01-25 14:32:11 -0600

    Merged PR 181823: fixing setting descriptions that were too long
    Related work items: #1188089, #1391868

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/UserAccessSummarySettings.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/UserAccessSummarySettings.sql
@@ -2,29 +2,29 @@ Go
 -- UserAccessSummary and Report settings
 if NOT EXISTS ( SELECT TOP 1 1 FROM Enterprise.ProductSettingType WHERE name = 'UserReport_MaxDOP' )
 BEGIN
-	INSERT INTO enterprise.ProductSettingType ( name, Description, SensitiveData ) VALUES ( 'UserReport_MaxDOP', 'The MaxDegreeOfParallelism to use when getting the user product roles and properties for the user report.', 0)
+	INSERT INTO enterprise.ProductSettingType ( name, Description, SensitiveData ) VALUES ( 'UserReport_MaxDOP', 'The max threads to use when calling apis for the user report.', 0)
 END
 ELSE
 BEGIN
-	UPDATE Enterprise.ProductSettingType SET Description = 'The MaxDegreeOfParallelism to use when getting the user product roles and properties for the user report.' WHERE Name = 'UserReport_MaxDOP'
+	UPDATE Enterprise.ProductSettingType SET Description = 'The max threads to use when calling apis for the user report.' WHERE Name = 'UserReport_MaxDOP'
 END
 
 IF NOT EXISTS ( SELECT TOP 1 1 FROM Enterprise.ProductSettingType WHERE name = 'UserReport_ProductApiCacheDuration' )
 BEGIN
-	INSERT INTO enterprise.ProductSettingType ( name, Description, SensitiveData ) VALUES ( 'UserReport_ProductApiCacheDuration', 'The number of minutes to cache a users product information for the user report. Should be greater than 0.', 0)
+	INSERT INTO enterprise.ProductSettingType ( name, Description, SensitiveData ) VALUES ( 'UserReport_ProductApiCacheDuration', 'The number of minutes to cache product information for the user report. Should be greater than 0.', 0)
 END
 ELSE
 BEGIN
-	UPDATE Enterprise.ProductSettingType SET Description = 'The number of minutes to cache a users product information for the user report. Should be greater than 0.' WHERE Name = 'UserReport_ProductApiCacheDuration'
+	UPDATE Enterprise.ProductSettingType SET Description = 'The number of minutes to cache product information for the user report. Should be greater than 0.' WHERE Name = 'UserReport_ProductApiCacheDuration'
 END
 
 IF NOT EXISTS ( SELECT TOP 1 1 FROM Enterprise.ProductSettingType WHERE name = 'UserReport_RetryUserReportCount' )
 BEGIN
-	INSERT INTO enterprise.ProductSettingType ( name, Description, SensitiveData ) VALUES ( 'UserReport_RetryUserReportCount', 'The number of times to retry the product api when getting a users properties or roles for the user report.', 0)
+	INSERT INTO enterprise.ProductSettingType ( name, Description, SensitiveData ) VALUES ( 'UserReport_RetryUserReportCount', 'The number of times to retry the product api for the user report.', 0)
 END
 ELSE
 BEGIN
-	UPDATE Enterprise.ProductSettingType SET Description = 'The number of times to retry the product api when getting a users properties or roles for the user report.' WHERE Name = 'UserReport_RetryUserReportCount'
+	UPDATE Enterprise.ProductSettingType SET Description = 'The number of times to retry the product api for the user report.' WHERE Name = 'UserReport_RetryUserReportCount'
 END
 
 IF NOT EXISTS ( SELECT TOP 1 1 FROM Enterprise.ProductSettingType WHERE name = 'UserReport_MaxRunningTime' )
@@ -38,39 +38,39 @@ END
 
 IF NOT EXISTS ( SELECT TOP 1 1 FROM Enterprise.ProductSettingType WHERE name = 'UserReport_ExcludeProductIdList' )
 BEGIN
-	INSERT INTO enterprise.ProductSettingType ( name, Description, SensitiveData ) VALUES ( 'UserReport_ExcludeProductIdList', 'The list of product ids to exclude when retrieving a users list of products when running a user report.', 0)
+	INSERT INTO enterprise.ProductSettingType ( name, Description, SensitiveData ) VALUES ( 'UserReport_ExcludeProductIdList', 'Product ids to exclude when running a user report.', 0)
 END
 ELSE
 BEGIN
-	UPDATE Enterprise.ProductSettingType SET Description = 'The list of product ids to exclude when retrieving a users list of products when running a user report.' WHERE Name = 'UserReport_ExcludeProductIdList'
+	UPDATE Enterprise.ProductSettingType SET Description = 'Product ids to exclude when running a user report.' WHERE Name = 'UserReport_ExcludeProductIdList'
 END
 
 ------------------------------------------------------------
 if NOT EXISTS ( SELECT TOP 1 1 FROM Enterprise.ProductSettingType WHERE name = 'UserAccessDetails_MaxDOP' )
 BEGIN
-	INSERT INTO enterprise.ProductSettingType ( name, Description, SensitiveData ) VALUES ( 'UserAccessDetails_MaxDOP', 'The MaxDegreeOfParallelism to use when getting the user product roles and properties for the user access details.', 0)
+	INSERT INTO enterprise.ProductSettingType ( name, Description, SensitiveData ) VALUES ( 'UserAccessDetails_MaxDOP', 'The max threads to use when calling apis for the user access details.', 0)
 END
 ELSE
 BEGIN
-	UPDATE Enterprise.ProductSettingType SET Description = 'The MaxDegreeOfParallelism to use when getting the user product roles and properties for the user access details.' WHERE Name = 'UserAccessDetails_MaxDOP'
+	UPDATE Enterprise.ProductSettingType SET Description = 'The max threads to use when calling apis for the user access details.' WHERE Name = 'UserAccessDetails_MaxDOP'
 END
 
 IF NOT EXISTS ( SELECT TOP 1 1 FROM Enterprise.ProductSettingType WHERE name = 'UserAccessDetails_ProductApiCacheDuration' )
 BEGIN
-	INSERT INTO enterprise.ProductSettingType ( name, Description, SensitiveData ) VALUES ( 'UserAccessDetails_ProductApiCacheDuration', 'The number of minutes to cache a users product information for the user access details. Should be greater than 0.', 0)
+	INSERT INTO enterprise.ProductSettingType ( name, Description, SensitiveData ) VALUES ( 'UserAccessDetails_ProductApiCacheDuration', 'The number of minutes to cache product info for the user access details. Should be greater than 0.', 0)
 END
 ELSE
 BEGIN
-	UPDATE Enterprise.ProductSettingType SET Description = 'The number of minutes to cache a users product information for the user access details. Should be greater than 0.' WHERE Name = 'UserAccessDetails_ProductApiCacheDuration'
+	UPDATE Enterprise.ProductSettingType SET Description = 'The number of minutes to cache product info for the user access details. Should be greater than 0.' WHERE Name = 'UserAccessDetails_ProductApiCacheDuration'
 END
 
 IF NOT EXISTS ( SELECT TOP 1 1 FROM Enterprise.ProductSettingType WHERE name = 'UserAccessDetails_RetryUserReportCount' )
 BEGIN
-	INSERT INTO enterprise.ProductSettingType ( name, Description, SensitiveData ) VALUES ( 'UserAccessDetails_RetryUserReportCount', 'The number of times to retry the product api when getting a users properties or roles for the user access details.', 0)
+	INSERT INTO enterprise.ProductSettingType ( name, Description, SensitiveData ) VALUES ( 'UserAccessDetails_RetryUserReportCount', 'The number of times to retry the product api for the user access details.', 0)
 END
 ELSE
 BEGIN
-	UPDATE Enterprise.ProductSettingType SET Description = 'The number of times to retry the product api when getting a users properties or roles for the user access details.' WHERE Name = 'UserAccessDetails_RetryUserReportCount'
+	UPDATE Enterprise.ProductSettingType SET Description = 'The number of times to retry the product api for the user access details.' WHERE Name = 'UserAccessDetails_RetryUserReportCount'
 END
 
 IF NOT EXISTS ( SELECT TOP 1 1 FROM Enterprise.ProductSettingType WHERE name = 'UserAccessDetails_MaxRunningTime' )
@@ -84,11 +84,11 @@ END
 
 IF NOT EXISTS ( SELECT TOP 1 1 FROM Enterprise.ProductSettingType WHERE name = 'UserAccessDetails_ExcludeProductIdList' )
 BEGIN
-	INSERT INTO enterprise.ProductSettingType ( name, Description, SensitiveData ) VALUES ( 'UserAccessDetails_ExcludeProductIdList', 'The list of product ids to exclude when retrieving a users list of products when running a user access details.', 0)
+	INSERT INTO enterprise.ProductSettingType ( name, Description, SensitiveData ) VALUES ( 'UserAccessDetails_ExcludeProductIdList', 'Product ids to exclude when running user access details.', 0)
 END
 ELSE
 BEGIN
-	UPDATE Enterprise.ProductSettingType SET Description = 'The list of product ids to exclude when retrieving a users list of products when running a user access details.' WHERE Name = 'UserAccessDetails_ExcludeProductIdList'
+	UPDATE Enterprise.ProductSettingType SET Description = 'Product ids to exclude when running user access details.' WHERE Name = 'UserAccessDetails_ExcludeProductIdList'
 END
 
 -- UserAccessSummary and Report settings
