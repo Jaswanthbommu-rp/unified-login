7f0c89fbb7d65a7cda7170378eb65a63f613f0cb
Mallesham Sankati <mallesham.sankati@RealPage.com>
2023-02-08 00:12:19 -0600

    Merged PR 183665: Bug 1301949: Enterprise Roles and Primary Properties - issues with assigning/...
    Bug 1301949: Enterprise Roles and Primary Properties - issues with assigning/editing products for a user
    
    Related work items: #1301949

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/UserManagement/Stored Procedures/GetEnterpriseRoleProductsByOrganization.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/UserManagement/Stored Procedures/GetEnterpriseRoleProductsByOrganization.sql
@@ -1,18 +1,23 @@
-﻿CREATE PROCEDURE [Security].[GetEnterpriseRoleProductsByOrganization]
-(
-	@RoleTemplateId int, 
-	@OrganizationRealPageId UNIQUEIDENTIFIER = NULL,
-    @PartyId BIGINT = NULL
-)
-AS
-BEGIN
-	SELECT DISTINCT RTP.ProductId
+﻿CREATE PROCEDURE [Security].[GetEnterpriseRoleProductsByOrganization]    
+(    
+ @RoleTemplateId int,     
+ @OrganizationRealPageId UNIQUEIDENTIFIER = NULL,    
+    @PartyId BIGINT = NULL    
+)    
+AS    
+BEGIN    
+ DECLARE @NOW DATETIME = GETUTCDATE();  
+ SELECT DISTINCT RTP.ProductId
 	FROM [Security].[RoleTemplate] RT
 	JOIN [Security].[RoleTemplateProduct] RTP ON
 		RT.RoleTemplateId = RTP.RoleTemplateId
 	JOIN Enterprise.Party P ON
 		p.PartyId = RT.PartyID
-	WHERE RT.RoleTemplateId = @RoleTemplateId
-	AND (P.RealPageId = @OrganizationRealPageId OR @OrganizationRealPageId IS NULL)
-	AND (P.PartyId = @PartyId OR @PartyId IS NULL)
+ JOIN Enterprise.OrganizationProduct OP ON OP.ProductId = RTP.ProductId AND p.PartyId = OP.PartyId
+ JOIN Enterprise.GlobalProductConfiguration GPC on GPC.ConfigurationId =OP.ConfigurationId AND  GPC.ProductId = RTP.ProductId
+ WHERE RT.RoleTemplateId = @RoleTemplateId  
+ AND (P.RealPageId = @OrganizationRealPageId OR @OrganizationRealPageId IS NULL)   
+ AND (P.PartyId = @PartyId OR @PartyId IS NULL)  
+ AND ((@NOW BETWEEN gpc.FromDate AND gpc.ThruDate) OR (@NOW >= gpc.FromDate AND gpc.ThruDate IS NULL))  
+ AND ((@NOW BETWEEN OP.FromDate AND OP.ThruDate) OR (@NOW >= OP.FromDate AND OP.ThruDate IS NULL))  
 END;
