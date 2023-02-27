3f90619c9c95fe22d174064be73848071f1fcfd1
KalyanaChakravarthy Bingi <kalyanachakravarthy.bingi@RealPage.com>
2023-02-27 08:16:02 -0600

    Merged PR 187668: 1276811 : Added setting script for Admin portal tile
    1276811 : Added setting script for Admin portal tile

---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/AddingSettingsForAdminPortal.sql
@@ -0,0 +1,23 @@
+﻿ -- User Story 1276811: UL: Admin & Support Portal - Dynamic change of tile name (dev only)
+DECLARE @ProductsettingTypeid int;
+IF NOT EXISTS (SELECT TOP 1 1 FROM Enterprise.ProductSettingType WHERE [Name] = 'BatchUserProductStatusSleepTimeout')
+BEGIN
+	INSERT INTO Enterprise.ProductSettingType ([Name], [Description], SensitiveData)
+	VALUES ('BatchUserProductStatusSleepTimeout', 'The amount of time in ms to wait checking a product batch process', 0);
+
+	SELECT @ProductsettingTypeid = ProductSettingTypeId FROM Enterprise.ProductSettingType WHERE [Name] = 'BatchUserProductStatusSleepTimeout'
+    exec [Enterprise].[SetProductSetting] 0,89,@ProductsettingTypeid,'5000'
+
+END
+
+IF NOT EXISTS (SELECT TOP 1 1 FROM Enterprise.ProductSettingType WHERE [Name] = 'BatchUserProductStatusRetryCount')
+BEGIN
+	INSERT INTO Enterprise.ProductSettingType ([Name], [Description], SensitiveData)
+	VALUES ('BatchUserProductStatusRetryCount', 'The number of times to retry when checking if a product batch process is complete', 0);
+
+	SELECT @ProductsettingTypeid = ProductSettingTypeId FROM Enterprise.ProductSettingType WHERE [Name] = 'BatchUserProductStatusRetryCount'
+    exec [Enterprise].[SetProductSetting] 0,89,@ProductsettingTypeid,'5'
+
+END
+GO
+ 
