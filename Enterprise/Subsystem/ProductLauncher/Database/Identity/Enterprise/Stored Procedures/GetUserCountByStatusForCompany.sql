3c3dd1458685e939269bcf272ed35e50f86ca90a
Jaswanth Bommu <Jaswanth.Bommu@RealPage.com>
2023-02-27 01:17:05 -0600

    Merged PR 187200: User Story 752731: API to get count of UL Active users for a PMC
    User Story 752731: API to get count of UL Active users for a PMC
    
    Related work items: #752731

---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Enterprise/Stored Procedures/GetUserCountByStatusForCompany.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [Enterprise].[GetUserCountByStatusForCompany]
+(
+	@companyId UNIQUEIDENTIFIER
+)
+AS
+BEGIN
+	SELECT 
+		COUNT(p.personaId) AS [ActiveUsers]
+		FROM ident.userlogin ul
+		INNER JOIN ident.UserLoginPersona ulp ON ulp.UserLoginId = ul.UserId
+		INNER JOIN person.persona p ON p.UserLoginPersonaId = ulp.UserLoginPersonaId
+		INNER JOIN Enterprise.Organization o ON o.PartyId = ulp.OrganizationPartyId
+		INNER JOIN Enterprise.Party P2 ON P2.PartyId=o.PartyId
+		WHERE P2.RealPageId=@CompanyId
+		AND ulp.StatusTypeId IN (1,2,3,12)
+END
