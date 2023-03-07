54d1ea79597174bddc85a71c3290994d4f5dfd72
Mallesham Sankati <mallesham.sankati@RealPage.com>
2023-03-07 01:08:04 -0600

    Merged PR 188978: Merged PR 186282: Bug 1288169: UL: Enterprise Roles name validation is not co...
    Merged PR 186282: Bug 1288169: UL: Enterprise Roles name validation is not contained within a PMC
    
    Bug 1288169: UL: Enterprise Roles name validation is not contained within a PMC
    
    Related work items: #1288169
    
    Cherry-picked from commit `7faa3f42`.
    
    Related work items: #1288169

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/RoleRightsManagement/Stored Procedures/GetRoleTemplateByRoleTemplateName.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/RoleRightsManagement/Stored Procedures/GetRoleTemplateByRoleTemplateName.sql
@@ -1,10 +1,10 @@
-CREATE PROCEDURE [Security].[GetRoleTemplateByRoleTemplateName] (
- @RoleTemplateName varchar(100)
-)    
-AS    
-BEGIN 
-	SELECT 
-		RoleTemplateId,
-		RoleTemplateName
-	FROM Security.RoleTemplate WHERE RoleTemplateName = @RoleTemplateName
+CREATE PROCEDURE [Security].[GetRoleTemplateByRoleTemplateName] (  
+ @RoleTemplateName varchar(100)  , @PartyId bigint
+)      
+AS      
+BEGIN   
+ SELECT   
+  RoleTemplateId,  
+  RoleTemplateName  
+ FROM [Security].RoleTemplate WHERE RoleTemplateName = @RoleTemplateName and PartyID = @PartyId
 END;
