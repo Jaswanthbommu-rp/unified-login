f7cd237339553ad694fda77810bd7cac2cfe6e4e
Jaswanth Bommu <Jaswanth.Bommu@RealPage.com>
2023-04-13 09:09:46 -0500

    Merged PR 196018: User Story 1396670: User Audit Report - Pt2: Create Report - duplicate records
    Duplicate records error
    
    Related work items: #1396670

--- Enterprise/Subsystem/ProductLauncher/Database/UPReporting/UserAudit/Tables/UserProduct.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/UPReporting/UserAudit/Tables/UserProduct.sql
@@ -5,11 +5,8 @@
     [ProductId]             INT           NOT NULL,
     [UserName]              VARCHAR (250) NULL,
     [CreatedDate]           DATETIME2 (7) DEFAULT (getutcdate()) NULL,
-    [PropertyCompletedDate] DATETIME2 (7) NULL,
-    [RoleCompletedDate]     DATETIME2 (7) NULL,
-    [PropertyStatus]        TINYINT       DEFAULT ((0)) NULL,
-    [RoleStatus]            TINYINT       DEFAULT ((0)) NULL,
+    [CompletedDate]         DATETIME2 (7) NULL,
+    [Status]                TINYINT       DEFAULT ((0)) NULL
     CONSTRAINT [PK_UserProduct_UserProductId] PRIMARY KEY CLUSTERED ([UserProductId] ASC),
     CONSTRAINT [FK_UserProduct_AuditUserId] FOREIGN KEY ([AuditUserId]) REFERENCES [UserAudit].[User] ([AuditUserId]) ON DELETE CASCADE
 );
-
