4abb063a482c91c8e0dd707be69d27881529dab3
Siva Savaram <siva.savaram@realpage.com>
2023-04-19 12:30:42 +0000

    Updated ManageTelecommunicationNumberTests.cs

--- Enterprise/Subsystem/ProductLauncher/Service/LandingAPI/LandingAPI.Test/Logic/ManageTelecommunicationNumberTests.cs
+++ Enterprise/Subsystem/ProductLauncher/Service/LandingAPI/LandingAPI.Test/Logic/ManageTelecommunicationNumberTests.cs
@@ -110,7 +110,7 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.LandingAPI.Test.Logic
 						w.PartyContactMechanismId == o.PartyContactMechanismId
 					)
 				) == true
-				&& NumberOfProperties == 10
+				&& NumberOfProperties == 11
 			);
 		}
 	}
