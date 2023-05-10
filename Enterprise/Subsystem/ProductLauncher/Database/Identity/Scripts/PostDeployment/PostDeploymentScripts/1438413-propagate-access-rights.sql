7888cb49717c2ed49503e24297d449980351473f
Siva Savaram <siva.savaram@realpage.com>
2023-05-10 09:31:53 -0500

    Merged PR 201009: Fix for Reporting menu display based on reporting rights
    Fix for Reporting menu display based on reporting rights
    
    Related work items: #1485763

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/1438413-propagate-access-rights.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/1438413-propagate-access-rights.sql
@@ -52,3 +52,14 @@ UPDATE [Security].[Right] SET PersistRight = 1 WHERE RightName IN
 )
 
 GO
+
+Declare @Id int, @rightId bigint;
+Select @Id = Id from Enterprise.NavigationMenu where Title = 'Manage Reports' and Origin = 'unified-reporting';
+Select @rightId = RightId from Security.[Right] where RightName = 'ManagePropertyLevelReporting';
+
+IF NOT EXISTS (Select Top 1 1 from enterprise.navigationMenuRights where NavigationMenuId =@Id and RightId = @rightId)
+BEGIN
+  Insert into enterprise.navigationMenuRights (NavigationMenuId,RightId) values (@Id,@rightId);
+END
+
+Go
