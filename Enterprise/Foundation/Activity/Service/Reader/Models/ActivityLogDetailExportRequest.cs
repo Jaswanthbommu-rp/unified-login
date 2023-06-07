7a5bc3d4d5497fa2b75251d8251fe8ec436eba9c
Muzammil Khaja Mohammed <muzammil.khaja@realpage.com>
2023-06-07 12:26:12 +0530

    namespace change, include file in proj

--- Enterprise/Foundation/Activity/Service/Reader/Models/ActivityLogDetailExportRequest.cs
+++ Enterprise/Foundation/Activity/Service/Reader/Models/ActivityLogDetailExportRequest.cs
@@ -2,7 +2,7 @@
 using System;
 using System.Collections.Generic;
 
-namespace RP.Enterprise.Foundation.Activity.Service.Logging.Shared.Models
+namespace RP.Enterprise.Foundation.Activity.Service.Logging.Reader.Models
 {
     public class ActivityLogDetailExportRequest
     {
