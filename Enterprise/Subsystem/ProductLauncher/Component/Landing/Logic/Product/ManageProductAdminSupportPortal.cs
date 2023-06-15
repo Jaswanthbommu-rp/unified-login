d57c97673ebfb5e9efeae5df0fc577efe0bf68f3
KalyanaChakravarthy Bingi <kalyanachakravarthy.bingi@RealPage.com>
2023-06-15 01:17:42 -0500

    Merged PR 207042: Revert 'User Story 1515457: Do not create duplicate contact record creation in Salesforce'
    User Story 1515457: Do not create duplicate contact record creation in Salesforce
    
    Reverts !204940
    
    Related work items: #1515457

--- Enterprise/Subsystem/ProductLauncher/Component/Landing/Logic/Product/ManageProductAdminSupportPortal.cs
+++ Enterprise/Subsystem/ProductLauncher/Component/Landing/Logic/Product/ManageProductAdminSupportPortal.cs
@@ -340,13 +340,6 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Component.Landing.Logic.Produc
 
                             adminSupportPortalContactResults.Add(item);
                         }
-                        else if ((item.OMS_ID__c != null && item.OMS_ID__c.StartsWith("C") && !string.IsNullOrEmpty(item.ParentOMS_ID__c) && item.ParentOMS_ID__c.StartsWith("C")))
-                        {
-                            WriteToDiagnosticLog($"ManageProductAdminSupportPortal.ManageAdminSupportPortalUser - Company is having a Parent company - {editorPersonaId} and editorPersonaId  - {userPersonaId}");
-
-                            adminSupportPortalContactResults.Add(item);
-
-                        }
                     }
                 }
 
