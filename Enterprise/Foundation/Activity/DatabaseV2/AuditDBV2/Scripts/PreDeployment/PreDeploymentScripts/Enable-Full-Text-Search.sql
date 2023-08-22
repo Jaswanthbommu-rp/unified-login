6ab0532815b0feedfbcab02481e9496da1e59f22
Jaswanth Bommu <Jaswanth.Bommu@RealPage.com>
2023-08-22 06:52:35 -0500

    Merged PR 219332: User Story 1584553: Activity logs performance by tuning the SQL changes in proc
    User Story 1584553: Activity logs performance by tuning the SQL changes in proc
    
    Related work items: #1584553

---
+++ Enterprise/Foundation/Activity/DatabaseV2/AuditDBV2/Scripts/PreDeployment/PreDeploymentScripts/Enable-Full-Text-Search.sql
@@ -0,0 +1,11 @@
+﻿GO
+IF NOT EXISTS (SELECT 1 FROM sys.fulltext_catalogs WHERE name = 'FullTextCatalog') 
+BEGIN 
+	CREATE FULLTEXT CATALOG FullTextCatalog AS DEFAULT;
+END
+GO
+IF NOT EXISTS(SELECT * FROM sys.indexes WHERE name = 'PK_Activity_ActivityId' AND object_id = OBJECT_ID('[Logging].[Activity]')) 
+BEGIN 
+	CREATE FULLTEXT INDEX ON [Logging].[Activity] (Message) KEY INDEX PK_Activity_ActivityId WITH STOPLIST = OFF 
+END
+GO
