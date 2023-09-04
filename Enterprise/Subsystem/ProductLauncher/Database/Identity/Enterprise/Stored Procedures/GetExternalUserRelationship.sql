dde02de043bc66c6fbc6e103c1dc5f45e522987c
KalyanaChakravarthy Bingi <kalyanachakravarthy.bingi@RealPage.com>
2023-09-04 05:26:12 -0500

    Merged PR 221595: User Story 1594124: Property Translation for AO Property IDs
    User Story 1594124: Property Translation for AO Property IDs
    
    Related work items: #1594124

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Enterprise/Stored Procedures/GetExternalUserRelationship.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Enterprise/Stored Procedures/GetExternalUserRelationship.sql
@@ -9,7 +9,9 @@ BEGIN
 		EUR.ThirdPartyRelationshipId,
 		TPR.ThirdPartyRelationship,
 		CASE WHEN EUR.ThirdPartyCompanyPartyId IS NULL THEN EUR.CompanyName ELSE O.Name END AS ThirdPartyCompanyName,
-		P.RealPageId AS ThirdPartyCompanyRealPageId
+		P.RealPageId AS ThirdPartyCompanyRealPageId,    
+		CASE WHEN OperatorValue IS NULL THEN NULL ELSE SUBSTRING(OperatorValue, 1, CHARINDEX('|', OperatorValue + '|') - 1) END AS OperatorCode,
+		CASE WHEN OperatorValue IS NULL THEN NULL ELSE SUBSTRING(OperatorValue, CHARINDEX('|', OperatorValue) + 1, LEN(OperatorValue)) END AS OperatorValue
 		
 	FROM enterprise.ExternalUserRelationship EUR 
 		INNER JOIN [Enterprise].[ThirdPartyRelationship] TPR ON EUR.ThirdPartyRelationshipId = TPR.ThirdPartyRelationshipId
