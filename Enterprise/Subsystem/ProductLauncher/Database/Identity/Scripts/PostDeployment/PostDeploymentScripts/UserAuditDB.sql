ee99756caf2375119461c3bf390da8596afce5fc
Shyam Yaddanapudi Yaddanapudi <shyam.yaddanapudi@realpage.com>
2023-09-14 16:02:12 +0530

    New product setting type

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/UserAuditDB.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/UserAuditDB.sql
@@ -175,3 +175,18 @@ BEGIN
 END
 
 GO
+
+IF NOT EXISTS (SELECT TOP 1 1 FROM Enterprise.ProductSettingType WHERE [Name] = 'UserAccessReport_MaxDOP')
+BEGIN
+	INSERT INTO Enterprise.ProductSettingType ([Name], [Description], SensitiveData)
+	VALUES ('UserAccessReport_MaxDOP', 'Max DOP Setting for Users Access Review Audit Reporting', 0);
+END
+DECLARE @ProductId INT = 3, @ProductsettingTypeid int;
+IF NOT EXISTS (SELECT TOP (1) (1) FROM ENTERPRISE.ProductSetting PS INNER JOIN enterprise.ProductSettingType pst ON pst.ProductSettingTypeId = PS.ProductSettingTypeId 
+    WHERE PS.ProductId = @ProductId AND pst.Name = 'UserAccessReport_MaxDOP' )
+BEGIN
+    SELECT @ProductsettingTypeid = ProductSettingTypeId FROM Enterprise.ProductSettingType WHERE [Name] = 'UserAccessReport_MaxDOP'
+    exec [Enterprise].[SetProductSetting] 0,@Productid,@ProductsettingTypeid, 12
+END
+
+GO
