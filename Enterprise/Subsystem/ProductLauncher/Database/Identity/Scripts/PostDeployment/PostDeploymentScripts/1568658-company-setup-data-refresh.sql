bdc0c96f0b565b207df0bb9c00a7a5f8f6cbf299
Jaswanth Bommu <Jaswanth.Bommu@RealPage.com>
2023-10-18 04:39:13 -0500

    Merged PR 229374: User Story 1568658: API & UI : Migration Tool Refresh in Company Setup
    User Story 1568658: API & UI : Migration Tool Refresh in Company Setup
    
    Related work items: #1568658

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/1568658-company-setup-data-refresh.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/1568658-company-setup-data-refresh.sql
@@ -5,4 +5,10 @@ BEGIN
 	VALUES(N'MigrationUri',N'Migration Domain Name',DEFAULT)
 END
 
+IF NOT EXISTS(SELECT 1 FROM Enterprise.ProductSettingType WHERE Name = 'MigrationUri')
+BEGIN
+	INSERT INTO Enterprise.ProductSettingType(Name,Description,SensitiveData)
+	VALUES(N'IgnoreProductsForMigrationTool',N'List of product id that do not support migration',DEFAULT)
+END
+
 GO
