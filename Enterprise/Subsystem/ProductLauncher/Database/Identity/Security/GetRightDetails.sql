e7d7897e8f29570af508a1696de87faae64e74c9
Shiva Kodaparthi <shiva.kodaparthi@Realpage.com>
2023-10-18 05:17:23 -0500

    Merged PR 229145: User Story 1666169: Add Setting that enables the right to be excluded from Impersonation
    DB changes for Right setting impersonation
    
    Related work items: #1666169

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Security/GetRightDetails.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Security/GetRightDetails.sql
@@ -1,13 +1,13 @@
 ﻿CREATE Procedure [Security].[GetRightDetails] (@rightId int)
 AS
-Begin
-	select r.RightId, RightName, r.Description, r.Value,
-	p.ProductId, p.Name as ProductName, r.TargetProductId, p1.Name as TargetProductName, st.StatusTypeId, st.Name as StatusType, 
-	r.VisibilityStatusId, st1.Name as Visibility
-	from Security.[Right] r
-	Join Enterprise.Product p on p.ProductId = r.ProductId
-	Join Enterprise.Product p1 on p1.ProductId = r.TargetProductId
-	Join Enterprise.StatusType st on st.StatusTypeId = r.StatusTypeId
-	Join Enterprise.StatusType st1  on st1.StatusTypeId = r.VisibilityStatusId
-	where r.RightId = @rightId
+Begin  
+ select r.RightId, RightName, r.Description, r.Value,  
+ p.ProductId, p.Name as ProductName, r.TargetProductId, p1.Name as TargetProductName, st.StatusTypeId, st.Name as StatusType,   
+ r.VisibilityStatusId, st1.Name as Visibility, r.IsExcludeRightFromImpersonation 
+ from Security.[Right] r  
+ Join Enterprise.Product p on p.ProductId = r.ProductId  
+ Join Enterprise.Product p1 on p1.ProductId = r.TargetProductId  
+ Join Enterprise.StatusType st on st.StatusTypeId = r.StatusTypeId  
+ Join Enterprise.StatusType st1  on st1.StatusTypeId = r.VisibilityStatusId  
+ where r.RightId = @rightId  
 End
