69d6d08d00c8a689d40cd48022af7b8206a94058
Uma Mahesh Nallamelli <umamahesh.nallamelli@realpage.com>
2023-11-29 19:08:21 +0530

    Role Error Change

--- Enterprise/Subsystem/ProductLauncher/Service/SharedObjects/ResponseObject/Error.cs
+++ Enterprise/Subsystem/ProductLauncher/Service/SharedObjects/ResponseObject/Error.cs
@@ -8,18 +8,21 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Component.SharedObjects.Respon
         public string Source { get; set; }
         public string Title { get; set; }
         public string Detail { get; set; }
-        [JsonProperty(PropertyName = "msgCode")]
-        public string MessageCode { get; set; }
-        [JsonProperty(PropertyName = "message")]
-        public string Message { get; set; }
     }
     public class FieldErrors
     {
-        public Error Error { get; set; }
+        public RoleError Error { get; set; }
     }
     public class RoleErrors
     {
         [JsonProperty(PropertyName = "fieldErrors")]
         public FieldErrors FieldErrors { get; set; }
+    }
+    public class RoleError
+    {
+        [JsonProperty(PropertyName = "msgCode")]
+        public string MessageCode { get; set; }
+        [JsonProperty(PropertyName = "message")]
+        public string Message { get; set; }
     }
 }
