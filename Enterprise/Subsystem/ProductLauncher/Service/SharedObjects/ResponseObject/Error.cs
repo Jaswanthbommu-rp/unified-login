cad61ee2e727e5fd316ac8ae0d84a2ed50ef378c
Uma Mahesh Nallamelli <umamahesh.nallamelli@realpage.com>
2023-12-01 12:13:13 +0530

    Added new class for Role errors

--- Enterprise/Subsystem/ProductLauncher/Service/SharedObjects/ResponseObject/Error.cs
+++ Enterprise/Subsystem/ProductLauncher/Service/SharedObjects/ResponseObject/Error.cs
@@ -1,6 +1,4 @@
-﻿using Newtonsoft.Json;
-
-namespace RP.Enterprise.Subsystem.ProductLauncher.Component.SharedObjects.ResponseObject
+﻿namespace RP.Enterprise.Subsystem.ProductLauncher.Component.SharedObjects.ResponseObject
 {
     public class Error
     {
@@ -8,21 +6,5 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Component.SharedObjects.Respon
         public string Source { get; set; }
         public string Title { get; set; }
         public string Detail { get; set; }
-    }
-    public class FieldErrors
-    {
-        public RoleError Error { get; set; }
-    }
-    public class RoleErrors
-    {
-        [JsonProperty(PropertyName = "fieldErrors")]
-        public FieldErrors FieldErrors { get; set; }
-    }
-    public class RoleError
-    {
-        [JsonProperty(PropertyName = "msgCode")]
-        public string MessageCode { get; set; }
-        [JsonProperty(PropertyName = "message")]
-        public string Message { get; set; }
     }
 }
