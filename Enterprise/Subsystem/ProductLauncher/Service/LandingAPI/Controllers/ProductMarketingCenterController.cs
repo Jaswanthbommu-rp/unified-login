6c7442a142f814bc577e4cb40cb65b22b9c0ee57
Uma Mahesh Nallamelli <umamahesh.nallamelli@realpage.com>
2024-01-03 15:54:59 +0530

    Fixing Sonar comments

--- Enterprise/Subsystem/ProductLauncher/Service/LandingAPI/Controllers/ProductMarketingCenterController.cs
+++ Enterprise/Subsystem/ProductLauncher/Service/LandingAPI/Controllers/ProductMarketingCenterController.cs
@@ -461,7 +461,27 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Service.LandingAPI.Controllers
             if (editorPersonaId == 0 || editorPersonaId == 0) { throw new HttpResponseException(HttpStatusCode.BadRequest); }
             ManageProductMarketingCenter mc = new ManageProductMarketingCenter(base._userClaims);
             ListResponse response = mc.UpdateNewMCRoleWithRights(editorPersonaId, mcRole);
-            return Request.CreateResponse(Convert.ToString(response.Additional) == "RoleError" ? HttpStatusCode.OK : (!response.IsError ? HttpStatusCode.OK : HttpStatusCode.BadRequest), response);
+
+            HttpStatusCode responseStatus;
+            if (Convert.ToString(response.Additional) == "RoleError")
+            {
+                responseStatus = HttpStatusCode.OK;
+            }
+            else
+            {
+                if (!response.IsError)
+                {
+                    responseStatus = HttpStatusCode.OK;
+                }
+                else
+                {
+                    responseStatus = HttpStatusCode.BadRequest;
+                }
+            }
+
+            return Request.CreateResponse(responseStatus, response);
+
+            //return Request.CreateResponse(Convert.ToString(response.Additional) == "RoleError" ? HttpStatusCode.OK : (!response.IsError ? HttpStatusCode.OK : HttpStatusCode.BadRequest), response);
         }
         #endregion
 
