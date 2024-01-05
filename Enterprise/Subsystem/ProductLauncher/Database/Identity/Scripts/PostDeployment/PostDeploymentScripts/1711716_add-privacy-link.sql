15072f966397d9a59b199c9f26fc50743c03ae3d
Jaswanth Bommu <Jaswanth.Bommu@RealPage.com>
2024-01-05 05:15:52 -0600

    Merged PR 241791: 1711716-add-link-for-privacy-policy-login-screen
    1711716-add-link-for-privacy-policy-login-screen
    
    Related work items: #1711716

---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/1711716_add-privacy-link.sql
@@ -0,0 +1,22 @@
+﻿GO
+
+DECLARE	@RealPageEmployeePartyId BIGINT, @PlatformLoginPageCategoryId SMALLINT, @PrivacyUrl NVARCHAR(100), @ServerName SYSNAME = @@SERVERNAME
+
+SELECT @RealPageEmployeePartyId = PartyId FROM Enterprise.Organization WHERE Name = 'realpage employee';
+SELECT @PlatformLoginPageCategoryId = SettingCategoryTypeId FROM [Settings].[SettingCategoryType] WHERE Name = 'platformloginpage';
+
+IF @ServerName IN ('reagbkdbsql001','repgbkdbsql001a','repgbkdbsql001b') --EUSAT, EUPROD
+BEGIN
+	SET @PrivacyUrl = N'https://www.realpage.co.uk/privacy-policy'
+END
+ELSE
+BEGIN
+	SET @PrivacyUrl = N'https://www.realpage.com/privacy-policy'
+END
+
+IF NOT EXISTS (SELECT * FROM [Settings].[OrganizationSettings] WHERE PartyId = @RealPageEmployeePartyId AND SettingCategoryTypeId=@PlatformLoginPageCategoryId)
+BEGIN
+	INSERT INTO Settings.OrganizationSettings(PartyId,SettingCategoryTypeId,MappingName,MappingValue,Editable,Hidden,CreatedBy,CreatedDate,UpdatedDate)
+	VALUES(@RealPageEmployeePartyId, @PlatformLoginPageCategoryId, N'privacyurl', @PrivacyUrl, DEFAULT, DEFAULT, 0, DEFAULT, NULL)
+END
+GO
