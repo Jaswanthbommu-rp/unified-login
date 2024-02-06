f1e73b7ce887dc19334fbdaf49db555a55929d54
Uma Mahesh Nallamelli <umamahesh.nallamelli@realpage.com>
2024-02-06 13:02:28 +0530

    Change in cache name

--- Enterprise/Subsystem/ProductLauncher/Component/Landing/Logic/Product/ManageProductResidentPortal.cs
+++ Enterprise/Subsystem/ProductLauncher/Component/Landing/Logic/Product/ManageProductResidentPortal.cs
@@ -2239,9 +2239,9 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Component.Landing.Logic.Produc
         private IList<ResidentPortalProperty> ListResidentPortalProperties()
         {
             List<ResidentPortalProperty> propertyProductList = new List<ResidentPortalProperty>();
-            if (_manageResidentPortalCache["ResidentPortalProperties"] != null)
+            if (_manageResidentPortalCache["ResidentPortalProperties" + _userClaims.OrganizationPartyId] != null)
             {
-                propertyProductList = _manageResidentPortalCache["ResidentPortalProperties"] as List<ResidentPortalProperty>;
+                propertyProductList = _manageResidentPortalCache["ResidentPortalProperties" + _userClaims.OrganizationPartyId] as List<ResidentPortalProperty>;
             }
             else
             {
@@ -2270,9 +2270,9 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Component.Landing.Logic.Produc
 
                 CacheItemPolicy policy = new CacheItemPolicy
                 {
-                    AbsoluteExpiration = DateTimeOffset.Now.AddMinutes(SIDREFRESHTIMEMINUTES)
+                    AbsoluteExpiration = DateTimeOffset.Now.AddMinutes(90)
                 };
-                _manageResidentPortalCache.Set("ResidentPortalProperties", propertyProductList, policy);
+                _manageResidentPortalCache.Set("ResidentPortalProperties" + _userClaims.OrganizationPartyId, propertyProductList, policy);
             }
             return propertyProductList;
         }
