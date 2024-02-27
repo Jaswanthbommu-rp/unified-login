e11ec8aa0a3279abf893f339afefabea585192e9
James Reames <james.reames@realpage.com>
2024-02-27 13:35:32 -0600

    Update ApiExceptionLogger.cs

--- Enterprise/Subsystem/ProductLauncher/Service/SharedObjects/Exceptions/ApiExceptionLogger.cs
+++ Enterprise/Subsystem/ProductLauncher/Service/SharedObjects/Exceptions/ApiExceptionLogger.cs
@@ -1,9 +1,12 @@
-﻿using Serilog.Events;
+﻿using System;
+using Serilog.Events;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Web.Http.ExceptionHandling;
 using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Reflection;
 
 namespace RP.Enterprise.Subsystem.ProductLauncher.Component.SharedObjects.Exceptions
 {
@@ -14,53 +17,37 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Component.SharedObjects.Except
     {
         public override void Log(ExceptionLoggerContext context)
         {
-
-            Dictionary<string, object> AdditionalInfo = new Dictionary<string, object>
+            var additionalInfo = new Dictionary<string, object>
             {
                 {"RequestURI", context.Request.RequestUri}, 
                 {"CatchBlockName", context.CatchBlock.Name}, 
-                {"Principal Name", context.RequestContext.Principal.Identity.Name}, 
                 {"AuthenticationType", context.RequestContext.Principal.Identity.AuthenticationType}
             };
 
             //TODO: Get Organization & other info
-
             string correlationId = "";
 
             var user = context.RequestContext.Principal as ClaimsPrincipal;
             if (user != null)
             {
-                var userNmClaim = user.Claims.FirstOrDefault(a => a.Type == "loginName");
-                //if (userNmClaim != null) logDetails.UserName = userNmClaim.Value;
-                //if (userNmClaim != null && !dict.ContainsKey("loginName"))
-                //    dict.Add("loginName", userNmClaim.Value);
+                var userNmClaim = user.Claims.FirstOrDefault(a => a.Type.Equals("loginName", StringComparison.OrdinalIgnoreCase));
+                if (userNmClaim != null) additionalInfo.Add("loginName", userNmClaim.Value);
 
-                var orgIdClaim = user.Claims.FirstOrDefault(a => a.Type == "orgPartyId");
-                //if (orgIdClaim != null) logDetails.PmcId = orgIdClaim.Value;
-                //if (orgIdClaim != null && !dict.ContainsKey("pmcId"))
-                //    dict.Add("pmcId", orgIdClaim.Value);
+                var personaClaim = user.Claims.FirstOrDefault(a => a.Type.Equals("personaId", StringComparison.OrdinalIgnoreCase));
+                if (personaClaim != null) additionalInfo.Add("personaId", personaClaim.Value);
 
-                var orgNameClaim = user.Claims.FirstOrDefault(a => a.Type == "orgName");
-                //if (orgNameClaim != null) logDetails.PmcName = orgNameClaim.Value;
-
-                var realPageClaim = user.Claims.FirstOrDefault(a => a.Type == "realPageId");
-                //if (realPageClaim != null && !dict.ContainsKey("realPageId")) dict.Add("realPageId", realPageClaim.Value);
-                
                 var correlationIdClaim = user.Claims.FirstOrDefault(a => a.Type == "correlationId");
                 if (correlationIdClaim != null)
                 {
                     correlationId = correlationIdClaim.ToString();
                 }
-
-                var userIdClaim = user.Claims.FirstOrDefault(a => a.Type == "sub");
-                //if (userIdClaim != null) logDetails.UserId = userIdClaim.Value;
             }
 
             var logger = Serilog.Log.Logger;
-			logger = logger.ForContext($"AdditionalInfo", JsonConvert.SerializeObject(AdditionalInfo, Formatting.Indented), true);
+			logger = logger.ForContext("AdditionalInfo", JsonConvert.SerializeObject(additionalInfo, Formatting.Indented), true);
 			logger = logger.ForContext("ProductModule", this.GetType());
             logger = logger.ForContext("CorrelationId", correlationId);
-            logger.Write(LogEventLevel.Error, context.Exception, context.Exception.Message);
+            logger.Write(LogEventLevel.Error, context.Exception, "{methodName} - {state}", propertyValues: new object[] { "ApiExceptionLogger", $"Error : {context.Exception.Message}" });
             // CorrelationId used as a key to search exception in the database
             //context.Exception.Data.Add("CorrelationId", logDetails.CorrelationId);
         }
