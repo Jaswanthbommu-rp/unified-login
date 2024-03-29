ae398086c331a9459a256e3185d19681a06704c2
Mallesham Sankati <mallesham.sankati@RealPage.com>
2024-03-29 02:18:26 -0500

    Merged PR 258284: User Story 1777580: Enterprise Role: Add Messaging Group and Notifications ta...
    User Story 1777580: Enterprise Role: Add Messaging Group and Notifications tab to Resident Portal in Enterprise Role
    
    Related work items: #1777580

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/RoleRightsManagement/Stored Procedures/GetRoleTemplateProductRolesMappings.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/RoleRightsManagement/Stored Procedures/GetRoleTemplateProductRolesMappings.sql
@@ -1,48 +1,50 @@
-CREATE PROCEDURE [Security].[GetRoleTemplateProductRolesMappings]
-(
-	@RoleTemplateId int, 
-	@OrganizationRealPageId UNIQUEIDENTIFIER = NULL,
-    @PartyId BIGINT = NULL
-)
-AS
-BEGIN
-WITH
-RoleTemplateUser   
- AS    
- (    
-  SELECT     
-   RT.RoleTemplateId as RoleTemplateId     
-   ,count(RTP.PersonaId) as UserCount    
-  FROM Security.RoleTemplate RT    
-  LEFT OUTER JOIN Security.RoleTemplateUserMapping RTP on RTP.RoleTemplateId = RT.roleTemplateId   
-  LEFT OUTER JOIN Person.Persona P ON P.PersonaId = RTP.PersonaId  
-  GROUP BY RT.RoleTemplateId    
- ) 
-
-SELECT 
-	rt.RoleTemplateId
-	,rt.PartyID
-	,rt.RoleTemplateName
-	,rt.RoleTemplateDescription
-	,ISNULL(rtp.RoleTemplateProductId,0) AS RoleTemplateProductId
-	,ISNULL(rtp.ProductId,0) as ProductId
-	,ISNULL(rprm.RoleTemplateProductRoleMappingId,0) AS RoleTemplateProductRoleMappingId
-	,ISNULL(rprm.ProductRoleId,0) as ProductRoleId
-	,rprm.ProductRoleName
-	,ISNULL(rtprm.RoleTemplateAdditionalProductRoleMappingId,0) AS RoleTemplateAdditionalProductRoleMappingId
-	,rtprm.AttributeName
-	,rtprm.AttributeValue
-	,RTUM.UserCount as Users
-	,EP.Name AS ProductName
-FROM Security.RoleTemplate rt
-	INNER JOIN Enterprise.Party P ON P.PartyId = rt.PartyID
-	LEFT OUTER  JOIN Security.RoleTemplateProduct rtp ON rt.RoleTemplateId = rtp.RoleTemplateId
-	LEFT OUTER JOIN Security.RoleTemplateProductRoleMapping rprm ON rprm.RoleTemplateProductId = rtp.RoleTemplateProductId
-	LEFT OUTER JOIN Security.RoleTemplateAdditionalProductRoleMapping rtprm ON rtprm.RoleTemplateProductId = rtp.RoleTemplateProductId
-	LEFT OUTER JOIN RoleTemplateUser RTUM ON RTUM.RoleTemplateId = RT.RoleTemplateId
-	INNER JOIN Enterprise.Product EP ON EP.ProductId = rtp.ProductId
-WHERE rt.RoleTemplateId = @RoleTemplateId 
-	AND (P.RealPageId = @OrganizationRealPageId OR @OrganizationRealPageId IS NULL)
-	AND (P.PartyId = @PartyId OR @PartyId IS NULL)
-ORDER BY rtp.ProductId
+CREATE PROCEDURE [Security].[GetRoleTemplateProductRolesMappings]    
+(    
+ @RoleTemplateId int,     
+ @OrganizationRealPageId UNIQUEIDENTIFIER = NULL,    
+    @PartyId BIGINT = NULL    
+)    
+AS    
+BEGIN    
+WITH    
+RoleTemplateUser       
+ AS        
+ (        
+  SELECT         
+   RT.RoleTemplateId as RoleTemplateId         
+   ,count(RTP.PersonaId) as UserCount        
+  FROM Security.RoleTemplate RT        
+  LEFT OUTER JOIN Security.RoleTemplateUserMapping RTP on RTP.RoleTemplateId = RT.roleTemplateId       
+  LEFT OUTER JOIN Person.Persona P ON P.PersonaId = RTP.PersonaId      
+  GROUP BY RT.RoleTemplateId        
+ )     
+    
+SELECT     
+ rt.RoleTemplateId    
+ ,rt.PartyID    
+ ,rt.RoleTemplateName    
+ ,rt.RoleTemplateDescription    
+ ,ISNULL(rtp.RoleTemplateProductId,0) AS RoleTemplateProductId    
+ ,ISNULL(rtp.ProductId,0) as ProductId    
+ ,ISNULL(rprm.RoleTemplateProductRoleMappingId,0) AS RoleTemplateProductRoleMappingId    
+ ,ISNULL(rprm.ProductRoleId,0) as ProductRoleId    
+ ,rprm.ProductRoleName    
+ ,ISNULL(rtprm.RoleTemplateAdditionalProductRoleMappingId,0) AS RoleTemplateAdditionalProductRoleMappingId    
+ ,rtprm.AttributeName    
+ ,rtprm.AttributeValue    
+ ,RTUM.UserCount as Users   
+ ,EP.Name AS ProductName  
+ ,RTAT.TabJson as RoleTemplateProductBatch
+FROM Security.RoleTemplate rt    
+ INNER JOIN Enterprise.Party P ON P.PartyId = rt.PartyID    
+ LEFT OUTER  JOIN Security.RoleTemplateProduct rtp ON rt.RoleTemplateId = rtp.RoleTemplateId    
+ LEFT OUTER JOIN Security.RoleTemplateProductRoleMapping rprm ON rprm.RoleTemplateProductId = rtp.RoleTemplateProductId    
+ LEFT OUTER JOIN Security.RoleTemplateAdditionalProductRoleMapping rtprm ON rtprm.RoleTemplateProductId = rtp.RoleTemplateProductId    
+ LEFT OUTER JOIN RoleTemplateUser RTUM ON RTUM.RoleTemplateId = RT.RoleTemplateId  
+ LEFT OUTER JOIN [Security].[RoleTemplateProductAdditionalTab] RTAT ON RTAT.RoleTemplateId = RT.RoleTemplateId
+ INNER JOIN Enterprise.Product EP ON EP.ProductId = rtp.ProductId  
+WHERE rt.RoleTemplateId = @RoleTemplateId     
+ AND (P.RealPageId = @OrganizationRealPageId OR @OrganizationRealPageId IS NULL)    
+ AND (P.PartyId = @PartyId OR @PartyId IS NULL)    
+ORDER BY rtp.ProductId    
 END
