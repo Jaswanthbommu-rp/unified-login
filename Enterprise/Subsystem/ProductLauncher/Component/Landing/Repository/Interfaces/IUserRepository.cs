18ebe31b5fe7e66ff4417c9d855c77786b45bd72
Kalyana Chakravarthy Bingi <kalyanachakravarthy.bingi@realpage.com>
2024-05-07 10:57:12 +0530

    removing white spaces

--- Enterprise/Subsystem/ProductLauncher/Component/Landing/Repository/Interfaces/IUserRepository.cs
+++ Enterprise/Subsystem/ProductLauncher/Component/Landing/Repository/Interfaces/IUserRepository.cs
@@ -10,116 +10,116 @@ using SO = RP.Enterprise.Subsystem.ProductLauncher.Component.SharedObjects;
 
 namespace RP.Enterprise.Subsystem.ProductLauncher.Component.Landing.Repository.Interfaces
 {
-	/// <summary>
-	/// User Repository
-	/// </summary>
-	public interface IUserRepository
-	{
-		/// <summary>
-		/// Get user by enterprise username
-		/// </summary>
-		/// <param name="enterpriseUserName">enterprise UserName</param>
-		/// <returns>User object</returns>
-		SO.User GetEnterpriseUser(string enterpriseUserName);
+    /// <summary>
+    /// User Repository
+    /// </summary>
+    public interface IUserRepository
+    {
+        /// <summary>
+        /// Get user by enterprise username
+        /// </summary>
+        /// <param name="enterpriseUserName">enterprise UserName</param>
+        /// <returns>User object</returns>
+        SO.User GetEnterpriseUser(string enterpriseUserName);
 
-		/// <summary>
-		/// Get user by enterprise user id
-		/// </summary>
-		/// <param name="enterpriseUserId">enterprise User Id</param>
-		/// <returns>User Object</returns>
-		SO.User GetEnterpriseUser(int enterpriseUserId);
+        /// <summary>
+        /// Get user by enterprise user id
+        /// </summary>
+        /// <param name="enterpriseUserId">enterprise User Id</param>
+        /// <returns>User Object</returns>
+        SO.User GetEnterpriseUser(int enterpriseUserId);
 
-		/// <summary>
-		/// Get Starter Profile Options
-		/// </summary>
-		/// <param name="enterpriseUserName">enterprise UserName</param>
-		/// <returns>StarterProfileOptionsResponse object</returns>
-		StarterProfileOptionsResponse GetStarterProfileOptions(string enterpriseUserName);
+        /// <summary>
+        /// Get Starter Profile Options
+        /// </summary>
+        /// <param name="enterpriseUserName">enterprise UserName</param>
+        /// <returns>StarterProfileOptionsResponse object</returns>
+        StarterProfileOptionsResponse GetStarterProfileOptions(string enterpriseUserName);
 
-		/// <summary>
-		/// Set Starter Profile Options
-		/// </summary>
-		/// <param name="starterProfileOptions">starterProfile Options object</param>
-		/// <returns>SetStarterProfile object</returns>
-		SetStarterProfile SetStarterProfileOptions(StarterProfile starterProfileOptions);
+        /// <summary>
+        /// Set Starter Profile Options
+        /// </summary>
+        /// <param name="starterProfileOptions">starterProfile Options object</param>
+        /// <returns>SetStarterProfile object</returns>
+        SetStarterProfile SetStarterProfileOptions(StarterProfile starterProfileOptions);
 
-		/// <summary>
-		/// Update user login
-		/// </summary>
-		/// <param name="realPageId">enterprise User Id</param>
-		/// <param name="organizationPartyId">organizationPartyId</param>
-		/// <param name="loginId">loginId</param>
-		/// <param name="isActive">isActive</param>
-		/// <param name="passwordHash">passwordHash</param>
-		/// <param name="passwordSalt">passwordSalt</param>
-		/// <param name="isLocked">isLocked</param>
-		/// <param name="isTainted">isTainted</param>
-		/// <param name="fromDate">fromDate</param>
-		/// <param name="thruDate">thruDate</param>
-		/// <returns>UserLogin object</returns>
-		UserLogin UpdateUserLogin(Guid realPageId, long organizationPartyId, string loginId = null, bool? isActive = null,
-			string passwordHash = null, string passwordSalt = null, bool? isLocked = null, bool? isTainted = null, DateTime? fromDate = null, DateTime? thruDate = null);
+        /// <summary>
+        /// Update user login
+        /// </summary>
+        /// <param name="realPageId">enterprise User Id</param>
+        /// <param name="organizationPartyId">organizationPartyId</param>
+        /// <param name="loginId">loginId</param>
+        /// <param name="isActive">isActive</param>
+        /// <param name="passwordHash">passwordHash</param>
+        /// <param name="passwordSalt">passwordSalt</param>
+        /// <param name="isLocked">isLocked</param>
+        /// <param name="isTainted">isTainted</param>
+        /// <param name="fromDate">fromDate</param>
+        /// <param name="thruDate">thruDate</param>
+        /// <returns>UserLogin object</returns>
+        UserLogin UpdateUserLogin(Guid realPageId, long organizationPartyId, string loginId = null, bool? isActive = null,
+            string passwordHash = null, string passwordSalt = null, bool? isLocked = null, bool? isTainted = null, DateTime? fromDate = null, DateTime? thruDate = null);
 
-		/// <summary>
-		/// Get enterprise user
-		/// </summary>
-		/// <param name="realPageId">enterprise User Id</param>
-		/// <returns>UserLogin object</returns>
-		UserLogin GetEnterpriseUser(Guid realPageId);
+        /// <summary>
+        /// Get enterprise user
+        /// </summary>
+        /// <param name="realPageId">enterprise User Id</param>
+        /// <returns>UserLogin object</returns>
+        UserLogin GetEnterpriseUser(Guid realPageId);
 
-		/// <summary>
-		/// Create User
-		/// </summary>
-		/// <param name="newProfile">New Profile object</param>
-		/// <param name="persona">Persona list</param>
-		/// <returns>CreateUserResponse object with a Error Status object</returns>
-		CreateUserResponse<IErrorData> CreateUser(ProfileDetail newProfile, IList<Persona> persona);
+        /// <summary>
+        /// Create User
+        /// </summary>
+        /// <param name="newProfile">New Profile object</param>
+        /// <param name="persona">Persona list</param>
+        /// <returns>CreateUserResponse object with a Error Status object</returns>
+        CreateUserResponse<IErrorData> CreateUser(ProfileDetail newProfile, IList<Persona> persona);
 
-		/// <summary>
-		/// Update new user profile
-		/// </summary>
-		/// <param name="userLogin">userLogin</param>
-		/// <param name="newProfile">newProfile object</param>
-		/// <param name="partyRoleTypeId">party RoleType Id</param>
-		/// <param name="companyJobTitle">company Job Title</param>
-		/// <param name="activityToken">activity Token</param>
-		/// <returns>RepositoryResponse object</returns>
-		RepositoryResponse UpdateNewUser(string userLogin, Profile newProfile, int partyRoleTypeId, string companyJobTitle, string activityToken);
+        /// <summary>
+        /// Update new user profile
+        /// </summary>
+        /// <param name="userLogin">userLogin</param>
+        /// <param name="newProfile">newProfile object</param>
+        /// <param name="partyRoleTypeId">party RoleType Id</param>
+        /// <param name="companyJobTitle">company Job Title</param>
+        /// <param name="activityToken">activity Token</param>
+        /// <returns>RepositoryResponse object</returns>
+        RepositoryResponse UpdateNewUser(string userLogin, Profile newProfile, int partyRoleTypeId, string companyJobTitle, string activityToken);
 
-		/// <summary>
-		/// Update User Detail and Products
-		/// </summary>
-		/// <param name="loggedInUserRealPageId">Logged-In User unique identifier</param>
-		/// <param name="profile">Edited User detail and Products</param>
-		/// <param name="oldProfile">Old detail profile from database</param>
-		/// <returns>Repository response object</returns>
-		RepositoryResponse UpdateUser(Guid loggedInUserRealPageId, IProfileDetail profile, IProfileDetail oldProfile);
+        /// <summary>
+        /// Update User Detail and Products
+        /// </summary>
+        /// <param name="loggedInUserRealPageId">Logged-In User unique identifier</param>
+        /// <param name="profile">Edited User detail and Products</param>
+        /// <param name="oldProfile">Old detail profile from database</param>
+        /// <returns>Repository response object</returns>
+        RepositoryResponse UpdateUser(Guid loggedInUserRealPageId, IProfileDetail profile, IProfileDetail oldProfile);
 
-		/// <summary>
-		/// Used to disable a list of users across all products the users have access to
-		/// </summary>
-		/// <param name="createUserRealPageId"></param>
-		/// <param name="createUserPersonaId"></param>
-		/// <param name="userLogins"></param>
-		void DisableUserProduct(Guid createUserRealPageId, long createUserPersonaId, IList<UserLoginOnly> userLogins);
+        /// <summary>
+        /// Used to disable a list of users across all products the users have access to
+        /// </summary>
+        /// <param name="createUserRealPageId"></param>
+        /// <param name="createUserPersonaId"></param>
+        /// <param name="userLogins"></param>
+        void DisableUserProduct(Guid createUserRealPageId, long createUserPersonaId, IList<UserLoginOnly> userLogins);
 
-		/// <summary>
-		/// Used to activate products for the given list of users whom previously disabled
-		/// </summary>
-		/// <param name="createUserRealPageId"></param>
-		/// <param name="createUserPersonaId"></param>
-		/// <param name="userLogins"></param>
-		void ActivateUserProducts(Guid createUserRealPageId, long createUserPersonaId, IList<UserLoginOnly> userLogins);
-		
-		/// <summary>
-		/// Get User details by Persona Id or user realpage id
-		/// </summary>
-		UserDetails GetUserDetails(long? personaId = null, string userRealPageId = null);
+        /// <summary>
+        /// Used to activate products for the given list of users whom previously disabled
+        /// </summary>
+        /// <param name="createUserRealPageId"></param>
+        /// <param name="createUserPersonaId"></param>
+        /// <param name="userLogins"></param>
+        void ActivateUserProducts(Guid createUserRealPageId, long createUserPersonaId, IList<UserLoginOnly> userLogins);
 
-		/// <summary>
-		/// Set UnifiedLoginUser in Salesforce
-		/// </summary>
-		void ActivateSalesForceUser(Guid createUserRealPageId, long createUserPersonaId, IList<UserLoginOnly> userLogins,  bool isAssigned);
+        /// <summary>
+        /// Get User details by Persona Id or user realpage id
+        /// </summary>
+        UserDetails GetUserDetails(long? personaId = null, string userRealPageId = null);
+
+        /// <summary>
+        /// Set UnifiedLoginUser in Salesforce
+        /// </summary>
+        void ActivateSalesForceUser(Guid createUserRealPageId, long createUserPersonaId, IList<UserLoginOnly> userLogins, bool isAssigned);
 
         /// <summary>
         /// Used to disable products for the given list of users which is called from windows service
@@ -145,26 +145,26 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Component.Landing.Repository.I
         /// <param name="userTypeId"></param>
         void ProcessDisableUserProductData(IRepository repository, long assignUserPersonaId, Guid createUserRealPageId, long createUserPersonaId, int? userTypeId, long impersonatorUserId);
 
-		/// <summary>
-		/// Get the an UserEmployee by UserLoginPersonaId and OrganizationPartyId
-		/// </summary>
-		/// <param name="UserLoginPersonaId"></param>
-		/// <param name="OrganizationPartyId"></param>
-		/// <returns>IUserEmployeeId</returns>
-		IUserEmployeeId GetUserEmployeeId(long UserLoginPersonaId, long OrganizationPartyId);
+        /// <summary>
+        /// Get the an UserEmployee by UserLoginPersonaId and OrganizationPartyId
+        /// </summary>
+        /// <param name="UserLoginPersonaId"></param>
+        /// <param name="OrganizationPartyId"></param>
+        /// <returns>IUserEmployeeId</returns>
+        IUserEmployeeId GetUserEmployeeId(long UserLoginPersonaId, long OrganizationPartyId);
 
-		/// <summary>
-		/// Update user Employee Id
-		/// </summary>
-		/// <param name="employeeIdDetail"></param>
-		/// <returns></returns>
-		RepositoryResponse UpdateUserEmployeeId(IUserEmployeeId employeeIdDetail);
+        /// <summary>
+        /// Update user Employee Id
+        /// </summary>
+        /// <param name="employeeIdDetail"></param>
+        /// <returns></returns>
+        RepositoryResponse UpdateUserEmployeeId(IUserEmployeeId employeeIdDetail);
 
-		IList<NavigationMenuEntry> GetNavigationMenu();
+        IList<NavigationMenuEntry> GetNavigationMenu();
 
-		IList<NavigationMenuRightEntry> GetNavigationMenuRights();
+        IList<NavigationMenuRightEntry> GetNavigationMenuRights();
 
-		IList<NavigationMenuSetting> GetNavigationMenuSettingsUnaccessable(long partyId);
+        IList<NavigationMenuSetting> GetNavigationMenuSettingsUnaccessable(long partyId);
 
         AdUserDetail GetAzureUserDetails(long userId);
 
@@ -172,7 +172,7 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Component.Landing.Repository.I
 
         RepositoryResponse AddUpdateEmployeeProductADGroupMapping(long personaId, int productId, int adGroupId);
 
-		ExternalUserRelationship GetExternalUserRelationship(long userLoginPersonaId);
+        ExternalUserRelationship GetExternalUserRelationship(long userLoginPersonaId);
 
         /// <summary>
         /// Insert or update User Status by company
@@ -190,17 +190,17 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Component.Landing.Repository.I
         /// </summary>
         ActivityAttempt UpdateUserActivityAttempts(string enterpriseUserName, ActivityType activityType, UserDeviceDetails userDeviceDetails, long organizationPartyId, string authenticationServiceId = "");
 
-		/// <summary>
-		/// DelegateAdmin Role Template
-		/// </summary>
-		/// <param name="UserLoginPersonaId"></param>
-		/// <returns></returns>
-		List<int> GetDelegateAdminRoleTemplate(long UserLoginPersonaId);
-		/// <summary>
-		/// Get Settings Data
-		/// </summary>
-		/// <param name="settingName"></param>
-		/// <returns></returns>
+        /// <summary>
+        /// DelegateAdmin Role Template
+        /// </summary>
+        /// <param name="UserLoginPersonaId"></param>
+        /// <returns></returns>
+        List<int> GetDelegateAdminRoleTemplate(long UserLoginPersonaId);
+        /// <summary>
+        /// Get Settings Data
+        /// </summary>
+        /// <param name="settingName"></param>
+        /// <returns></returns>
         bool GetUnifiedSettingData(string settingName);
     }
 }
