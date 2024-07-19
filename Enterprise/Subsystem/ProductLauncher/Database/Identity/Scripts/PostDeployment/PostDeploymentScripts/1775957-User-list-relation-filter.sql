5f4ea802f25a9b3f3a489617410595fa5ac010c3
Jaswanth Bommu <Jaswanth.Bommu@RealPage.com>
2024-07-19 04:11:44 -0500

    Merged PR 279989: User Story 1934885: Display No Email options at the bottom of the user type l...
    User Story 1934885: Display No Email options at the bottom of the user type list instead of the top
    
    Related work items: #1934885

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/1775957-User-list-relation-filter.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/1775957-User-list-relation-filter.sql
@@ -2,3 +2,9 @@
 SET EUR.ThirdPartyRelationshipId = ETR.ThirdPartyRelationshipId
 FROM Enterprise.ThirdPartyRelationship ETR
 INNER JOIN Enterprise.UserRelationShip EUR ON EUR.UserRelationshipName = ETR.ThirdPartyRelationship
+
+UPDATE Enterprise.RoleTypeDependency SET SortOrder =10 WHERE ChildRoleTypeId = 403
+UPDATE Enterprise.RoleTypeDependency SET SortOrder =20 WHERE ChildRoleTypeId = 401
+UPDATE Enterprise.RoleTypeDependency SET SortOrder =30 WHERE ChildRoleTypeId = 402
+UPDATE Enterprise.RoleTypeDependency SET SortOrder =40 WHERE ChildRoleTypeId = 405
+UPDATE Enterprise.RoleTypeDependency SET SortOrder =50 WHERE ChildRoleTypeId = 404
