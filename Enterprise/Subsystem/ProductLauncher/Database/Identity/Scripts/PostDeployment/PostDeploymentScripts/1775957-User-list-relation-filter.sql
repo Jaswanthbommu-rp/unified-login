f406657ce76086cef4b9b011eaa3e28c21a44cc9
Jaswanth Bommu <Jaswanth.Bommu@RealPage.com>
2024-07-19 09:33:00 -0500

    Merged PR 280250: User Story 1934885: Display No Email options at the bottom of the user type list instead of the top
    User Story 1934885: Display No Email options at the bottom of the user type list instead of the top
    
    Related work items: #1934885

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/1775957-User-list-relation-filter.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/1775957-User-list-relation-filter.sql
@@ -8,3 +8,13 @@ UPDATE Enterprise.RoleTypeDependency SET SortOrder =20 WHERE ChildRoleTypeId = 4
 UPDATE Enterprise.RoleTypeDependency SET SortOrder =30 WHERE ChildRoleTypeId = 402
 UPDATE Enterprise.RoleTypeDependency SET SortOrder =40 WHERE ChildRoleTypeId = 405
 UPDATE Enterprise.RoleTypeDependency SET SortOrder =50 WHERE ChildRoleTypeId = 404
+
+
+
+DECLARE @Id1 INT, @SortIndex1 INT, @Id2 INT, @SortIndex2 INT
+
+SELECT @Id1 = Id, @SortIndex1 = SortIndex  FROM [Enterprise].[UserRelationShip] WHERE UserRelationshipName = 'Third Party Vendor (External User)'
+SELECT @Id2 = Id, @SortIndex2 = SortIndex  FROM [Enterprise].[UserRelationShip] WHERE UserRelationshipName = 'Other (external user)'
+
+UPDATE Enterprise.UserRelationShip SET SortIndex = @SortIndex2 WHERE Id=@Id1
+UPDATE Enterprise.UserRelationShip SET SortIndex = @SortIndex1 WHERE Id=@Id2
