6a56fbfdc857be7d3c2e61e1c9a7dc48caeb096e
James Reames <James.Reames@RealPage.com>
2024-08-06 01:27:42 -0500

    Merged PR 283503: Merged PR 277374: adding new entra consent
    Merged PR 277374: adding new entra consent
    
    adding new entra consent and index for adgroup user table
    
    Related work items: #1795901
    
    Cherry-picked from commit `f7ff743d`.
    
    Related work items: #1795901

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Ident/Tables/IdentityProviderSetting.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Ident/Tables/IdentityProviderSetting.sql
@@ -6,3 +6,8 @@
     CONSTRAINT [PK_IdentityProviderSetting] PRIMARY KEY ([IdentityProviderSettingId]), 
     CONSTRAINT [FK_IdentityProviderSetting_IdentityProviderSettingType] FOREIGN KEY ([IdentityProviderSettingTypeId]) REFERENCES Ident.[IdentityProviderSettingType]([IdentityProviderSettingTypeId]) ON DELETE CASCADE ON UPDATE CASCADE
 )
+GO
+
+CREATE NONCLUSTERED INDEX [IDX_IdentityProviderSetting_IdentityProviderSettingTypeId]
+ON [Ident].[IdentityProviderSetting] ([IdentityProviderSettingTypeId])
+INCLUDE ([Value])
--- Enterprise/Subsystem/ProductLauncher/Web/Landing/Controllers/AuthController.cs
+++ Enterprise/Subsystem/ProductLauncher/Web/Landing/Controllers/AuthController.cs
@@ -1,15 +1,12 @@
 ﻿using RP.Enterprise.Subsystem.ProductLauncher.Component.SharedObjects.Helper;
-using System;
-using System.Collections.Generic;
 using System.Web.Mvc;
-using Thinktecture.IdentityModel.Client;
 
 namespace RP.Enterprise.Subsystem.ProductLauncher.Web.Landing.Controllers
 {
-	/// <summary>
-	/// Used to initiate a login from a third party identity provider that does not require a custom client context
-	/// </summary>
-	public class AuthController : BaseController
+    /// <summary>
+    /// Used to initiate a login from a third party identity provider that does not require a custom client context
+    /// </summary>
+    public class AuthController : BaseController
     {
 		[AllowAnonymous]
 	    public ActionResult AzureConsent(string error)
@@ -32,13 +29,33 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Web.Landing.Controllers
             return View();
 	    }
 
-		/// <summary>
-		/// Used to initiate a login from an Azure user
-		/// </summary>
-		/// <returns></returns>
-		public ActionResult Azure()
-		{
-			var returnUrl = BuildIDPRequest("aad", true);
+        [AllowAnonymous]
+        public ActionResult EntraConsent(string error)
+        {
+            if (string.IsNullOrEmpty(error))
+            {
+                ViewBag.StatusMessage = "You've successfully connected RealPage to Entra!";
+                ViewBag.AdditionalStatusInfo = "You can close this tab now and return to your work.";
+                ViewBag.ErrorStatus = false;
+            }
+            else
+            {
+                if (error == "access_denied")
+                {
+                    ViewBag.StatusMessage = "There was an error connecting to Entra.";
+                    ViewBag.AdditionalStatusInfo = "Check your Entra permissions and try again.";
+                }
+                ViewBag.ErrorStatus = true;
+            }
+            return View();
+        }
+        /// <summary>
+        /// Used to initiate a login from an Azure user
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Azure()
+        {
+            var returnUrl = ConfigReader.GetRedirectUri.TrimEnd('/') + "?auth=azure";
 
             Response.Redirect(returnUrl, false);
 			return View();
@@ -51,9 +68,9 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Web.Landing.Controllers
 	    /// <returns></returns>
 	    public ActionResult AzureDirect()
 	    {
-		    var returnUrl = BuildIDPRequest("aad", false);
+            var returnUrl = ConfigReader.GetRedirectUri.TrimEnd('/') + "?auth=azuredirect";
 
-		    Response.Redirect(returnUrl, false);
+            Response.Redirect(returnUrl, false);
 		    return View();
 	    }
 
@@ -63,9 +80,9 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Web.Landing.Controllers
 		/// <returns></returns>
 		public ActionResult Google()
 	    {
-			var returnUrl = BuildIDPRequest("oidcgoogle", true);
+            var returnUrl = ConfigReader.GetRedirectUri.TrimEnd('/') + "?auth=google";
 
-		    Response.Redirect(returnUrl, false);
+            Response.Redirect(returnUrl, false);
 		    return View();
 	    }
 
@@ -75,36 +92,10 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Web.Landing.Controllers
 	    /// <returns></returns>
 	    public ActionResult GoogleDirect()
 	    {
-		    var returnUrl = BuildIDPRequest("oidcgoogle", false);
+            var returnUrl = ConfigReader.GetRedirectUri.TrimEnd('/') + "?auth=googledirect";
 
-		    Response.Redirect(returnUrl);
+            Response.Redirect(returnUrl);
 		    return View();
-	    }
-
-		/// <summary>
-		/// Build the request to the third party idp
-		/// </summary>
-		/// <param name="idp"></param>
-		/// <param name="forceLogin">request a forced user login if needed</param>
-		/// <returns></returns>
-		private static string BuildIDPRequest(string idp, bool forceLogin)
-	    {
-		    var state = Guid.NewGuid().ToString("N");
-		    var nonce = Guid.NewGuid().ToString("N");
-		    var client = new OAuth2Client(new Uri($"{ConfigReader.GetIssuerUri}/connect/authorize"));
-		    string acrAdditional = "";
-		    Dictionary<string, string> additional = null;
-			if (forceLogin)
-		    {
-			    acrAdditional = " prompt:login";
-			    additional = new Dictionary<string, string>();
-			    additional.Add("prompt", "login");
-			}
-
-		    // NOTE! the IDP value is currently case sensitive!
-		    var returnUrl = client.CreateAuthorizeUrl(ConfigReader.GetLandingClientId, "id_token token", ConfigReader.GetLandingScopes,
-			                    ConfigReader.GetRedirectUri, state, nonce, acrValues: $"idp:{idp}"+ acrAdditional, responseMode: "form_post", additionalValues: additional);
-			return returnUrl;
 	    }
 	}
 }
