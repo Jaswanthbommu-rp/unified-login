1726eca269c05e672e67bd3df77631a78129ffc9
Jaswanth Bommu <Jaswanth.Bommu@RealPage.com>
2024-09-16 07:06:12 -0500

    Merged PR 291155: User Story 2009478: RealConnect - Fix for the Issue when Updating Email Address
    User Story 2009478: RealConnect - Fix for the Issue when Updating Email Address
    
    Related work items: #2009478

--- Enterprise/Subsystem/ProductLauncher/Service/SharedObjects/Enum/SamlAttributeEnum.cs
+++ Enterprise/Subsystem/ProductLauncher/Service/SharedObjects/Enum/SamlAttributeEnum.cs
@@ -1,87 +1,92 @@
 ﻿namespace RP.Enterprise.Subsystem.ProductLauncher.Component.SharedObjects.Enum
 {
-	/// <summary>
-	/// Saml Attribute Enum
-	/// </summary>
-	public enum SamlAttributeEnum : int
-	{
-		/// <summary>
-		/// productUsername
-		/// </summary>
-		productUsername = 1,
+    /// <summary>
+    /// Saml Attribute Enum
+    /// </summary>
+    public enum SamlAttributeEnum : int
+    {
+        /// <summary>
+        /// productUsername
+        /// </summary>
+        productUsername = 1,
 
-		/// <summary>
-		/// eidPersonLogin
-		/// </summary>
-		eidPersonLogin = 2,
+        /// <summary>
+        /// eidPersonLogin
+        /// </summary>
+        eidPersonLogin = 2,
 
-		/// <summary>
-		/// Property Management Company Id
-		/// </summary>
-		PMCID = 3,
+        /// <summary>
+        /// Property Management Company Id
+        /// </summary>
+        PMCID = 3,
 
-		/// <summary>
-		/// UserId
-		/// </summary>
-		UserId = 4,
+        /// <summary>
+        /// UserId
+        /// </summary>
+        UserId = 4,
 
-		/// <summary>
-		/// RealPageID Products
-		/// </summary>
-		RealPageIDProducts = 5,
+        /// <summary>
+        /// RealPageID Products
+        /// </summary>
+        RealPageIDProducts = 5,
 
-		/// <summary>
-		/// RealPageID Products Raw
-		/// </summary>
-		RealPageIDProductsRaw = 6,
+        /// <summary>
+        /// RealPageID Products Raw
+        /// </summary>
+        RealPageIDProductsRaw = 6,
 
-		/// <summary>
-		/// User FirstName
-		/// </summary>
-		User_FirstName = 7,
+        /// <summary>
+        /// User FirstName
+        /// </summary>
+        User_FirstName = 7,
 
-		/// <summary>
-		/// User Username
-		/// </summary>
-		User_Username = 8,
+        /// <summary>
+        /// User Username
+        /// </summary>
+        User_Username = 8,
 
-		/// <summary>
-		/// User email
-		/// </summary>
-		User_email = 9,
+        /// <summary>
+        /// User email
+        /// </summary>
+        User_email = 9,
 
-		/// <summary>
-		/// portal id
-		/// </summary>
-		portal_id = 10,
+        /// <summary>
+        /// portal id
+        /// </summary>
+        portal_id = 10,
 
-		/// <summary>
-		/// organization id
-		/// </summary>
-		organization_id = 11,
+        /// <summary>
+        /// organization id
+        /// </summary>
+        organization_id = 11,
 
-		/// <summary>
-		/// Enterprise Login
-		/// </summary>
-		EnterpriseLogin = 12,
-		/// <summary>
-		/// NWP User Type
-		/// </summary>
-		NWPUserType = 13,
+        /// <summary>
+        /// Enterprise Login
+        /// </summary>
+        EnterpriseLogin = 12,
+        /// <summary>
+        /// NWP User Type
+        /// </summary>
+        NWPUserType = 13,
 
-		/// <summary>
-		/// Integration marketPlace role code
-		/// </summary>
-		RoleCode = 15,
+        /// <summary>
+        /// Integration marketPlace role code
+        /// </summary>
+        RoleCode = 15,
 
-		/// <summary>
-		/// For Real connect product
-		/// </summary>
+        /// <summary>
+        /// For Real connect product
+        /// </summary>
         LearnerId = 16,
 
         /// <summary>
         /// For Real connect product
         /// </summary>
-        ManagerId = 17
+        ManagerId = 17,
+
+        /// <summary>
+        /// For Real connect product
+        /// </summary>
+        DualRole = 18
     }
 }
