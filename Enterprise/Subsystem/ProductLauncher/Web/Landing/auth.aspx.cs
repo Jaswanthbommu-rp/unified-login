bbf79c6bdbc5a7c30a6e26ecb5c7b8aad8e8e350
Kalyana Chakravarthy Bingi <kalyanachakravarthy.bingi@realpage.com>
2024-09-20 15:48:08 +0530

    2027718 : fortify fixes

--- Enterprise/Subsystem/ProductLauncher/Web/Landing/auth.aspx.cs
+++ Enterprise/Subsystem/ProductLauncher/Web/Landing/auth.aspx.cs
@@ -8,21 +8,27 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Web.Landing
 {
 	public partial class auth : System.Web.UI.Page
     {
-		
+
         protected void Page_Load(object sender, EventArgs e)
         {
-			Response.Cache.SetCacheability(HttpCacheability.NoCache);
-	        Response.AppendHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
-	        Response.AppendHeader("Pragma", "no-cache"); // HTTP 1.0.
-	        Response.AppendHeader("Expires", "0"); // Proxies.
+            Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            Response.AppendHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
+            Response.AppendHeader("Pragma", "no-cache"); // HTTP 1.0.
+            Response.AppendHeader("Expires", "0"); // Proxies.
 
-			var idp = Request.QueryString["idp"];
-            if (string.IsNullOrEmpty(idp))
-                throw new Exception("No idp included in redirect querystring!!");
+            var strURLArray = new string[] { };
+            String redirect = Request.QueryString["idp"];
+            Int32 strDest = System.Convert.ToInt32(redirect);
+            if ((strDest >= 0) && (strDest <= strURLArray.Length - 1))
+            {
+                var idp = strURLArray[strDest];
+                if (string.IsNullOrEmpty(idp))
+                    throw new Exception("No idp included in redirect querystring!!");
 
-            var returnUrl = ConfigReader.GetRedirectUri.TrimEnd('/') + "?auth=" + idp;
-            Response.Redirect(returnUrl, false);
+                var returnUrl = ConfigReader.GetRedirectUri.TrimEnd('/') + "?auth=" + idp;
+                Response.Redirect(returnUrl, false);
 
+            }
         }
     }
 }
