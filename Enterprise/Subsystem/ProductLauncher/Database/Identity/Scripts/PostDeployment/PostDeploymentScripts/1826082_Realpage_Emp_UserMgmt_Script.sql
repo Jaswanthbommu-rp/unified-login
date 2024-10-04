89134ecf9318aa85928278ca7c90d73f461c4597
KalyanaChakravarthy Bingi <kalyanachakravarthy.bingi@RealPage.com>
2024-10-04 09:44:40 -0500

    Merged PR 295306: 1826082 : Realpage Employee User Management
    1826082 : Realpage Employee User Management
    
    Cherry picked from !289734
    
    Related work items: #1826082

---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/1826082_Realpage_Emp_UserMgmt_Script.sql
@@ -0,0 +1,41 @@
+﻿Go
+Declare @createdate datetime, @CreatedBy bigint, @rightid bigint, @orgPartyId bigint
+
+select @createdate = GETUTCDATE()
+SELECT	@CreatedBy = UserId
+FROM	Ident.UserLogin
+WHERE	LoginName LIKE 'realpagead@%'
+
+select @orgPartyId = PartyId from Enterprise.Organization where Name = 'Realpage Employee'
+
+IF NOT EXISTS (SELECT TOP 1 1 FROM Security.[Right] Where RightName = 'RealPageEmployeeUserManagementViewOnly')
+BEGIN
+ INSERT INTO Security.[Right] (RightName,[Description],[Value],StatusTypeId,VisibilityStatusId,ProductId,TargetProductId,CreatedBy,CreatedDate,PersistRight,IsExcludeRightFromImpersonation)
+ VALUES('RealPageEmployeeUserManagementViewOnly','RealPage Employee Company User Management: View Only','RealPage Employee Company User Management: View Only',13,9,3,3,@CreatedBy,@createdate,0,0)
+END
+
+select @rightid = RightId FROM Security.[Right] Where RightName = 'RealPageEmployeeUserManagementViewOnly' 
+
+IF NOT EXISTS (SELECT TOP 1 1 FROM Enterprise.NavigationMenuRights where RightId = @rightid)
+BEGIN
+ INSERT INTO Enterprise.NavigationMenuRights (NavigationMenuId, RightId)
+ VALUES(2,@rightid),
+ (3,@rightid)
+END
+
+IF NOT EXISTS (SELECT TOP 1 1 FROM Security.RightRoute where RightId = @rightid)
+BEGIN
+ INSERT INTO Security.RightRoute(RightId, RouteId,CreatedBy,CreatedDate)
+ VALUES(@rightid, 2, @CreatedBy, @createdate),
+ (@rightid, 5, @CreatedBy, @createdate),
+ (@rightid, 9, @CreatedBy, @createdate),
+ (@rightid, 12, @CreatedBy, @createdate)
+END
+
+IF NOT EXISTS (SELECT TOP 1 1 FROM Security.OrganizationOverRideRight where RightId = @rightid)
+BEGIN
+  INSERT INTO Security.OrganizationOverRideRight(RightId,OrgPartyId,VisibilityStatusId,CreatedBy,CreatedDate)
+  VALUES(@rightid, @orgPartyId,9,@CreatedBy,@createdate)
+END
+
+Go
