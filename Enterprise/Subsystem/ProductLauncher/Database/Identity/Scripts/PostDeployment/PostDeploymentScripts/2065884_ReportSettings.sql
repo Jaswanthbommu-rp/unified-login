db987ef7c7bb0788c097dec287691119805b391a
Mallesham Sankati <mallesham.sankati@realpage.com>
2024-10-17 15:08:02 +0530

    User Story 2065884: Create a new product setting called "ShowInPrimaryPropertiesReport" and "CacheInMinutesPrimaryPropertyReportData"

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/2065884_ReportSettings.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/2065884_ReportSettings.sql
@@ -2,7 +2,7 @@
 IF NOT EXISTS (SELECT TOP (1) 1 FROM Enterprise.ProductSettingType WHERE [Name] = 'ShowInPrimaryPropertiesReport')
 BEGIN
 	INSERT INTO Enterprise.ProductSettingType ([Name], [Description], SensitiveData) 
-	VALUES('ShowInPrimaryPropertiesReport', 'Based on this setting value only items will be populated in primary property audit report dropdown list', 0)
+	VALUES('ShowInPrimaryPropertiesReport', 'Based on this setting value only items will be populated in audit report dropdown list', 0)
 END
 IF NOT EXISTS (SELECT TOP (1) 1 FROM Enterprise.ProductSettingType WHERE [Name] = 'CacheInMinutesPrimaryPropertyReportData')
 BEGIN
