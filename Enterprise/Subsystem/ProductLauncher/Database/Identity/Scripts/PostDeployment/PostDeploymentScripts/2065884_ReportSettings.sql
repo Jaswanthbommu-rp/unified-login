e999901537fd7b9c1cd605daa1f682f31de76642
Mallesham Sankati <mallesham.sankati@RealPage.com>
2024-10-17 04:44:30 -0500

    Merged PR 297732: User Story 2065884: Create a new product setting called "ShowInPrimaryPropert...
    User Story 2065884: Create a new product setting called "ShowInPrimaryPropertiesReport" and "CacheInMinutesPrimaryPropertyReportData"
    
    Related work items: #2065884

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/2065884_ReportSettings.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/2065884_ReportSettings.sql
@@ -2,12 +2,12 @@
 IF NOT EXISTS (SELECT TOP (1) 1 FROM Enterprise.ProductSettingType WHERE [Name] = 'ShowInPrimaryPropertiesReport')
 BEGIN
 	INSERT INTO Enterprise.ProductSettingType ([Name], [Description], SensitiveData) 
-	VALUES('ShowInPrimaryPropertiesReport', 'Based on this setting value only items will be populated in primary property audit report dropdown list', 0)
+	VALUES('ShowInPrimaryPropertiesReport', 'Based on this setting value only items will be populated in audit report dropdown list', 0)
 END
 IF NOT EXISTS (SELECT TOP (1) 1 FROM Enterprise.ProductSettingType WHERE [Name] = 'CacheInMinutesPrimaryPropertyReportData')
 BEGIN
 	INSERT INTO Enterprise.ProductSettingType ([Name], [Description], SensitiveData) 
-	VALUES('CacheInMinutesPrimaryPropertyReportData', 'Caching the data from UDM translated primary property API call for specified time in minutes', 0)
+	VALUES('CacheInMinutesPrimaryPropertyReportData', 'Caching the data from UDM property API call for specified time in minutes', 0)
 END
 GO
 
