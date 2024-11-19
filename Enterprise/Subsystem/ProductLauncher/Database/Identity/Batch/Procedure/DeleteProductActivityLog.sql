bd1bff4e26e8d3064cdf51625dd62cfbdb70222d
Kalyana Chakravarthy Bingi <kalyanachakravarthy.bingi@realpage.com>
2024-11-20 01:03:33 +0530

    1649387 : API Activity Logs standard UPFM

---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Batch/Procedure/DeleteProductActivityLog.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [Batch].[DeleteProductActivityLog](  
+@BatchProcessorGroupId bigint)  
+AS  
+BEGIN  
+ DELETE from batch.ProductActivityLog  
+ where BatchProcessorGroupId = @BatchProcessorGroupId  
+END
