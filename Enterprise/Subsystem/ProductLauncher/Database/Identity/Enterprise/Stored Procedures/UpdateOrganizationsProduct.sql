d43083d8d2a67c674d02b3367a01a0e193b794a7
Shiva Kodaparthi <shiva.kodaparthi@Realpage.com>
2025-01-27 22:19:32 +0530

    removing multiple nested select statments

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Enterprise/Stored Procedures/UpdateOrganizationsProduct.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Enterprise/Stored Procedures/UpdateOrganizationsProduct.sql
@@ -7,10 +7,17 @@ BEGIN
   CREATE TABLE #OrganizationsType(            
   OrganizationsTypeIds NVARCHAR(150)
  )  
+
+ CREATE TABLE #OrgPartyIds(            
+  PartyIds NVARCHAR(150)
+ )  
+
  INSERT INTO #OrganizationsType(OrganizationsTypeIds) SELECT OrganizationTypeId FROM Enterprise.OrganizationType WHERE Name IN (SELECT value FROM string_split(@Value,','))
+ 
+ INSERT INTO #OrgPartyIds(PartyIds) SELECT PartyId FROM Enterprise.Organization WHERE OrganizationTypeId IN (SELECT * FROM #OrganizationsType)
 
  IF EXISTS(SELECT 1 FROM #OrganizationsType)  
-  UPDATE Enterprise.OrganizationProduct SET ThruDate = GETUTCDATE() WHERE ProductId = @ProductId and partyid NOT IN (SELECT PartyId FROM Enterprise.Organization WHERE OrganizationTypeId IN (SELECT * FROM #OrganizationsType));  
+  UPDATE Enterprise.OrganizationProduct SET ThruDate = GETUTCDATE() WHERE ProductId = @ProductId and partyid NOT IN (SELECT * FROM #OrgPartyIds); 
     
   DROP TABLE if exists #OrganizationsType
 
