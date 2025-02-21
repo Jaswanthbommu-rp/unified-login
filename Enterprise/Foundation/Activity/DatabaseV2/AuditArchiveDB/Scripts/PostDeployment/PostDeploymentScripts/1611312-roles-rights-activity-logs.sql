ef2cc5aa2d2213c480fbcf81ab4a7d7656b6a294
Shiva Kodaparthi <shiva.kodaparthi@Realpage.com>
2025-02-21 05:13:10 -0600

    Merged PR 320902: User Story 1611312: API - Activity Log details page(DEV)
    changes for 1611312
    
    Related work items: #1611312

--- Enterprise/Foundation/Activity/DatabaseV2/AuditArchiveDB/Scripts/PostDeployment/PostDeploymentScripts/1611312-roles-rights-activity-logs.sql
+++ Enterprise/Foundation/Activity/DatabaseV2/AuditArchiveDB/Scripts/PostDeployment/PostDeploymentScripts/1611312-roles-rights-activity-logs.sql
@@ -1,14 +1,13 @@
+ 
 IF NOT EXISTS (SELECT TOP 1 1 FROM Logging.LogCategoryType WHERE Name = 'RolesRights')
 BEGIN
 	Declare @catId int;
- 
 	SELECT @catId = MAX(LogCategoryTypeId) + 1
 	FROM Logging.LogCategoryType
- 
 	Insert into Logging.LogCategoryType (LogCategoryTypeId, Name, Description)
-	values(@catId, 'InternalProductSettings','Internal Product Settings.')
+	values(@catId, 'RolesRights','Roles and Rights.')
 END
-
+ 
 IF NOT EXISTS
 (
     SELECT 1
@@ -16,16 +15,16 @@ IF NOT EXISTS
     WHERE  Name = 'Roles and Rights'
 )
 BEGIN
-    DECLARE @catId INT;
+    DECLARE @cateId INT;
     DECLARE @logId INT;
-	SELECT @catId = LogCategoryTypeId
+	SELECT @cateId = LogCategoryTypeId
 	FROM logging.LogCategoryType 
 	WHERE NAME = 'RolesRights'
 	SELECT @logId = MAX(LogTypeId) + 1
 	FROM Logging.LogType
-
+ 
 	INSERT INTO Logging.LogType (LogTypeId, LogcategoryTypeId, Name, Description)
-	VALUES (@logId, @catId, 'Roles and Rights', 'Roles and Rights activities')
+	VALUES (@logId, @cateId, 'Roles and Rights', 'Roles and Rights activities')
 END;
-
+ 
 GO
--- Enterprise/Foundation/Activity/DatabaseV2/AuditDBV2/Scripts/PostDeployment/PostDeploymentScripts/1611312-roles-rights-activity-logs.sql
+++ Enterprise/Foundation/Activity/DatabaseV2/AuditDBV2/Scripts/PostDeployment/PostDeploymentScripts/1611312-roles-rights-activity-logs.sql
@@ -1,17 +1,12 @@
-
-
-
 IF NOT EXISTS (SELECT TOP 1 1 FROM Logging.LogCategoryType WHERE Name = 'RolesRights')
 BEGIN
 	Declare @catId int;
- 
 	SELECT @catId = MAX(LogCategoryTypeId) + 1
 	FROM Logging.LogCategoryType
- 
 	Insert into Logging.LogCategoryType (LogCategoryTypeId, Name, Description)
-	values(@catId, 'InternalProductSettings','Internal Product Settings.')
+	values(@catId, 'RolesRights','Roles and Rights.')
 END
-
+ 
 IF NOT EXISTS
 (
     SELECT 1
@@ -19,16 +14,16 @@ IF NOT EXISTS
     WHERE  Name = 'Roles and Rights'
 )
 BEGIN
-    DECLARE @catId INT;
+    DECLARE @cateId INT;
     DECLARE @logId INT;
-	SELECT @catId = LogCategoryTypeId
+	SELECT @cateId = LogCategoryTypeId
 	FROM logging.LogCategoryType 
 	WHERE NAME = 'RolesRights'
 	SELECT @logId = MAX(LogTypeId) + 1
 	FROM Logging.LogType
-
+ 
 	INSERT INTO Logging.LogType (LogTypeId, LogcategoryTypeId, Name, Description)
-	VALUES (@logId, @catId, 'Roles and Rights', 'Roles and Rights activities')
+	VALUES (@logId, @cateId, 'Roles and Rights', 'Roles and Rights activities')
 END;
-
+ 
 GO
