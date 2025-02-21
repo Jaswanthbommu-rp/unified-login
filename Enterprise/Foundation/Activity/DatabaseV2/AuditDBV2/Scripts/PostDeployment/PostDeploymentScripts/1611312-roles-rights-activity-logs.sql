a728289c93b151c1371ff09616061758059bdd5b
Shiva Kodaparthi <shiva.kodaparthi@Realpage.com>
2025-02-21 04:12:28 -0600

    Merged PR 320893: User Story 1611312: API - Activity Log details page(DEV)
    Cherry picked from !320890
    
    Related work items: #1611312

--- Enterprise/Foundation/Activity/DatabaseV2/AuditDBV2/Scripts/PostDeployment/PostDeploymentScripts/1611312-roles-rights-activity-logs.sql
+++ Enterprise/Foundation/Activity/DatabaseV2/AuditDBV2/Scripts/PostDeployment/PostDeploymentScripts/1611312-roles-rights-activity-logs.sql
@@ -1,37 +1,34 @@
 
-DECLARE @LogCategoryTypeId int;
 
-IF NOT EXISTS
-(
-    SELECT 1
-    FROM [Logging].[LogCategoryType] WHERE Name = 'RolesRights'
-)
+
+IF NOT EXISTS (SELECT TOP 1 1 FROM Logging.LogCategoryType WHERE Name = 'RolesRights')
 BEGIN
-    INSERT INTO [Logging].[LogCategoryType]( Name )
-    VALUES( 'RolesRights' );
-    SELECT @LogcategoryTypeId = SCOPE_IDENTITY();
-END;
-ELSE
-BEGIN
-    SELECT @LogCategoryTypeId=  LogcategoryTypeId
-    FROM [Logging].[LogCategoryType]
-    WHERE Name = 'RolesRights';
-END;
+	Declare @catId int;
+ 
+	SELECT @catId = MAX(LogCategoryTypeId) + 1
+	FROM Logging.LogCategoryType
+ 
+	Insert into Logging.LogCategoryType (LogCategoryTypeId, Name, Description)
+	values(@catId, 'InternalProductSettings','Internal Product Settings.')
+END
 
 IF NOT EXISTS
 (
     SELECT 1
     FROM [Logging].[LogType]
-    WHERE LogCategoryTypeId = @LogCategoryTypeId AND 
-          Name = 'Roles and Rights'
+    WHERE  Name = 'Roles and Rights'
 )
 BEGIN
+    DECLARE @catId INT;
     DECLARE @logId INT;
+	SELECT @catId = LogCategoryTypeId
+	FROM logging.LogCategoryType 
+	WHERE NAME = 'RolesRights'
 	SELECT @logId = MAX(LogTypeId) + 1
 	FROM Logging.LogType
 
 	INSERT INTO Logging.LogType (LogTypeId, LogcategoryTypeId, Name, Description)
-	VALUES (@logId, @LogCategoryTypeId, 'Roles and Rights', 'Roles and Rights activities')
+	VALUES (@logId, @catId, 'Roles and Rights', 'Roles and Rights activities')
 END;
 
 GO
