edad5ad9236570a5b644eca1a4b05d334305654b
Jaswanth Bommu <Jaswanth.Bommu@RealPage.com>
2025-02-25 11:14:36 -0600

    Merged PR 321625: User Story 2178316: DEV:Integration tests framework for coreapi and coverage
    User Story 2178316: DEV:Integration tests framework for coreapi and coverage
    
    Related work items: #2178316

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Security/IdentityServer_User.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Security/IdentityServer_User.sql
@@ -1,7 +1,7 @@
-﻿CREATE USER [IdentityServer] FOR LOGIN [IdentityServer]
-    WITH DEFAULT_SCHEMA = [Auth];
+﻿--CREATE USER [IdentityServer] FOR LOGIN [IdentityServer]
+--    WITH DEFAULT_SCHEMA = [Auth];
 
-GO
--- Permissions
-GRANT EXECUTE TO [IdentityServer]
-GO
+--GO
+---- Permissions
+--GRANT EXECUTE TO [IdentityServer]
+--GO
--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Security/RoleMemberships.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Security/RoleMemberships.sql
@@ -1,6 +1,6 @@
-﻿ALTER ROLE [db_datawriter] ADD MEMBER [IdentityServer];
+﻿--ALTER ROLE [db_datawriter] ADD MEMBER [IdentityServer];
 
 
-GO
-ALTER ROLE [db_datareader] ADD MEMBER [IdentityServer];
+--GO
+--ALTER ROLE [db_datareader] ADD MEMBER [IdentityServer];
 
