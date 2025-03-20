f0c9055c64ea8dd67343e9417c35c0be6b9dd6ee
Kalyana Chakravarthy Bingi <kalyanachakravarthy.bingi@realpage.com>
2025-03-20 17:37:09 +0530

    2251226 : employee users unable to login as myself

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/2251226_adding_IsActivityNotCheckRequired_Flag.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/2251226_adding_IsActivityNotCheckRequired_Flag.sql
@@ -4,7 +4,7 @@ DECLARE @ProductsettingTypeid int;
 IF NOT EXISTS (SELECT * FROM Enterprise.ProductSettingType WHERE [Name] = 'IsActivityCheckNotRequired')
 BEGIN
 	INSERT INTO Enterprise.ProductSettingType ([Name], [Description], SensitiveData)
-	VALUES ('IsActivityCheckNotRequired', 'Produtct activity logic check is required', 0);
+	VALUES ('IsActivityCheckNotRequired', 'Product activity logic check is required', 0);
 	SELECT @ProductsettingTypeid = ProductSettingTypeId FROM Enterprise.ProductSettingType WHERE [Name] = 'IsActivityCheckNotRequired'
     exec [Enterprise].[SetProductSetting] 0,1,@ProductsettingTypeid,''
 END
