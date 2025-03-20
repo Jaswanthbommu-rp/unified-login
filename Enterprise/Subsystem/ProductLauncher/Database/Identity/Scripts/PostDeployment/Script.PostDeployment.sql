f391ce4c31daadb2ce2a468bba087745742255d0
Kalyana Chakravarthy Bingi <kalyanachakravarthy.bingi@realpage.com>
2025-03-20 16:33:32 +0530

    2251226 : Employee users unable to login as myself.

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/Script.PostDeployment.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/Script.PostDeployment.sql
@@ -17,3 +17,4 @@ Post-Deployment Script Template
 :r .\PostDeploymentScripts\UpdateStatistics.sql
 :r .\PostDeploymentScripts\RecompileAllProcs.sql
 :R .\PostDeploymentScripts\UL_Identity_DataCleanup.sql
+:r .\PostDeploymentScripts\2251226_adding_IsActivityNotCheckRequired_Flag.sql
