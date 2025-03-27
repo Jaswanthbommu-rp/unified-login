869f8d6cba27bf71e2c1983a71947d3bb2bceebd
KalyanaChakravarthy Bingi <kalyanachakravarthy.bingi@RealPage.com>
2025-03-27 02:37:06 -0500

    Merged PR 327176: 216258 : Adding investment management product family
    216258 : Adding investment management product family
    
    Related work items: #2161258

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/2161258_adding_investment_management_family.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/2161258_adding_investment_management_family.sql
@@ -2,8 +2,8 @@
 
 IF NOT EXISTS(select TOP 1 1 from Enterprise.ProductType  where ProductTypeId = 800)
 BEGIN
- INSERT INTO Enterprise.ProductType(ParentProductTypeId, [Name],[Description],ProductTypeGuid )
- VALUES(NULL,'Investment Management','Investment Management','B3E956D7-98BB-48AF-B11D-773D07A69518')
+ INSERT INTO Enterprise.ProductType(ProductTypeId,ParentProductTypeId, [Name],[Description],ProductTypeGuid )
+ VALUES(800, NULL,'Investment Management','Investment Management','B3E956D7-98BB-48AF-B11D-773D07A69518')
 END
 
 Go
