bd2d251c97e11e3159ac923b29a04cd1247e24c4
Jaswanth Bommu <Jaswanth.Bommu@RealPage.com>
2025-04-01 06:28:57 -0500

    Merged PR 327874: Add new product codes to OmnibarProductCodes table
    Add new product codes to OmnibarProductCodes table
    
    This commit introduces multiple new entries to the `[Enterprise].[OmnibarProductCodes]` table. Each new product code ('KNCKLMA', 'KNCKANA', 'KNCKADM', 'KNCKCLAW') is checked for existence before insertion. If absent, a new record is added with `ProductId` 91, corresponding `OmnibarProductCode`, `Description`, and an `IsActive` status of 1.
    
    Related work items: #2219040

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/2219040-lumina-product-codes.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/2219040-lumina-product-codes.sql
@@ -39,3 +39,27 @@ BEGIN
     INSERT [Enterprise].[OmnibarProductCodes] ([ProductId], [OmnibarProductCode], [Description], [IsActive]) 
     VALUES (4, N'aocore', N'Asset Optimization', 1)
 END
+--Knock products
+IF NOT EXISTS (SELECT 1 FROM [Enterprise].[OmnibarProductCodes] WHERE [OmnibarProductCode] = N'KNCKLMA')
+BEGIN
+    INSERT [Enterprise].[OmnibarProductCodes] ([ProductId], [OmnibarProductCode], [Description], [IsActive]) 
+    VALUES (91, N'KNCKLMA', N'Knock CRM', 1)
+END
+
+IF NOT EXISTS (SELECT 1 FROM [Enterprise].[OmnibarProductCodes] WHERE [OmnibarProductCode] = N'KNCKANA')
+BEGIN
+    INSERT [Enterprise].[OmnibarProductCodes] ([ProductId], [OmnibarProductCode], [Description], [IsActive]) 
+    VALUES (91, N'KNCKANA', N'Knock Analytics', 1)
+END
+
+IF NOT EXISTS (SELECT 1 FROM [Enterprise].[OmnibarProductCodes] WHERE [OmnibarProductCode] = N'KNCKADM')
+BEGIN
+    INSERT [Enterprise].[OmnibarProductCodes] ([ProductId], [OmnibarProductCode], [Description], [IsActive]) 
+    VALUES (91, N'KNCKADM', N'Knock Admin', 1)
+END
+
+IF NOT EXISTS (SELECT 1 FROM [Enterprise].[OmnibarProductCodes] WHERE [OmnibarProductCode] = N'KNCKCLAW')
+BEGIN
+    INSERT [Enterprise].[OmnibarProductCodes] ([ProductId], [OmnibarProductCode], [Description], [IsActive]) 
+    VALUES (91, N'KNCKCLAW', N'Knock CLAW', 1)
+END
