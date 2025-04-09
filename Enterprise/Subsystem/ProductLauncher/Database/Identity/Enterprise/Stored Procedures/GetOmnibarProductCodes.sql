87aca5f0d425ddf1d759d29f4045cf8c5f3b8793
Jaswanth Bommu <Jaswanth.Bommu@RealPage.com>
2025-04-09 07:18:53 -0500

    Merged PR 329543: User Story 2219040: Lumina - Setup a database schema to incorporate all new omnibar product codes also Integrate the 4 Knock Apps within Lumina AIsettings api
    User Story 2219040: Lumina - Setup a database schema to incorporate all new omnibar product codes also Integrate the 4 Knock Apps within Lumina AIsettings api
    
    Related work items: #2219040

---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Enterprise/Stored Procedures/GetOmnibarProductCodes.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [Enterprise].[GetOmnibarProductCodes]
+AS
+BEGIN
+	SELECT OPC.Id,
+           OPC.OmnibarProductCode,
+           EP.BooksProductCode
+	FROM Enterprise.OmnibarProductCodes OPC
+	JOIN Enterprise.Product EP ON EP.ProductId = OPC.ProductId
+	WHERE OPC.IsActive = 1 AND EP.Active = 1
+END
