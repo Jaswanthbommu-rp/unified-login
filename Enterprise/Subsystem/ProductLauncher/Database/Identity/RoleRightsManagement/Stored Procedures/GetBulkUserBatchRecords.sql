3a6c6e068d6a128e9ca44c0777ab5fbf9aaa9a9b
Mallesham Sankati <mallesham.sankati@realpage.com>
2025-06-18 20:45:12 +0530

    User Story 2173457: DEV - UI Only - Build an Aside for Products (Remove)

---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/RoleRightsManagement/Stored Procedures/GetBulkUserBatchRecords.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE Batch.GetBulkUserBatchRecords (@BulkUserBatchProcessId INT)  
+   AS  
+   BEGIN  
+  
+   SELECT bu.BulkUserBatchProcessId,EditorUserPersonaId,SubjectUserPersonaId,StatusTypeId,BatchProcessTypeId ,bup.ProductId FROM  
+   Batch.[BulkUserBatchProcess] bu  
+   inner join [Security].[BulkUserProducts] bup on bu.bulkUserBatchProcessId = bup.bulkUserBatchProcessId  
+   WHERE bu.bulkUserBatchProcessId = @BulkUserBatchProcessId  
+  
+   END
