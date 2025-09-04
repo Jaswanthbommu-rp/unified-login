4a0c87857af44c132f54cef0c7bb8b406c169b69
Mallesham Sankati <mallesham.sankati@RealPage.com>
2025-09-04 09:08:41 -0500

    Merged PR 358103: Commit 7c441e0b: Merged PR 342570: User Story 2173457: DEV - UI Only - Build...
    Commit 7c441e0b: Merged PR 342570: User Story 2173457: DEV - UI Only - Build an Aside for Products (Remove)
    
    User Story 2173457: DEV - UI Only - Build an Aside for Products (Remove)
    
    Related work items: #2173457
    
    Cherry picked from !358100
    
    Related work items: #2173457

---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/RoleRightsManagement/Stored Procedures/ListBulkUserBatchProcessor.sql
@@ -0,0 +1,68 @@
+﻿
+CREATE PROCEDURE [Batch].[ListBulkUserBatchProcessor] ( @BatchSize INT)    
+AS      
+BEGIN      
+      
+    SET NOCOUNT ON;      
+     DECLARE @PBFiltered TABLE      
+    (      
+        [BulkUserBatchProcessId] [BIGINT] NOT NULL,      
+        [EditorUserPersonaId] [BIGINT] NOT NULL,      
+        [SubjectUserPersonaId] [BIGINT] NOT NULL,               
+        [StatusTypeId] [INT] NOT NULL,      
+        [CreatedDateTime] [DATETIME] NOT NULL,      
+        [BatchProcessTypeId] [TINYINT] NOT NULL      
+    );      
+    
+    BEGIN TRANSACTION; -- HAve to lock the tables so that another process can't come in and scoop up our waiting processes      
+      
+ ;with batchtoprocess as (      
+  SELECT      
+      [BulkUserBatchProcessId],      
+      [EditorUserPersonaId],      
+      [SubjectUserPersonaId],      
+      [StatusTypeId],      
+      [CreatedDateTime],      
+      [BatchProcessTypeId],    
+      row_number() over (partition by subjectuserpersonaid order by BulkUserBatchProcessId asc ) as rn,      
+      row_number() over (partition by editoruserpersonaid order by BulkUserBatchProcessId asc ) as rn2        
+  FROM Batch.[BulkUserBatchProcess] BP      
+  WHERE BP.StatusTypeID = 5 AND bp.createddatetime > dateadd(dd, -3, getutcdate()))      
+     
+    INSERT INTO @PBFiltered      
+    (      
+      [BulkUserBatchProcessId],      
+      [EditorUserPersonaId],      
+      [SubjectUserPersonaId] ,      
+      [StatusTypeId],      
+      [CreatedDateTime],      
+      [BatchProcessTypeId]    
+    )      
+ SELECT TOP (@BatchSize)      
+      [BulkUserBatchProcessId],      
+      [EditorUserPersonaId],      
+      [SubjectUserPersonaId] ,      
+      [StatusTypeId],      
+      [CreatedDateTime],      
+      [BatchProcessTypeId]    
+  From batchtoprocess       
+  WHERE   rn = 1       
+  And    rn2 <= 5      
+      
+    UPDATE Batch.[BulkUserBatchProcess]      
+    SET StatusTypeId = 6 --Running      
+    FROM Batch.[BulkUserBatchProcess] BP      
+        JOIN @PBFiltered F      
+            ON F.[BulkUserBatchProcessId] = BP.[BulkUserBatchProcessId];      
+      
+    SELECT [BulkUserBatchProcessId],      
+      [EditorUserPersonaId],      
+      [SubjectUserPersonaId] ,     
+      [StatusTypeId],      
+      [CreatedDateTime],      
+      [BatchProcessTypeId]    
+    FROM @PBFiltered;      
+      
+    COMMIT TRANSACTION;      
+    
+END
