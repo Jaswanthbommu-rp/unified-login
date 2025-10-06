c07308d507ae7ae1b5551a6ee0d3308390fe48ba
Jaswanth Bommu <Jaswanth.Bommu@RealPage.com>
2025-10-06 02:58:41 -0500

    Merged PR 364049: User Story 2514506: Add new product setting to control whether product is included in User Audit Reporting
    User Story 2514506: Add new product setting to control whether product is included in User Audit Reporting
    
    Related work items: #2514506

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/Script.PostDeployment.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/Script.PostDeployment.sql
@@ -32,6 +32,8 @@ Post-Deployment Script Template
 :r .\PostDeploymentScripts\2422401_Setting_TrustedDeviceExpiryDays.sql
 :r .\PostDeploymentScripts\1164582-remove-suggestProperties.sql
 
+:r .\PostDeploymentScripts\2514506-exclude-from-audit-reporting.sql
+
 -- keep these at the end!
 :r .\PostDeploymentScripts\UpdateStatistics.sql
 :r .\PostDeploymentScripts\RecompileAllProcs.sql
