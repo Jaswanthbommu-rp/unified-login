1a18876ad03781b33cc06edd62143befeb6bdc1b
Jaswanth Bommu <jaswanth.bommu@realpage.com>
2025-12-01 16:15:54 +0530

    Add enterprise project and update app configuration
    Added `UnifiedLogin_LandingAPIEnterprise` to support enterprise-specific functionality. Updated `launchSettings.json` to reflect new URLs, removed IIS Express settings, and restructured `appsettings.json` to include new connection strings, logging configurations, and sections for platform integration, client integration, and feature flag management. Removed outdated configurations like `Serilog` and `LandingAPICORSAllowedOrigins`. Enhanced logging granularity and aligned settings with enterprise requirements.

--- CoreMigration/UnifiedLogin.AppHost/AppHost.cs
+++ CoreMigration/UnifiedLogin.AppHost/AppHost.cs
@@ -2,4 +2,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.UnifiedLogin_LandingAPI>("unifiedlogin-landingapi");
 
+builder.AddProject<Projects.UnifiedLogin_LandingAPIEnterprise>("unifiedlogin-landingapi-enterprise");
+
+
 builder.Build().Run();
