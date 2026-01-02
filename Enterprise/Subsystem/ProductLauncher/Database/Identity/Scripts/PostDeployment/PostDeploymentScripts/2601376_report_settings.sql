86981989dd6437ddf4fb4194baa7d40ed213b27c
KalyanaChakravarthy Bingi <kalyanachakravarthy.bingi@RealPage.com>
2026-01-02 05:32:54 -0600

    Merged PR 380759: User Story 2601376: Unified Platform reports failed to generate to change status to Error
    2601376 : Report settings db changes
    
    Cherry picked from !379125

---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/2601376_report_settings.sql
@@ -0,0 +1,19 @@
+﻿--User Story 2601376: Unified Platform reports failed to generate to change status to Error 
+DECLARE @ProductsettingTypeid int;
+ 
+IF NOT EXISTS (SELECT * FROM Enterprise.ProductSettingType WHERE [Name] = 'UserReport_Batchsize')
+BEGIN
+    INSERT INTO Enterprise.ProductSettingType ([Name], [Description], SensitiveData)
+    VALUES ('UserReport_Batchsize', 'Specifies the number records to be processed on each batch.', 0);
+    SELECT @ProductsettingTypeid = ProductSettingTypeId FROM Enterprise.ProductSettingType WHERE [Name] = 'UserReport_Batchsize'
+    exec [Enterprise].[SetProductSetting] 300,3,@ProductsettingTypeid,''
+END
+
+IF NOT EXISTS (SELECT * FROM Enterprise.ProductSettingType WHERE [Name] = 'UserReport_PerBatchDelaySeconds')
+BEGIN
+    INSERT INTO Enterprise.ProductSettingType ([Name], [Description], SensitiveData)
+    VALUES ('UserReport_PerBatchDelaySeconds', 'Specifies the seconds the next batch has to hold.', 0);
+    SELECT @ProductsettingTypeid = ProductSettingTypeId FROM Enterprise.ProductSettingType WHERE [Name] = 'UserReport_Batchsize'
+    exec [Enterprise].[SetProductSetting] 30,3,@ProductsettingTypeid,''
+END
+GO
