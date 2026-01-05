3cc6d6b36368a174afe0cf299d07eb0b903da524
KalyanaChakravarthy Bingi <kalyanachakravarthy.bingi@RealPage.com>
2026-01-05 09:19:05 -0600

    Merged PR 381056: 2546533 :incorrect primary properties logging fix
    2546533 :incorrect primary properties logging fix
    
    Cherry picked from !378915
    
    Related work items: #2546533

--- Enterprise/Subsystem/ProductLauncher/Component/Landing/Repository/UserRepository.cs
+++ Enterprise/Subsystem/ProductLauncher/Component/Landing/Repository/UserRepository.cs
@@ -55,6 +55,7 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Component.Landing.Repository
         IProductInternalSettingRepository _productInternalSettingRepository;
         private ManageBlueBook _manageBlueBook;
         private ManageUnifiedSettings _manageUnifiedSettings;
+        private readonly IUnifiedSettingsRepository _unifiedSettingsRepository;
 
         #region Ctor
 
@@ -73,6 +74,7 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Component.Landing.Repository
             _roleTypeRepository = new RoleTypeRepository();
             _manageBlueBook = new ManageBlueBook();
             _manageUnifiedSettings = new ManageUnifiedSettings(_userClaim);
+            _unifiedSettingsRepository = new UnifiedSettingsRepository();
         }
 
         /// <summary>
@@ -6268,6 +6270,14 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Component.Landing.Repository
             var productInternalSettingList = _productInternalSettingRepository.GetProductInternalSettings((int)ProductEnum.UnifiedPlatform);
             var platformAdminRole = productInternalSettingList.FirstOrDefault(s => s.Name.Equals("PlatformAdminRole", StringComparison.OrdinalIgnoreCase))?.Value;
             IUserLoginOnly impersonatorUserLoginOnly = new UserLoginOnly();
+            
+            int organizationUsePrimaryProperties = 0;
+            var companyProductSettings = _unifiedSettingsRepository.GetUnifiedSettings(_userClaim.OrganizationPartyId, "company");
+            if (companyProductSettings.Any(a => a.Name.Equals("PrimaryProperty", StringComparison.OrdinalIgnoreCase)))
+            {
+                var settingValue = companyProductSettings.FirstOrDefault(a => a.Name.Equals("PrimaryProperty", StringComparison.OrdinalIgnoreCase)).Value;
+                int.TryParse(settingValue, out organizationUsePrimaryProperties);
+            }
             if (_userClaim.ImpersonatedBy != Guid.Empty)
             {
                 impersonatorUserLoginOnly = _userLoginRepository.GetUserLoginOnly(_userClaim.ImpersonatedBy);
@@ -7083,9 +7093,9 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Component.Landing.Repository
                         repository.UnitOfWork.Commit();
 
                         AuditUserUpdate(updateUserProfileEntity.OldProfile, updateUserProfileEntity.NewProfile);
-
+                        
                         //add activity log for Primary property
-                        if (isPrimaryPropertiesUpdated)
+                        if (isPrimaryPropertiesUpdated && organizationUsePrimaryProperties == 1)
                         {
                             string message = "{2} updated Primary Properties for {0} {1}.";
                             List<AdditionalParameters> additionalParameters = new List<AdditionalParameters>();
