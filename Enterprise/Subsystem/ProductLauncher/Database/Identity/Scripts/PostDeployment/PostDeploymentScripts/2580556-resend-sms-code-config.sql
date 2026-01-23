acbc3e7fbcdf942d1e66cf974311128abfad2dee
Jaswanth Bommu <Jaswanth.Bommu@RealPage.com>
2026-01-23 09:10:19 -0600

    Merged PR 384189: User Story 2580556: MFA - Add Resend Code link in SMS method - DEV
    User Story 2580556: MFA - Add Resend Code link in SMS method - DEV
    
    Related work items: #2580556

---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/2580556-resend-sms-code-config.sql
@@ -0,0 +1,8 @@
+﻿--User Story 2580556: MFA - Add Resend Code link in SMS method - DEV
+
+IF NOT EXISTS (SELECT * FROM Enterprise.ProductSettingType WHERE [Name] = 'SmsResendCodeTimeoutSeconds')
+BEGIN
+    INSERT INTO Enterprise.ProductSettingType ([Name], [Description], SensitiveData)
+    VALUES ('SmsResendCodeTimeoutSeconds', 'Time in seconds for resending SMS verification codes.', 0);
+END
+GO
--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/Script.PostDeployment.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/Script.PostDeployment.sql
@@ -10,38 +10,10 @@ Post-Deployment Script Template
 --------------------------------------------------------------------------------------
 */
 
-:r .\PostDeploymentScripts\2156346-redis-cache-settings.sql
-:r .\PostDeploymentScripts\2161258_adding_investment_management_family.sql
-:r .\PostDeploymentScripts\2219040-lumina-product-codes.sql
-:r .\PostDeploymentScripts\2200712-adding-bulletin-board-rights.sql
-
-:r .\PostDeploymentScripts\2252341_ContractMangementSetting.sql
-:r .\PostDeploymentScripts\2173457BulkUserUnAssignProducts.sql
-
-:r .\PostDeploymentScripts\2255379-consume-demo-orgtype.sql
---:r .\PostDeploymentScripts\RemoveOldIdentityServerSchema.sql
-:r .\PostDeploymentScripts\UserStory2310608RealConnectCache.sql
-
-:r .\PostDeploymentScripts\2375427-product-add-setting-friendlyurlproductname.sql
-:r .\PostDeploymentScripts\2310599-add-settings-rights.sql
-:r .\PostDeploymentScripts\2251226_adding_IsActivityNotCheckRequired.sql
-
-:r .\PostDeploymentScripts\2481503-lumina-product-codes.sql
-:r .\PostDeploymentScripts\FusionCacheSettings_2156341.sql
-:r .\PostDeploymentScripts\2251226_adding_IsActivityNotCheckRequired.sql
-:r .\PostDeploymentScripts\2422401_Setting_TrustedDeviceExpiryDays.sql
-:r .\PostDeploymentScripts\1164582-remove-suggestProperties.sql
-:r .\PostDeploymentScripts\2541436-add-setting-PlatformAdminRole.sql
-:r .\PostDeploymentScripts\2410493-adding-right-ManageMaintenanceContactCenterProductAccess.sql
 :r .\PostDeploymentScripts\2625610_add_updateIfExists_MEDS_setting.sql
-
-:r .\PostDeploymentScripts\2472100-add-reporting-access-to-sde-right.sql
-
-:r .\PostDeploymentScripts\2514506-exclude-from-audit-reporting.sql
-:r .\PostDeploymentScripts\Migo_deprecate.sql
-:r .\PostDeploymentScripts\2322387_CompanyPropertyActivateDeactivate.sql
 :r .\PostDeploymentScripts\2601376_report_settings.sql
 :r .\PostDeploymentScripts\2625222-add-setting-UseNewProductUsersEndPoint.sql
+:r .\PostDeploymentScripts\2580556-resend-sms-code-config.sql
 
 -- keep these at the end!
 :r .\PostDeploymentScripts\UpdateStatistics.sql
