ec1b5f7d0b554a5a86e5a5dfc5143b6ca1bdc9d9
Shiva Kodaparthi <shiva.kodaparthi@Realpage.com>
2026-02-19 07:34:44 -0600

    Merged PR 390504: feature(API) Enable Voice AI product-level configuration setting
    feature(API) Enable Voice AI product-level configuration setting
    
    Cherry picked from !384706
    
    Related work items: #2648822

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/2018027-lumina-settings.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/2018027-lumina-settings.sql
@@ -14,4 +14,14 @@ BEGIN
 	INSERT INTO Enterprise.ProductSettingType ([Name], [Description], SensitiveData)
 	VALUES ('CopilotActionDefault', 'Default value for copilot action and product', 0);
 END
+IF NOT EXISTS (SELECT TOP (1) 1 FROM Enterprise.ProductSettingType WHERE [Name] = 'CopilotVoiceAIDefault')
+BEGIN
+	INSERT INTO Enterprise.ProductSettingType ([Name], [Description], SensitiveData)
+	VALUES ('CopilotVoiceAIDefault', 'Default value for copilot VoiceAI and product', 0);
+END
+IF NOT EXISTS (SELECT TOP (1) 1 FROM Enterprise.ProductSettingType WHERE [Name] = 'CopilotAgenticAIDefault')
+BEGIN
+	INSERT INTO Enterprise.ProductSettingType ([Name], [Description], SensitiveData)
+	VALUES ('CopilotAgenticAIDefault', 'Default value for Copilot AgenticAI Default and product', 0);
+END
 GO
