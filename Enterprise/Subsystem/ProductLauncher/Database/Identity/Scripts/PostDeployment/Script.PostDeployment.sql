1fe988d3b8579329c64fdf37db24198dcce4fe38
KalyanaChakravarthy Bingi <kalyanachakravarthy.bingi@RealPage.com>
2026-02-24 12:02:18 -0600

    Merged PR 391636: admin support portal script changes
    admin support portal script changes
    
    Related work items: #2563145

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/Script.PostDeployment.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/Script.PostDeployment.sql
@@ -15,6 +15,7 @@ Post-Deployment Script Template
 :r .\PostDeploymentScripts\2625222-add-setting-UseNewProductUsersEndPoint.sql
 :r .\PostDeploymentScripts\2580556-resend-sms-code-config.sql
 :r .\PostDeploymentScripts\2479012_Update_Menu_ClientSettings.sql
+:r .\PostDeploymentScripts\2563145_AdminSupportal_Standard_IntegrationScript.sql
 
 -- keep these at the end!
 :r .\PostDeploymentScripts\UpdateStatistics.sql
