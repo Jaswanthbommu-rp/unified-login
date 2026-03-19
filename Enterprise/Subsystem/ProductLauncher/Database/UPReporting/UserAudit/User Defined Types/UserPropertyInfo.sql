320995776091e2549ca711b4fbe6206e710eb4e3
Kalyana Chakravarthy Bingi <kalyanachakravarthy.bingi@realpage.com>
2026-03-19 15:13:45 +0530

    Tablevalue types added

--- Enterprise/Subsystem/ProductLauncher/Database/UPReporting/UserAudit/User Defined Types/UserPropertyInfo.sql
+++
@@ -1,5 +0,0 @@
-CREATE TYPE [UserAudit].[UserPropertyInfo] AS TABLE
-(
-    [Name]  NVARCHAR(512)  NULL,
-    [Id]    NVARCHAR(256)  NULL
-);
--- Enterprise/Subsystem/ProductLauncher/Database/UPReporting/UserAudit/User Defined Types/UserRoleInfo.sql
+++
@@ -1,6 +0,0 @@
-
-CREATE TYPE [UserAudit].[UserRoleInfo] AS TABLE
-(
-    [Name]  NVARCHAR(512)  NULL,
-    [Id]    NVARCHAR(256)  NULL
-);
