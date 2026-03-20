a3bc36af826c2b1a9e93c4b521558edbbbbaf9d5
Shiva Kodaparthi <shiva.kodaparthi@Realpage.com>
2026-03-20 02:16:19 -0500

    Merged PR 396305: User Story 2606618: Update Create User logic for Maintenance Contact Center 2.0
    adding setting PhoneNumberRequiredProducts
    
    Related work items: #2606618

---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/2606618_adding_setting_PhoneNumberRequiredProducts.sql
@@ -0,0 +1,12 @@
+ 
+
+DECLARE @ProductsettingTypeid int;
+ 
+IF NOT EXISTS (SELECT * FROM Enterprise.ProductSettingType WHERE [Name] = 'PhoneNumberRequiredProducts')
+BEGIN
+	INSERT INTO Enterprise.ProductSettingType ([Name], [Description], SensitiveData)
+	VALUES ('PhoneNumberRequiredProducts', 'Products that require a phone number to assign.', 0);
+	SELECT @ProductsettingTypeid = ProductSettingTypeId FROM Enterprise.ProductSettingType WHERE [Name] = 'PhoneNumberRequiredProducts'
+    exec [Enterprise].[SetProductSetting] 0,3,@ProductsettingTypeid,'78,91,105'
+END
+GO
