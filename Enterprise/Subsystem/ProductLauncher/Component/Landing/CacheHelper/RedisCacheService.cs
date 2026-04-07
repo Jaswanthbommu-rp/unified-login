57c00612f05f9a75096af7ebefec2c3988a0ed5d
KalyanaChakravarthy Bingi <kalyanachakravarthy.bingi@RealPage.com>
2026-04-07 05:02:12 +0000

    2774233 : Onesite roles & properties caching

--- Enterprise/Subsystem/ProductLauncher/Component/Landing/CacheHelper/RedisCacheService.cs
+++ Enterprise/Subsystem/ProductLauncher/Component/Landing/CacheHelper/RedisCacheService.cs
@@ -4,7 +4,7 @@ using Newtonsoft.Json;
 
 namespace RP.Enterprise.Subsystem.ProductLauncher.Component.Landing.CacheHelper
 {
-    public class RedisCacheService: IRedisCacheService
+    public class RedisCacheService : IRedisCacheService
     {
         private readonly IDatabase _cache;
 
@@ -15,18 +15,21 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Component.Landing.CacheHelper
 
         public void SetCacheValue<T>(string key, T value, TimeSpan? expiry = null)
         {
+            if (_cache == null) return;
             var serializedValue = JsonConvert.SerializeObject(value);
             _cache.StringSet(key, serializedValue, expiry);
         }
 
         public T GetCacheValue<T>(string key)
         {
+            if (_cache == null) return default(T);
             var serializedValue = _cache.StringGet(key);
             return serializedValue.HasValue ? JsonConvert.DeserializeObject<T>(serializedValue) : default(T);
         }
 
         public void RemoveCacheValue(string key)
         {
+            if (_cache == null) return;
             _cache.KeyDelete(key);
         }
     }
