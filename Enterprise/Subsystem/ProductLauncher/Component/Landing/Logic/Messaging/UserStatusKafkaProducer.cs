be3e7c52bee3dde993e4beaddef27d76a8ee6c78
Mallesham Sankati <mallesham.sankati@realpage.com>
2026-04-15 06:03:45 +0530

    schema values

--- Enterprise/Subsystem/ProductLauncher/Component/Landing/Logic/Messaging/UserStatusKafkaProducer.cs
+++ Enterprise/Subsystem/ProductLauncher/Component/Landing/Logic/Messaging/UserStatusKafkaProducer.cs
@@ -7,6 +7,7 @@ using Serilog;
 using Serilog.Events;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Threading.Tasks;
 
 namespace RP.Enterprise.Subsystem.ProductLauncher.Component.Landing.Logic.Messaging
@@ -91,7 +92,14 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Component.Landing.Logic.Messag
                     { "Username", avroMessage.user_login_name },
                     { "IsActive", avroMessage.is_active },
                     { "PersonaId", avroMessage.persona_id },
-                    { "ActivationDate", avroMessage.user_activation_deactivation_date }
+                    { "ActivationDate", avroMessage.user_activation_deactivation_date },
+                    { "OnPrem" , ConfigurationManager.AppSettings["Kafka:OnPrem"]},
+                    { "KafkaConfigurationOnPrem",KafkaConfiguration.OnPrem},
+                    { "BootstrapServers",ConfigurationManager.AppSettings["Kafka:BootstrapServers"]},
+                    { "SchemaRegistryUrl" , ConfigurationManager.AppSettings["Kafka:SchemaRegistryUrl"]},
+                    { "SchemaRegistryUserInfo" , ConfigurationManager.AppSettings["Kafka:SchemaRegistryUserInfo"]},
+                    { "SaslUsername" , ConfigurationManager.AppSettings["Kafka:SaslUsername"]},
+                    { "SaslPassword" , ConfigurationManager.AppSettings["Kafka:SaslPassword"]}
                 };
 
                 logger = logger.ForContext("AdditionalInfo", JsonConvert.SerializeObject(logData, Formatting.Indented), false);
