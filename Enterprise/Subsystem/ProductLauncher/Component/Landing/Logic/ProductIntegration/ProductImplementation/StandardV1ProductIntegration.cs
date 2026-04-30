f112cda4e79d1871569f20ea63ae4fe649be2920
KalyanaChakravarthy Bingi <kalyanachakravarthy.bingi@RealPage.com>
2026-04-30 08:07:47 -0500

    Merged PR 406344: Revert '2306166 : knock crm assign all properties'
    2306166 : knock crm assign all properties
    
    Cherry picked from !351605
    
    Reverts !405218
    
    Related work items: #2306166

--- Enterprise/Subsystem/ProductLauncher/Component/Landing/Logic/ProductIntegration/ProductImplementation/StandardV1ProductIntegration.cs
+++ Enterprise/Subsystem/ProductLauncher/Component/Landing/Logic/ProductIntegration/ProductImplementation/StandardV1ProductIntegration.cs
@@ -533,7 +533,6 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Component.Landing.Logic.Produc
                 WriteToDiagnosticLog(
                     "{ActionName} - {state}", logData: new Dictionary<string, object>() { { "baseUrlAndQuery", baseUrlAndQuery } }, messageProperties: new object[] { "GetProductPropertyGroups", $"Product {ProductId} editorPersona id - {EditorUserDetails.PersonaId}. Calling API" });
 
-                Dictionary<string, bool> additionalData = new Dictionary<string, bool>();
                 var groupList = GetResultFromApi<IList<ProductPropertyGroups>>(baseUrlAndQuery);
 
                 WriteToDiagnosticLog(
@@ -545,10 +544,6 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Component.Landing.Logic.Produc
                         "{ActionName} - {state}", messageProperties: new object[] { "GetProductPropertyGroups", $"Product {ProductId} editorPersona id - {EditorUserDetails.PersonaId}. Calling GetUser for subject persona Id -{SubjectUserDetails.PersonaId}" });
 
                     var user = GetProductUser();
-                    if (user.PropertyGroups.Contains("all"))
-                    {
-                        additionalData.Add("allProperties", true);
-                    }
 
                     // map user regions
                     if (user != null)
@@ -569,7 +564,6 @@ namespace RP.Enterprise.Subsystem.ProductLauncher.Component.Landing.Logic.Produc
                     TotalRows = groupList.Count(),
                     RowsPerPage = 9999,
                     ErrorReason = string.Empty,
-                    Additional = additionalData,
                     TotalPages = 1
                 };
             }
