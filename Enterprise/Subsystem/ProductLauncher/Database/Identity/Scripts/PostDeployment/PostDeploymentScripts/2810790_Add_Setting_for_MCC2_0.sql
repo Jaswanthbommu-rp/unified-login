9863d0700485775e8fe43404a6dcbbf54484f041
KalyanaChakravarthy Bingi <kalyanachakravarthy.bingi@RealPage.com>
2026-05-08 00:05:56 -0500

    Merged PR 407682: Cherry-pick PR 402964 (2810790: MMC2.0 update unity user) to release/2026.03.50.WMU
    Cherry-pick of PR 402964 (commit 1f613f160) into release/2026.03.50.WMU.
    
    Original PR: 402964 - 2810790 : MMC2.0 update unity user
    
    Conflict resolutions during cherry-pick:
    - Identity.sqlproj: Added only the 2810790 sqlproj entry (the change introduced by PR 402964). The 2718529 entry shown in the conflict belongs to a different PR and was left untouched.
    - Script.PostDeployment.sql: Added only :r .\PostDeploymentScripts\2810790_Add_Setting_for_MCC2_0.sql (release branch already had the surrounding entries).
    
    Files changed: 4 (matches original PR's stat).
    
    Related work items: #2810790
    
    Related work items: #2810790

---
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/Scripts/PostDeployment/PostDeploymentScripts/2810790_Add_Setting_for_MCC2_0.sql
@@ -0,0 +1,10 @@
+﻿GO
+IF NOT EXISTS (SELECT * FROM Enterprise.ProductSettingType WHERE [Name] = 'IsRequireSamlCreateOnUpdateUser')
+BEGIN
+	INSERT INTO Enterprise.ProductSettingType ([Name], [Description], SensitiveData)
+	VALUES ('IsRequireSamlCreateOnUpdateUser', 'Products requires saml add on updating user.', 0);
+	DECLARE @ProductsettingTypeid int;
+	SELECT @ProductsettingTypeid = ProductSettingTypeId FROM Enterprise.ProductSettingType WHERE [Name] = 'IsRequireSamlCreateOnUpdateUser'
+    exec [Enterprise].[SetProductSetting] 0,105,@ProductsettingTypeid,'1'
+END
+GO
