1df1d452140438a1f4104d49fc9c321135e85d99
KalyanaChakravarthy Bingi <kalyanachakravarthy.bingi@RealPage.com>
2026-05-08 01:57:01 -0500

    Merged PR 408027: Bug 2811407: Enterprise Role Updates Propagate to Deactivated Use...
    changes for Bug 2811407: Enterprise Role Updates Propagate to Deactivated Users and Reactivate Product Assignments
    
    Cherry picked from !405647

--- Enterprise/Subsystem/ProductLauncher/Database/Identity/RoleRightsManagement/Stored Procedures/InsertPersonaToBatchProcess.sql
+++ Enterprise/Subsystem/ProductLauncher/Database/Identity/RoleRightsManagement/Stored Procedures/InsertPersonaToBatchProcess.sql
@@ -7,7 +7,10 @@ AS
 	BEGIN
 		INSERT INTO Batch.[EnterpriseRoleBatchProcess] (EditorUserPersonaId,SubjectUserPersonaId,EnterpriseRoleTemplateId,StatusTypeId,  
 			CreatedDateTime, BatchProcessTypeId, UseAPIV2)
-		SELECT @EditorPersonaId, PersonaId, @EnterpriseRoleId, 5, GETUTCDATE(), 11, @UseAPIV2
-		FROM Security.RoleTemplateUserMapping
-		WHERE RoleTemplateId = @EnterpriseRoleId
+		SELECT @EditorPersonaId, RTUM.PersonaId, @EnterpriseRoleId, 5, GETUTCDATE(), 11, @UseAPIV2
+		FROM Security.RoleTemplateUserMapping RTUM
+		INNER JOIN Person.Persona p ON p.PersonaId = RTUM.PersonaId
+		INNER JOIN Ident.UserLoginPersona ulp ON p.UserLoginPersonaId = ulp.UserLoginPersonaId
+        WHERE RTUM.RoleTemplateId = @EnterpriseRoleId
+            AND ulp.StatusTypeId NOT IN (19,24)
 	END
